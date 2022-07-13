a571cc0a267aba421bdf9aead505971233377b7f
Joey Kraut <joey@otter.cash>
2022-07-13 11:26:26 -0700

    core: heartbeat: Expire known peers after consecutive failed heartbeats
    This commit adds peer expiration to the gossip protocol. If a peer
    has not sent a successful heartbeat within some tolerated time, we
    expire the peer and no longer send it heartbeats.
    
    This will eventually result in a replication event sent to other
    cluster members to re-replicate the wallets owned by the failed peer.

--- src/gossip.rs
+++ src/gossip.rs
@@ -16,8 +16,10 @@ use libp2p::{
     swarm::{Swarm, SwarmEvent}, kad::KademliaEvent,
 };
 use std::{
-    collections::HashSet,
+    collections::HashMap,
     error::Error,
+    sync::atomic::{AtomicU64, Ordering},
+    time::{SystemTime, UNIX_EPOCH},
 };
 use tokio::{
     time::{sleep, Duration},
@@ -31,18 +33,52 @@ use crate::gossip::{
 };
 
 const HEARTBEAT_INTERVAL_MS: u64 = 5000;
+const HEARTBEAT_FAILURE_S: u64 = 10;
 
-pub struct GossipPeer {
+pub struct GossipServer {
     // Local peer information
     port: u32,
     local_peer_id: PeerId,
     
     // Network information
-    known_peers: HashSet<PeerId>,
+    known_peers: HashMap<PeerId, PeerInfo>,
     swarm: Swarm<ComposedNetworkBehavior>
 }
 
-impl GossipPeer {
+// Contains information about connected peers
+#[derive(Debug)]
+struct PeerInfo {
+    // Last time a successful hearbeat was received from this peer
+    last_heartbeat: AtomicU64
+}
+
+impl PeerInfo {
+    pub fn new() -> Self {
+        Self { last_heartbeat: AtomicU64::new(current_time_seconds()) }
+    }
+
+    // Records a successful heartbeat
+    pub fn successful_heartbeat(&mut self) {
+        self.last_heartbeat.store(current_time_seconds(), Ordering::Relaxed);
+    }
+}
+
+// Returns a u64 representing the current unix timestamp in seconds
+fn current_time_seconds() -> u64 {
+    SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .expect("negative timestamp")
+                .as_secs()
+}
+
+// Clones PeerInfo to reference the curren time for the last heartbeat
+impl Clone for PeerInfo {
+    fn clone(&self) -> Self {
+        Self { last_heartbeat: AtomicU64::new(self.last_heartbeat.load(Ordering::Relaxed)) }         
+    }
+}
+
+impl GossipServer {
     // Creates a new gossip server
     pub async fn new(
         port: u32,
@@ -58,12 +94,13 @@ impl GossipPeer {
 
         let mut behavior = ComposedNetworkBehavior::new(local_peer_id);
         // Add all addresses for bootstrap servers
-        let mut known_peers = HashSet::new();
+        let mut known_peers = HashMap::new();
         for known_peer in bootstrap_servers.iter() {
             let addr: Multiaddr = "/ip4/127.0.0.1/tcp/12345".parse()?;
             behavior.kademlia_dht.add_address(known_peer, addr);
 
-            known_peers.insert(*known_peer);
+            let peer_info = PeerInfo::new(); 
+            known_peers.insert(*known_peer, peer_info);
         }
 
         // Connect the behavior and transport together through swarm
@@ -153,6 +190,10 @@ impl GossipPeer {
 
                 // A peer has responded to a dialup, confirming their heartbeat
                 RequestResponseMessage::Response { response, .. } => {
+                    // Update the heartbeat for the peer that responded
+                    self.record_heartbeat(&peer);
+
+                    // Update routing and replication information 
                     self.merge_peers(response.get_known_peers()?);
                     println!("Received heartbeat response: {:?}", response);
                 }
@@ -162,23 +203,21 @@ impl GossipPeer {
         Ok(())
     }
 
-    // Processes a Kademlia DHT event from the Kademlia network behavior
-    async fn process_kademlia_event(
-        &mut self,
-        event: KademliaEvent
-    ) -> Result<(), Box<dyn std::error::Error>> {
-        println!("Received Kademlia event: {:?}", event);
-        Ok(())
+    // Records a successful heartbeat
+    fn record_heartbeat(&mut self, peer_id: &PeerId) {
+        if let Some(peer_info) = self.known_peers.get_mut(peer_id) {
+            peer_info.successful_heartbeat();
+        }
     }
 
     // Merges the peers from a heartbeat into the set of known peers
     fn merge_peers(&mut self, new_peers: Vec<PeerId>) {
         for peer in new_peers.iter() {
-            if *peer == self.local_peer_id {
+            if *peer == self.local_peer_id || self.known_peers.contains_key(peer) {
                 continue;
             }
 
-            self.known_peers.insert(*peer);
+            self.known_peers.insert(*peer, PeerInfo::new());
         }
     }
 
@@ -192,18 +231,48 @@ impl GossipPeer {
             self.swarm
                 .behaviour_mut()
                 .request_response
-                .send_request(&peer_id, request);
+                .send_request(&peer_id.0, request);
         }
+
+        // Remove any peers that have timed out on heartbeats
+        self.expire_peers();
+    }
+
+    // Expires any peers that have not replied to a heartbeat recently
+    fn expire_peers(&mut self) {
+        let now = current_time_seconds();
+        let mut peers_to_remove = Vec::new();
+
+        for (peer_id, peer_info) in self.known_peers.iter() {
+            if now - peer_info.last_heartbeat.load(Ordering::Relaxed) >= HEARTBEAT_FAILURE_S {
+                println!("Expiring peer: {}", peer_id);
+                peers_to_remove.push(*peer_id);
+            } 
+        }
+
+        for peer in peers_to_remove.iter() {
+            self.known_peers.remove(peer);
+        }
+
+        println!("Finished expiring {} peers", peers_to_remove.len());
     }
 
     // Constructs the heartbeat message for the local peer
     fn get_heartbeat_message(&self) -> HeartbeatMessage {
         let mut known_peers: Vec<PeerId> = Vec::new();
         for peer in self.known_peers.iter() {
-            known_peers.push(*peer);
+            known_peers.push(*peer.0);
         }
 
         HeartbeatMessage::new(known_peers)
     }
 
+    // Processes a Kademlia DHT event from the Kademlia network behavior
+    async fn process_kademlia_event(
+        &mut self,
+        event: KademliaEvent
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        println!("Received Kademlia event: {:?}", event);
+        Ok(())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod config;
 mod gossip;
 
-use crate::gossip::GossipPeer;
+use crate::gossip::GossipServer;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -9,9 +9,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = config::parse_command_line_args().expect("error parsing command line args");
     println!("Relayer running with\n\t version: {}\n\t port: {}", args.version, args.port);
 
-    let mut gossip_peer = GossipPeer::new(
+    let mut gossip_server = GossipServer::new(
         args.port, 
         args.boostrap_servers
     ).await.unwrap();
-    gossip_peer.start().await
+    gossip_server.start().await
 }
