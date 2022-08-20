5ea732fd5e4830c13bce066acb53680edb9c4c91
Joey Kraut <joey@otter.cash>
2022-08-20 11:46:22 -0700

    circuits: bench: Remove verification from VALID_MATCH benchmark

--- circuits/benches/valid_match_benchmark.rs
+++ circuits/benches/valid_match_benchmark.rs
@@ -1,13 +1,12 @@
 use std::time::Duration;
 
-use ark_groth16::{prepare_verifying_key, verify_proof};
-use ark_r1cs_std::fields::fp::FpVar;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use circuits::{
-    types::{Wallet, Balance, Order, OrderSide, SystemField},
+    types::{Wallet, Balance, Order, OrderSide},
     circuits::valid_match::ValidMatchCircuit
 };
 
+#[allow(dead_code)]
 fn valid_match_proving_time(c: &mut Criterion) {
     // Create two wallets to benchmark against
     let max_balances = 20;
@@ -30,24 +29,15 @@ fn valid_match_proving_time(c: &mut Criterion) {
     let wallet1_hash = wallet1.hash();
     let wallet2_hash = wallet2.hash();
 
-    let wallet1_hash_var = SystemField::from(wallet1_hash.clone());
-    let wallet2_hash_var = SystemField::from(wallet2_hash.clone());
-
     // Build the proving key
     println!("Building circuit and proving key...");
     let proving_key = ValidMatchCircuit::create_proving_key(max_balances, max_orders).unwrap();
-
-    let verifying_key = prepare_verifying_key(&proving_key.vk);
 
     println!("Starting benchmark...");
     c.bench_function("VALID_MATCH proof generation n_balances = 20, n_orders = 20", |b| b.iter(
         || {
             let match_circuit = ValidMatchCircuit::new(wallet1.clone(), wallet2.clone(), wallet1_hash.clone(), wallet2_hash.clone());
-            let proof = match_circuit.create_proof(black_box(&proving_key)).unwrap();
-            assert!(
-                verify_proof(&verifying_key, &proof, &vec![wallet1_hash_var, wallet2_hash_var])
-                    .unwrap()
-            );
+            match_circuit.create_proof(black_box(&proving_key)).unwrap();
         })
     );
 }
