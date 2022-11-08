f05ee2222852565b3490eb4453acabd93ff119f6
Joey Kraut <joey@otter.cash>
2022-11-08 09:22:52 -0800

    workspace: Deny unsafe code in all crates
    Read a tweet about someone who always uses unsafe code; realized
    how absolutely awful of an idea that is, decided that we should
    add more barriers to writing unsafe code in our repos.

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,3 +1,5 @@
+#![deny(unsafe_code)]
+
 mod config;
 mod gossip;
 mod handshake;
--- integration-helpers/src/lib.rs
+++ integration-helpers/src/lib.rs
@@ -1,5 +1,7 @@
 //! Groups helpers used for integration testing
 #![deny(missing_docs)]
+#![deny(unsafe_code)]
+
 pub mod macros;
 pub mod mpc_network;
 pub mod types;
