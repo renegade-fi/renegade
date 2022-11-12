908a38bc08e03ddd8a3ad8974ddfe706493d4941
Joey Kraut <joey@otter.cash>
2022-11-11 17:46:40 -0800

    circuits: lib: Add test helper to prove and verify collaborative proof

--- circuits/src/lib.rs
+++ circuits/src/lib.rs
@@ -236,17 +236,22 @@ pub(crate) mod test_helpers {
     use ark_ff::{Fp256, MontBackend, MontConfig};
     use ark_sponge::poseidon::PoseidonConfig;
     use curve25519_dalek::scalar::Scalar;
+    use itertools::Itertools;
     use merlin::Transcript;
     use mpc_bulletproof::{
         r1cs::{Prover, Verifier},
-        r1cs_mpc::R1CSError,
+        r1cs_mpc::{MpcProver, MultiproverError, R1CSError},
         PedersenGens,
+    };
+    use mpc_ristretto::{
+        authenticated_ristretto::AuthenticatedCompressedRistretto, beaver::SharedValueSource,
+        network::MpcNetwork,
     };
     use num_bigint::{BigInt, BigUint};
 
     use crate::{
-        bigint_to_scalar, mpc_gadgets::poseidon::PoseidonSpongeParameters, scalar_to_bigint,
-        scalar_to_biguint, SingleProverCircuit,
+        bigint_to_scalar, mpc::SharedFabric, mpc_gadgets::poseidon::PoseidonSpongeParameters,
+        scalar_to_bigint, scalar_to_biguint, MultiProverCircuit, SingleProverCircuit,
     };
 
     const TRANSCRIPT_SEED: &str = "test";
@@ -318,7 +323,7 @@ pub(crate) mod test_helpers {
 
     /// Abstracts over the flow of proving and verifying a circuit given
     /// a valid statement + witness assignment
-    pub(crate) fn bulletproof_prove_and_verify<C: SingleProverCircuit>(
+    pub fn bulletproof_prove_and_verify<C: SingleProverCircuit>(
         witness: C::Witness,
         statement: C::Statement,
     ) -> Result<(), R1CSError> {
@@ -334,6 +339,43 @@ pub(crate) mod test_helpers {
         let verifier = Verifier::new(&pc_gens, &mut verifier_transcript);
 
         C::verify(&witness_commitments, statement, proof, verifier)
+    }
+
+    /// Abstracts over the flow of collaboratively proving and locally verifying
+    /// the statement specified by a given circuit
+    pub fn mulitprover_prove_and_verify<'a, N, S, C>(
+        witness: C::Witness,
+        statement: C::Statement,
+        fabric: SharedFabric<N, S>,
+    ) -> Result<(), MultiproverError>
+    where
+        N: MpcNetwork + Send,
+        S: SharedValueSource<Scalar>,
+        C: MultiProverCircuit<'a, N, S>,
+    {
+        let mut transcript = Transcript::new(TRANSCRIPT_SEED.as_bytes());
+        let pc_gens = PedersenGens::default();
+        let prover = MpcProver::new_with_fabric(fabric.0.clone(), &mut transcript, &pc_gens);
+
+        // Prove the statement
+        let (witness_commitments, proof) =
+            C::prove(witness, statement.clone(), prover, fabric).unwrap();
+
+        // Open the proof and commitments
+        let opened_proof = proof.open()?;
+        let opened_commits =
+            AuthenticatedCompressedRistretto::batch_open_and_authenticate(&witness_commitments)
+                .map_err(MultiproverError::Mpc)?
+                .iter()
+                .map(|comm| comm.value())
+                .collect_vec();
+
+        // Verify the statement with a fresh transcript
+        let mut verifier_transcript = Transcript::new(TRANSCRIPT_SEED.as_bytes());
+        let verifier = Verifier::new(&pc_gens, &mut verifier_transcript);
+
+        C::verify(&opened_commits, statement, opened_proof, verifier)
+            .map_err(MultiproverError::ProverError)
     }
 }
 
