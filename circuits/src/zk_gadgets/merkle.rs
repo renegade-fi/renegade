28b3fefbeb9623b4c7c69966e7df88d94cedca6a
Joey Kraut <joey@otter.cash>
2022-11-14 12:39:45 -0800

    circuits: zk-gadgets: merkle: Add path indices to Merkle proof

--- circuits/src/zk_gadgets/merkle.rs
+++ circuits/src/zk_gadgets/merkle.rs
@@ -10,6 +10,7 @@ use mpc_bulletproof::{
     BulletproofGens,
 };
 use rand_core::OsRng;
+use std::ops::Neg;
 
 use crate::{mpc_gadgets::poseidon::PoseidonSpongeParameters, SingleProverCircuit};
 
@@ -17,7 +18,6 @@ use super::poseidon::PoseidonHashGadget;
 
 /// The single-prover hash gadget, computes the Merkle root of a leaf given a path
 /// of sister nodes
-/// TODO: Add path selection variables
 pub struct PoseidonMerkleHashGadget {}
 
 impl PoseidonMerkleHashGadget {
@@ -27,18 +27,66 @@ impl PoseidonMerkleHashGadget {
         cs: &mut CS,
         leaf_node: Vec<S>,
         opening: Vec<Variable>,
+        opening_indices: Vec<Variable>,
     ) -> Result<LinearCombination, R1CSError>
     where
         S: Into<LinearCombination> + Clone,
         CS: RandomizableConstraintSystem,
     {
+        assert_eq!(opening.len(), opening_indices.len());
+
         // Hash the leaf_node into a field element
         let mut current_hash = Self::leaf_hash(&leaf_node, cs)?;
-        for path_elem in opening.into_iter() {
-            current_hash = Self::hash_internal_nodes(&current_hash, &path_elem.into(), cs)?;
+        for (path_elem, lr_select) in opening.into_iter().zip(opening_indices.into_iter()) {
+            // Select the left and right hand sides based on whether this node in the opening represents the
+            // left or right hand child of its parent
+            let (lhs, rhs) = Self::select_left_right(
+                cs,
+                current_hash.clone(),
+                path_elem.into(),
+                lr_select.into(),
+            );
+            current_hash = Self::hash_internal_nodes(&lhs, &rhs, cs)?;
         }
 
         Ok(current_hash)
+    }
+
+    /// Selects whether to place the current hash on the left or right hand
+    /// side of the 2-1 hash.
+    ///
+    /// This is based on whether the current hash represents the left or right
+    /// child of its parent to be computed
+    fn select_left_right<S, CS>(
+        cs: &mut CS,
+        current_hash: S,
+        sister_node: S,
+        lr_select: S,
+    ) -> (LinearCombination, LinearCombination)
+    where
+        S: Into<LinearCombination> + Clone,
+        CS: RandomizableConstraintSystem,
+    {
+        let current_hash_lc = current_hash.into();
+        let sister_node_lc = sister_node.into();
+        let lr_select_lc = lr_select.into();
+
+        // If lr_select == 0 { current_hash } else { sister_node }
+        //      ==> current_hash * (1 - lr_select) + sister_node * lr_select
+        let (_, _, left_child_term1) = cs.multiply(
+            current_hash_lc.clone(),
+            lr_select_lc.clone().neg() + Scalar::one(),
+        );
+        let (_, _, left_child_term2) = cs.multiply(sister_node_lc.clone(), lr_select_lc);
+
+        let left_child = left_child_term1 + left_child_term2;
+
+        // If lr_select == 0 { sister_node } else { current_hash }
+        // We can avoid an extra multiplication here, because we know the lhs term, the rhs term is
+        // equal to the other term, which can be computed by addition alone
+        //      rhs = a + b - lhs
+        let right_child = current_hash_lc + sister_node_lc - left_child.clone();
+        (left_child, right_child)
     }
 
     /// Compute the root and constrain it to an expected value
@@ -46,13 +94,14 @@ impl PoseidonMerkleHashGadget {
         cs: &mut CS,
         leaf_node: Vec<S>,
         opening: Vec<Variable>,
+        opening_indices: Vec<Variable>,
         expected_root: S,
     ) -> Result<(), R1CSError>
     where
         CS: RandomizableConstraintSystem,
         S: Into<LinearCombination> + Clone,
     {
-        let root = Self::compute_root(cs, leaf_node, opening)?;
+        let root = Self::compute_root(cs, leaf_node, opening, opening_indices)?;
         cs.constrain(expected_root.into() - root);
 
         Ok(())
@@ -94,6 +143,10 @@ pub struct MerkleWitness {
     /// The opening from the leaf node to the root, i.e. the set of sister nodes
     /// that hash together with the input from the leaf to the root
     pub opening: Vec<Scalar>,
+    /// The opening indices from the leaf node to the root, each value is zero or
+    /// one: 0 indicating that the node in the opening at index i is a left hand
+    /// child of its parent, 1 indicating it's a right hand child
+    pub opening_indices: Vec<Scalar>,
     /// The preimage for the leaf i.e. the value that is sponge hashed into the leaf
     pub leaf_data: Vec<Scalar>,
 }
@@ -112,7 +165,7 @@ impl SingleProverCircuit for PoseidonMerkleHashGadget {
     type Statement = MerkleStatement;
     type Witness = MerkleWitness;
 
-    const BP_GENS_CAPACITY: usize = 4096;
+    const BP_GENS_CAPACITY: usize = 8192;
 
     fn prove(
         witness: Self::Witness,
@@ -124,17 +177,23 @@ impl SingleProverCircuit for PoseidonMerkleHashGadget {
         let leaf_data_length = witness.leaf_data.len();
         let opening_len = witness.opening.len();
 
-        let (leaf_comm, leaf_vars): (Vec<CompressedRistretto>, Vec<Variable>) = witness
-            .leaf_data
-            .into_iter()
-            .zip((0..leaf_data_length).map(|_| Scalar::random(&mut rng)))
-            .map(|(val, blinder)| prover.commit(val, blinder))
-            .unzip();
         let (opening_comm, opening_vars): (Vec<CompressedRistretto>, Vec<Variable>) = witness
             .opening
             .into_iter()
             .zip((0..opening_len).map(|_| Scalar::random(&mut rng)))
             .map(|(val, blind)| prover.commit(val, blind))
+            .unzip();
+        let (indices_comm, indices_vars): (Vec<CompressedRistretto>, Vec<Variable>) = witness
+            .opening_indices
+            .into_iter()
+            .zip((0..opening_len).map(|_| Scalar::random(&mut rng)))
+            .map(|(val, blind)| prover.commit(val, blind))
+            .unzip();
+        let (leaf_comm, leaf_vars): (Vec<CompressedRistretto>, Vec<Variable>) = witness
+            .leaf_data
+            .into_iter()
+            .zip((0..leaf_data_length).map(|_| Scalar::random(&mut rng)))
+            .map(|(val, blinder)| prover.commit(val, blinder))
             .unzip();
 
         // Commit to the expected root
@@ -145,6 +204,7 @@ impl SingleProverCircuit for PoseidonMerkleHashGadget {
             &mut prover,
             leaf_vars,
             opening_vars,
+            indices_vars,
             root_var,
         )?;
 
@@ -155,6 +215,7 @@ impl SingleProverCircuit for PoseidonMerkleHashGadget {
         Ok((
             opening_comm
                 .into_iter()
+                .chain(indices_comm.into_iter())
                 .chain(leaf_comm.into_iter())
                 .collect_vec(),
             proof,
@@ -168,11 +229,21 @@ impl SingleProverCircuit for PoseidonMerkleHashGadget {
         mut verifier: Verifier,
     ) -> Result<(), R1CSError> {
         // Commit to the witness
-        let leaf_vars = witness_commitments[statement.tree_height - 1..]
+        // The first <tree-height> vars are the opening
+        let opening_len = statement.tree_height - 1;
+        let opening_vars = witness_commitments[..opening_len]
             .iter()
             .map(|comm| verifier.commit(*comm))
             .collect_vec();
-        let opening_vars = witness_commitments[..statement.tree_height - 1]
+        // The second <tree-height> vars are the opening indices
+        let indices_vars = witness_commitments[opening_len..2 * opening_len]
+            .iter()
+            .map(|comm| verifier.commit(*comm))
+            .collect_vec();
+        assert_eq!(opening_vars.len(), indices_vars.len());
+
+        // All remaining vars are the leaf payload
+        let leaf_vars = witness_commitments[2 * opening_len..]
             .iter()
             .map(|comm| verifier.commit(*comm))
             .collect_vec();
@@ -184,6 +255,7 @@ impl SingleProverCircuit for PoseidonMerkleHashGadget {
             &mut verifier,
             leaf_vars,
             opening_vars,
+            indices_vars,
             root_var,
         )?;
 
@@ -198,11 +270,12 @@ mod merkle_test {
     use ark_crypto_primitives::{
         crh::poseidon::{TwoToOneCRH, CRH},
         merkle_tree::{Config, IdentityDigestConverter},
-        MerkleTree,
+        CRHScheme, MerkleTree,
     };
     use curve25519_dalek::scalar::Scalar;
     use itertools::Itertools;
     use mpc_bulletproof::r1cs_mpc::R1CSError;
+    use rand::{thread_rng, Rng};
     use rand_core::OsRng;
 
     use crate::{
@@ -227,30 +300,56 @@ mod merkle_test {
         type LeafInnerDigestConverter = IdentityDigestConverter<TestField>;
     }
 
+    /// Create a random sequence of field elements
+    fn random_field_elements(n: usize) -> Vec<TestField> {
+        let mut rng = OsRng {};
+        (0..n)
+            .map(|_| scalar_to_prime_field(&Scalar::random(&mut rng)))
+            .collect_vec()
+    }
+
+    /// Get the opening indices from the index, this can be done by bit-decomposing the input to
+    /// determine incrementally which subtree a given index should be a part of.
+    ///
+    /// The tree indices from leaf to root are exactly the LSB decomposition of the scalar
+    fn get_opening_indices(leaf_index: usize, tree_height: usize) -> Vec<Scalar> {
+        if tree_height == 0 {
+            vec![]
+        } else {
+            let mut recursive_result = get_opening_indices(leaf_index >> 1, tree_height - 1);
+            recursive_result.insert(0, Scalar::from((leaf_index % 2) as u64));
+            recursive_result
+        }
+    }
+
     #[test]
+    #[allow(non_upper_case_globals)]
     fn test_against_arkworks() {
         // A random input at the leaf
-        let mut rng = OsRng {};
-        let n = 6;
+        const leaf_size: usize = 6;
+        let num_leaves = 32;
         let tree_height = 10;
-        let leaf_data = (0..n).map(|_| Scalar::random(&mut rng)).collect_vec();
 
-        // Compute the correct root via Arkworks
+        // Build a random Merkle tree via arkworks
         let poseidon_config = PoseidonSpongeParameters::default();
         let arkworks_params = convert_params(&poseidon_config);
-
-        let arkworks_leaf_data = leaf_data.iter().map(scalar_to_prime_field).collect_vec();
 
         let mut merkle_tree =
             MerkleTree::<MerkleConfig>::blank(&arkworks_params, &arkworks_params, tree_height)
                 .unwrap();
 
-        merkle_tree
-            .update(0 /* index */, &arkworks_leaf_data)
-            .unwrap();
+        let mut ark_leaf_data = Vec::with_capacity(num_leaves);
+        for i in 0..num_leaves {
+            let leaf_data: [TestField; leaf_size] =
+                (*random_field_elements(leaf_size)).try_into().unwrap();
+            merkle_tree.update(i, &leaf_data).unwrap();
+            ark_leaf_data.push(leaf_data);
+        }
 
+        // Generate a proof for a random index and prove it in the native gadget
+        let random_index = thread_rng().gen_range(0..num_leaves);
         let expected_root = felt_to_scalar(&merkle_tree.root());
-        let opening = merkle_tree.generate_proof(0 /* index */).unwrap();
+        let opening = merkle_tree.generate_proof(random_index).unwrap();
         let mut opening_scalars = opening
             .auth_path
             .iter()
@@ -258,13 +357,28 @@ mod merkle_test {
             .map(felt_to_scalar)
             .collect_vec();
 
-        // Add a zero to the opening scalar for the next leaf
-        opening_scalars.insert(0, Scalar::zero());
+        // Hash the sister leaf of the given scalar
+        let sister_leaf_data = if random_index % 2 == 0 {
+            ark_leaf_data[random_index + 1]
+        } else {
+            ark_leaf_data[random_index - 1]
+        };
+        let sister_leaf_hash: TestField =
+            CRH::evaluate(&arkworks_params, sister_leaf_data).unwrap();
+
+        opening_scalars.insert(0, felt_to_scalar(&sister_leaf_hash));
+
+        // Convert the leaf data for the given leaf to scalars
+        let leaf_data = ark_leaf_data[random_index]
+            .iter()
+            .map(felt_to_scalar)
+            .collect_vec();
 
         // Prove and verify the statement
         let witness = MerkleWitness {
             leaf_data,
             opening: opening_scalars,
+            opening_indices: get_opening_indices(random_index, opening.auth_path.len() + 1),
         };
 
         let statement = MerkleStatement {
@@ -314,6 +428,9 @@ mod merkle_test {
         let witness = MerkleWitness {
             leaf_data,
             opening: opening_scalars,
+            opening_indices: (0..opening.auth_path.len() + 1)
+                .map(|_| Scalar::zero())
+                .collect_vec(),
         };
 
         let statement = MerkleStatement {
