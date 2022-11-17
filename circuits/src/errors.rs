5817a311cfefd6643409942e1ebcea7b7d1a559a
Joey Kraut <joey@otter.cash>
2022-11-16 16:07:14 -0800

    circuits: errors: Add `ProverError` and `VerifierError`

--- circuits/src/errors.rs
+++ circuits/src/errors.rs
@@ -1,6 +1,8 @@
 //! Groups error types for the circuits crate
 
-use std::fmt::{Display, Formatter, Result};
+use std::fmt::{write, Display, Formatter, Result};
+
+use mpc_bulletproof::r1cs_mpc::R1CSError;
 
 /// Represents an error during the course of an MPC circuit execution
 #[derive(Clone, Debug, PartialEq, Eq)]
@@ -18,6 +20,35 @@ pub enum MpcError {
 }
 
 impl Display for MpcError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+/// Represents an error during the course of proving a statement
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum ProverError {
+    /// An error during the course of a multi-prover execution that results
+    /// from the MPC network itself
+    Mpc(MpcError),
+    /// An error that occurs from an R1CS error directly
+    R1CS(R1CSError),
+}
+
+impl Display for ProverError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+/// Represents an error during proof verification
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum VerifierError {
+    /// An error that occurs as a result of R1CS non-satisfaction
+    R1CS(R1CSError),
+}
+
+impl Display for VerifierError {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
         write!(f, "{:?}", self)
     }
