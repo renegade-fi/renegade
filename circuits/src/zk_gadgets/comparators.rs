141f5029cd616938abc789604b603503b97ae15c
Joey Kraut <joey@otter.cash>
2022-11-28 09:30:55 -0800

    circuits: zk-gadgets: greater than zero: Add multiprover gadget

--- circuits/src/zk_gadgets/comparators.rs
+++ circuits/src/zk_gadgets/comparators.rs
@@ -1,24 +1,29 @@
 //! Groups gadgets for binary comparison operators
 
+use std::marker::PhantomData;
+
 use curve25519_dalek::{ristretto::CompressedRistretto, scalar::Scalar};
 use itertools::Itertools;
 use mpc_bulletproof::{
     r1cs::{LinearCombination, Prover, R1CSProof, RandomizableConstraintSystem, Verifier},
+    r1cs_mpc::{MpcLinearCombination, MpcRandomizableConstraintSystem},
     BulletproofGens,
 };
+use mpc_ristretto::{beaver::SharedValueSource, network::MpcNetwork};
 use rand_core::OsRng;
 
 use crate::{
     errors::{ProverError, VerifierError},
-    mpc_gadgets::bits::scalar_to_bits_le,
+    mpc::SharedFabric,
+    mpc_gadgets::bits::{scalar_to_bits_le, to_bits_le},
     SingleProverCircuit, POSITIVE_SCALAR_MAX_BITS,
 };
 
 /// A gadget that enforces a value of a given bitlength is positive
 #[derive(Clone, Debug)]
-pub struct GreaterThanZeroGadget<const D: usize> {}
+pub struct GreaterThanEqZeroGadget<const D: usize> {}
 
-impl<const D: usize> GreaterThanZeroGadget<D> {
+impl<const D: usize> GreaterThanEqZeroGadget<D> {
     /// Constrain the value to be greater than zero
     pub fn constrain_greater_than_zero<L, CS>(cs: &mut CS, val: L)
     where
@@ -52,14 +57,14 @@ impl<const D: usize> GreaterThanZeroGadget<D> {
 
 /// The witness for the statement that a hidden value is greater than zero
 #[derive(Clone, Debug)]
-pub struct GreaterThanZeroWitness {
+pub struct GreaterThanEqZeroWitness {
     /// The value attested to that must be greater than zero
     val: Scalar,
 }
 
-impl<const D: usize> SingleProverCircuit for GreaterThanZeroGadget<D> {
+impl<const D: usize> SingleProverCircuit for GreaterThanEqZeroGadget<D> {
     type Statement = ();
-    type Witness = GreaterThanZeroWitness;
+    type Witness = GreaterThanEqZeroWitness;
     type WitnessCommitment = CompressedRistretto;
 
     const BP_GENS_CAPACITY: usize = 256;
@@ -103,6 +108,54 @@ impl<const D: usize> SingleProverCircuit for GreaterThanZeroGadget<D> {
     }
 }
 
+/// A multiprover version of the greater than or equal to zero gadget
+pub struct MultiproverGreaterThanEqZeroGadget<
+    'a,
+    const D: usize,
+    N: 'a + MpcNetwork + Send,
+    S: 'a + SharedValueSource<Scalar>,
+> {
+    _phantom: &'a PhantomData<(N, S)>,
+}
+
+impl<'a, const D: usize, N: 'a + MpcNetwork + Send, S: 'a + SharedValueSource<Scalar>>
+    MultiproverGreaterThanEqZeroGadget<'a, D, N, S>
+{
+    /// Constrains the input value to be greater than or equal to zero implicitly
+    /// by bit-decomposing the value and re-composing it thereafter
+    pub fn constrain_greater_than_zero<L, CS>(
+        cs: &mut CS,
+        val: L,
+        fabric: SharedFabric<N, S>,
+    ) -> Result<(), ProverError>
+    where
+        CS: MpcRandomizableConstraintSystem<'a, N, S>,
+        L: Into<MpcLinearCombination<N, S>> + Clone,
+    {
+        // Evaluate the assignment of the value in the underlying constraint system
+        let value_assignment = cs
+            .eval(&val.clone().into())
+            .map_err(ProverError::Collaborative)?;
+        let bits = to_bits_le::<D, N, S>(&value_assignment, fabric)
+            .map_err(ProverError::Mpc)?
+            .into_iter()
+            .map(|bit| cs.allocate(Some(bit)).unwrap())
+            .collect_vec();
+
+        // Constrain the bit decomposition to be correct
+        // This implicitly constrains the value to be greater than zero, i.e. if it can be represented
+        // without the highest bit set, then it is greater than zero. This assumes a two's complement
+        // representation
+        let mut res = MpcLinearCombination::default();
+        for bit in bits.into_iter().rev() {
+            res = res * Scalar::from(2u64) + bit;
+        }
+
+        cs.constrain(res - val.into());
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 mod comparators_test {
     use std::ops::Neg;
@@ -112,7 +165,7 @@ mod comparators_test {
 
     use crate::{errors::VerifierError, test_helpers::bulletproof_prove_and_verify};
 
-    use super::{GreaterThanZeroGadget, GreaterThanZeroWitness};
+    use super::{GreaterThanEqZeroGadget, GreaterThanEqZeroWitness};
 
     /// Test the greater than zero constraint
     #[test]
@@ -121,16 +174,16 @@ mod comparators_test {
 
         // Test first with a positive value
         let value1 = Scalar::from(rng.next_u64());
-        let witness = GreaterThanZeroWitness { val: value1 };
+        let witness = GreaterThanEqZeroWitness { val: value1 };
 
-        bulletproof_prove_and_verify::<GreaterThanZeroGadget<64 /* bitlength */>>(witness, ())
+        bulletproof_prove_and_verify::<GreaterThanEqZeroGadget<64 /* bitlength */>>(witness, ())
             .unwrap();
 
         // Test with a negative value
         let value2 = value1.neg();
-        let witness = GreaterThanZeroWitness { val: value2 };
+        let witness = GreaterThanEqZeroWitness { val: value2 };
         assert!(if let Err(VerifierError::R1CS(_)) =
-            bulletproof_prove_and_verify::<GreaterThanZeroGadget<64 /* bitlength */>>(witness, ())
+            bulletproof_prove_and_verify::<GreaterThanEqZeroGadget<64 /* bitlength */>>(witness, ())
         {
             true
         } else {
