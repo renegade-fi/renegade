746e80105441d0f0a72da0a52452f9d0991a0ac8
Christopher Bender <chrisbender@berkeley.edu>
2022-12-14 18:43:13 -0800

    external-events: Add scaffolding for PriceReport and PriceReporter, and implement Binance websocket subscription and parsing.

---
+++ external-events/src/errors.rs
@@ -0,0 +1,22 @@
+use std::error::Error;
+use std::fmt;
+use std::fmt::Display;
+
+#[derive(Clone, Debug)]
+pub enum ReporterError {
+    ConnectionFailure,
+    InvalidMessage,
+    NoDataReported,
+}
+
+impl Error for ReporterError {}
+impl Display for ReporterError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let display_string = match self {
+            ReporterError::ConnectionFailure => "Failed to connect to the remote websocket",
+            ReporterError::InvalidMessage => "Remote message was incorrectly formatted",
+            ReporterError::NoDataReported => "The PriceReporter has not yet collected any data",
+        };
+        write!(f, "{}", display_string)
+    }
+}
