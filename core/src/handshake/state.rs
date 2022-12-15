cf954adf42d0d6dca1a3b3e426710ebb41d05e00
Joey Kraut <joey@otter.cash>
2022-12-15 15:50:33 -0800

    core: handshake: manager: Track hashes in state updates and messages

--- core/src/handshake/state.rs
+++ core/src/handshake/state.rs
@@ -9,8 +9,16 @@ use std::{
 
 use crate::state::Shared;
 
-use super::{error::HandshakeManagerError, manager::OrderIdentifier};
-use circuits::types::{balance::Balance, fee::Fee, order::Order};
+use super::{
+    error::HandshakeManagerError,
+    r#match::KING,
+    types::{HashOutput, OrderIdentifier},
+};
+use circuits::{
+    types::{balance::Balance, fee::Fee, order::Order},
+    zk_circuits::valid_match_mpc::ValidMatchMpcStatement,
+};
+use crypto::fields::prime_field_to_scalar;
 use uuid::Uuid;
 
 /// Holds state information for all in-flight handshake correspondences
@@ -32,6 +40,7 @@ impl HandshakeStateIndex {
     }
 
     /// Adds a new handshake to the state
+    #[allow(clippy::too_many_arguments)]
     pub fn new_handshake(
         &self,
         request_id: Uuid,
@@ -39,20 +48,35 @@ impl HandshakeStateIndex {
         order: Order,
         balance: Balance,
         fee: Fee,
+        order_hash: HashOutput,
+        balance_hash: HashOutput,
+        fee_hash: HashOutput,
+        randomness_hash: HashOutput,
     ) {
-        // Use a dummy value for the peer order until it is negotiated
-        self.new_handshake_with_peer_order(
+        // Use dummy values until peer negotiates an order, balance, fee tuple
+        self.new_handshake_with_peer_info(
             request_id,
+            // Dummy value for peer_order_id
             Uuid::default(),
             local_order_id,
             order,
             balance,
             fee,
+            order_hash,
+            balance_hash,
+            fee_hash,
+            randomness_hash,
+            // Use dummy values for peer hashes
+            HashOutput::from(0),
+            HashOutput::from(0),
+            HashOutput::from(0),
+            HashOutput::from(0),
         )
     }
 
     /// Adds a new handshake to the state where the peer's order is already known (e.g. the peer initiated the handshake)
-    pub fn new_handshake_with_peer_order(
+    #[allow(clippy::too_many_arguments)]
+    pub fn new_handshake_with_peer_info(
         &self,
         request_id: Uuid,
         peer_order_id: OrderIdentifier,
@@ -60,6 +84,14 @@ impl HandshakeStateIndex {
         order: Order,
         balance: Balance,
         fee: Fee,
+        order_hash: HashOutput,
+        balance_hash: HashOutput,
+        fee_hash: HashOutput,
+        randomness_hash: HashOutput,
+        peer_order_hash: HashOutput,
+        peer_balance_hash: HashOutput,
+        peer_fee_hash: HashOutput,
+        peer_randomness_hash: HashOutput,
     ) {
         let mut locked_state = self.state_map.write().expect("state_map lock poisoned");
         locked_state.insert(
@@ -71,21 +103,42 @@ impl HandshakeStateIndex {
                 order,
                 balance,
                 fee,
+                order_hash,
+                balance_hash,
+                fee_hash,
+                randomness_hash,
+                peer_order_hash,
+                peer_balance_hash,
+                peer_fee_hash,
+                peer_randomness_hash,
             ),
         );
     }
 
     /// Update a request to fill in a peer's order_id that has been decided on
-    pub fn update_peer_order_id(
+    ///
+    /// This is decoupled from the constructor because the peer that initiates
+    /// a handshake will not know the peer's handshake information ahead of time
+    /// when the state is created
+    pub fn update_peer_info(
         &self,
         request_id: &Uuid,
         order_id: OrderIdentifier,
+        order_hash: HashOutput,
+        balance_hash: HashOutput,
+        fee_hash: HashOutput,
+        randomness_hash: HashOutput,
     ) -> Result<(), HandshakeManagerError> {
         let mut locked_state = self.state_map.write().expect("state_map lock poisoned");
         let state_entry = locked_state.get_mut(request_id).ok_or_else(|| {
             HandshakeManagerError::InvalidRequest(format!("request_id {:?}", request_id))
         })?;
+
         state_entry.peer_order_id = order_id;
+        state_entry.peer_order_hash = order_hash;
+        state_entry.peer_balance_hash = balance_hash;
+        state_entry.peer_fee_hash = fee_hash;
+        state_entry.peer_randomness_hash = randomness_hash;
 
         Ok(())
     }
@@ -143,6 +196,22 @@ pub struct HandshakeState {
     pub balance: Balance,
     /// The local peer's fee, paid out to the contract and the executing node
     pub fee: Fee,
+    /// The local peer's order hash
+    pub order_hash: HashOutput,
+    /// The local peer's balance hash
+    pub balance_hash: HashOutput,
+    /// The local peer's fee hash
+    pub fee_hash: HashOutput,
+    /// The local peer's randomness hash
+    pub randomness_hash: HashOutput,
+    /// The local peer's order hash
+    pub peer_order_hash: HashOutput,
+    /// The remote peer's balance hash
+    pub peer_balance_hash: HashOutput,
+    /// The remote peer's fee hash
+    pub peer_fee_hash: HashOutput,
+    /// The remote peer's randomness hash
+    pub peer_randomness_hash: HashOutput,
     /// The current state information of the
     pub state: State,
 }
@@ -168,6 +237,7 @@ pub enum State {
 
 impl HandshakeState {
     /// Create a new handshake in the order negotiation state
+    #[allow(clippy::too_many_arguments)]
     pub fn new(
         request_id: Uuid,
         peer_order_id: OrderIdentifier,
@@ -175,6 +245,14 @@ impl HandshakeState {
         order: Order,
         balance: Balance,
         fee: Fee,
+        order_hash: HashOutput,
+        balance_hash: HashOutput,
+        fee_hash: HashOutput,
+        randomness_hash: HashOutput,
+        peer_order_hash: HashOutput,
+        peer_balance_hash: HashOutput,
+        peer_fee_hash: HashOutput,
+        peer_randomness_hash: HashOutput,
     ) -> Self {
         Self {
             request_id,
@@ -183,6 +261,14 @@ impl HandshakeState {
             order,
             balance,
             fee,
+            order_hash,
+            balance_hash,
+            fee_hash,
+            randomness_hash,
+            peer_order_hash,
+            peer_balance_hash,
+            peer_fee_hash,
+            peer_randomness_hash,
             state: State::OrderNegotiation,
         }
     }
@@ -212,5 +298,57 @@ impl HandshakeState {
     /// Transition the state to Error
     pub fn error(&mut self, err: HandshakeManagerError) {
         self.state = State::Error(err);
+    }
+
+    /// Build a VALID MATCH MPC statement from the state of a given handshake
+    pub fn build_valid_match_statement(&self, party_id: u64) -> ValidMatchMpcStatement {
+        // Reorder the hashes depending on the party_id; the king's order (party_id 0) should
+        // come first in the statement
+        let mut hashes = Vec::new();
+        if party_id == KING {
+            hashes.append(&mut vec![
+                self.order_hash,
+                self.balance_hash,
+                self.fee_hash,
+                self.randomness_hash,
+            ]);
+            hashes.append(&mut vec![
+                self.peer_order_hash,
+                self.peer_balance_hash,
+                self.peer_fee_hash,
+                self.peer_randomness_hash,
+            ]);
+        } else {
+            // Swap order
+            hashes.append(&mut vec![
+                self.peer_order_hash,
+                self.peer_balance_hash,
+                self.peer_fee_hash,
+                self.peer_randomness_hash,
+            ]);
+            hashes.append(&mut vec![
+                self.order_hash,
+                self.balance_hash,
+                self.fee_hash,
+                self.randomness_hash,
+            ]);
+        }
+
+        // The hashes are done natively via Arkworks and must be converted to the Dalek scalar representation
+        let hashes_scalar = hashes
+            .iter()
+            .map(|hash| prime_field_to_scalar(&hash.0))
+            .collect::<Vec<_>>();
+
+        ValidMatchMpcStatement {
+            hash_order1: hashes_scalar[0].to_owned(),
+            hash_balance1: hashes_scalar[1].to_owned(),
+            hash_fee1: hashes_scalar[2].to_owned(),
+            hash_randomness1: hashes_scalar[3].to_owned(),
+            hash_order2: hashes_scalar[4].to_owned(),
+            hash_balance2: hashes_scalar[5].to_owned(),
+            hash_fee2: hashes_scalar[6].to_owned(),
+            hash_randomness2: hashes_scalar[7].to_owned(),
+        }
     }
 }
