b0344e6dd9e8bba880d30e9b53a50c5849390102
Christopher Bender <chrisbender@berkeley.edu>
2022-12-17 18:05:18 -0800

    external-events: Allow for multiple replicated ring buffers for multiple streams of midpoint prices over a single connection.

--- external-events/src/reporters.rs
+++ external-events/src/reporters.rs
@@ -1,12 +1,21 @@
-use ring_channel::{ring_channel, RingReceiver};
+use ring_channel::{ring_channel, RingReceiver, RingSender};
 use stats::median;
-use std::{collections::HashMap, num::NonZeroUsize, thread};
+use std::{
+    collections::HashMap,
+    num::NonZeroUsize,
+    sync::{Arc, Mutex},
+    thread,
+};
 
 use crate::{
     errors::ReporterError,
     exchanges::{Exchange, ExchangeConnection},
     tokens::Token,
 };
+
+fn new_ring_channel<T>() -> (RingSender<T>, RingReceiver<T>) {
+    ring_channel::<T>(NonZeroUsize::new(1).unwrap())
+}
 
 /// The PriceReport is the universal format for price feeds from all external exchanges.
 #[derive(Clone, Copy, Debug, Default)]
@@ -20,16 +29,19 @@ pub struct PriceReport {
 }
 
 /// The PriceReporter is responsible for opening websocket connection(s) to the specified
-/// exchange(s), translating messages into PriceReport's, and reporting recent prices.
+/// exchange(s), translating individual PriceReport's into aggregate median PriceReport's, and
+/// opening and closing channels for end-consumers to listen to the price feeds.
 #[derive(Clone, Debug)]
 pub struct PriceReporter {
     pub quote_token: Token,
     pub base_token: Token,
     pub exchanges: Vec<Exchange>,
-    /// Ring buffer of the median of all PriceReports.
-    median_price_report_receiver: RingReceiver<PriceReport>,
-    /// The current median PriceReport.
-    median_price_report_latest: PriceReport,
+    /// Thread-safe vector of all senders for the median PriceReport stream. This allows for a
+    /// flexible number of data subscribers (by calling PriceReporter::create_new_receiver, which
+    /// appends a new sender to this vector).
+    median_price_report_senders: Arc<Mutex<Vec<RingSender<PriceReport>>>>,
+    /// The latest median PriceReport.
+    median_price_report_latest: Arc<Mutex<PriceReport>>,
 }
 impl PriceReporter {
     /// Given a token pair and exchange identifier, create a new PriceReporter.
@@ -38,17 +50,18 @@ impl PriceReporter {
         base_token: Token,
         exchanges: Option<Vec<Exchange>>,
     ) -> Result<Self, ReporterError> {
-        // If the given exchanges were None, then use all exchanges by default.
+        // If the given exchanges are None, then use all exchanges by default.
         let exchanges = match exchanges {
             Some(exchanges) => exchanges,
             None => vec![Exchange::Binance, Exchange::Coinbase, Exchange::Kraken],
         };
 
         // Create the RingBuffer for the aggregate stream of all updates.
-        let (mut all_price_reports_sender, mut all_price_reports_receiver) =
-            ring_channel::<(PriceReport, Exchange)>(NonZeroUsize::new(1).unwrap());
+        let (all_price_reports_sender, mut all_price_reports_receiver) =
+            new_ring_channel::<(PriceReport, Exchange)>();
 
-        // Pipe all individual price_report_receiver into the aggregate all_price_reports_sender
+        // Connect to all the exchanges, and pipe the price report stream from each connection into
+        // the aggregate ring buffer.
         for exchange in exchanges.to_vec() {
             let mut price_report_receiver =
                 ExchangeConnection::new(quote_token, base_token, exchange).unwrap();
@@ -61,12 +74,22 @@ impl PriceReporter {
             });
         }
 
-        // Create the RingBuffer for the median price (to be consumed by end-users).
-        let (mut median_price_report_sender, median_price_report_receiver) =
-            ring_channel::<PriceReport>(NonZeroUsize::new(1).unwrap());
+        // Create the thread-safe vector of senders and receivers.
+        let (sender, mut receiver) = new_ring_channel::<PriceReport>();
+        let median_price_report_senders = Arc::new(Mutex::new(vec![sender]));
+        let median_price_report_senders_worker = median_price_report_senders.clone();
 
-        // Process the aggregate stream and send the aggregate median to the
-        // median_price_report_sender.
+        // The first hard-coded receiver is responsible for updating the
+        // median_price_report_latest.
+        let median_price_report_latest = Arc::new(Mutex::new(PriceReport::default()));
+        let median_price_report_latest_worker = median_price_report_latest.clone();
+        thread::spawn(move || loop {
+            let price_report = receiver.recv().unwrap();
+            *median_price_report_latest_worker.lock().unwrap() = price_report;
+        });
+
+        // Process the aggregate stream and send the aggregate median to each sender in
+        // median_price_report_channels.
         thread::spawn(move || {
             let mut latest_price_reports = HashMap::<Exchange, PriceReport>::new();
             loop {
@@ -98,13 +121,18 @@ impl PriceReporter {
                     Some(median_reported_timestamp) => Some(median_reported_timestamp as u128),
                     None => None,
                 };
-                median_price_report_sender
-                    .send(PriceReport {
-                        midpoint_price: median_midpoint_price as f32,
-                        local_timestamp: median_local_timestamp as u128,
-                        reported_timestamp: median_reported_timestamp,
-                    })
-                    .unwrap();
+                let median_price_report = PriceReport {
+                    midpoint_price: median_midpoint_price as f32,
+                    local_timestamp: median_local_timestamp as u128,
+                    reported_timestamp: median_reported_timestamp,
+                };
+                for sender in median_price_report_senders_worker
+                    .lock()
+                    .unwrap()
+                    .iter_mut()
+                {
+                    sender.send(median_price_report).unwrap();
+                }
             }
         });
 
@@ -112,18 +140,24 @@ impl PriceReporter {
             quote_token,
             base_token,
             exchanges,
-            median_price_report_receiver,
-            median_price_report_latest: PriceReport::default(),
+            median_price_report_senders,
+            median_price_report_latest,
         })
     }
 
-    /// Nonblocking report of the latest median price. Tries to read from the ring buffer; if a new
-    /// message exists, update the current price_report.
-    pub fn get_current_report(&mut self) -> Result<PriceReport, ReporterError> {
-        let median_price_report_recv = self.median_price_report_receiver.try_recv();
-        if median_price_report_recv.is_ok() {
-            self.median_price_report_latest = median_price_report_recv.unwrap();
-        }
-        Ok(self.median_price_report_latest)
+    /// Creates a new ring buffer to report median prices and returns the receiver for consumption
+    /// by the callee.
+    pub fn create_new_receiver(&self) -> RingReceiver<PriceReport> {
+        let (sender, receiver) = new_ring_channel::<PriceReport>();
+        self.median_price_report_senders
+            .lock()
+            .unwrap()
+            .push(sender);
+        receiver
+    }
+
+    /// Nonblocking report of the latest median price.
+    pub fn peek(&self) -> Result<PriceReport, ReporterError> {
+        Ok(*self.median_price_report_latest.lock().unwrap())
     }
 }
