12ba2196eba19c88c1ef38e3da6a54dbf527ea8c
Christopher Bender <chrisbender@berkeley.edu>
2022-12-19 17:11:18 -0800

    external-events: Add Uniswap v3 as a reporter.

--- external-events/src/exchanges.rs
+++ external-events/src/exchanges.rs
@@ -1,16 +1,25 @@
 use chrono::DateTime;
+use core::time::Duration;
+use futures::{executor::block_on, StreamExt};
 use hmac_sha256::HMAC;
 use ring_channel::{ring_channel, RingReceiver, RingSender};
 use serde_json::{self, json, Value};
-use std::{collections::HashMap, num::NonZeroUsize};
 use std::{
+    collections::HashMap,
     env,
     net::TcpStream,
+    num::NonZeroUsize,
+    str::FromStr,
     thread,
     time::{SystemTime, UNIX_EPOCH},
 };
 use tungstenite::{connect, stream::MaybeTlsStream, Message, WebSocket as WebSocketGeneric};
 use url::Url;
+use web3::{
+    self, ethabi,
+    types::{BlockId, BlockNumber, H160, U256},
+    Web3,
+};
 
 use crate::{errors::ReporterError, reporters::PriceReport, tokens::Token};
 
@@ -32,19 +41,13 @@ pub enum Exchange {
     Coinbase,
     Kraken,
     Okx,
-    // Kucoin,
-    // Huobi,
-    // Bybit,
-    // Uniswap,
+    UniswapV3,
 }
 
 /// A connection to an `Exchange`. Note that creating an `ExchangeConnection` via
 /// `ExchangeConnection::new(exchange: Exchange)` only returns a ring buffer channel receiver; the
 /// ExchangeConnection is never directly accessed, and all data is reported only via this receiver.
 pub struct ExchangeConnection {
-    quote_token: Token,
-    base_token: Token,
-    exchange: Exchange,
     binance_handler: Option<BinanceHandler>,
     coinbase_handler: Option<CoinbaseHandler>,
     kraken_handler: Option<KrakenHandler>,
@@ -60,12 +63,20 @@ impl ExchangeConnection {
         let (mut price_report_sender, price_report_receiver) =
             ring_channel::<PriceReport>(NonZeroUsize::new(1).unwrap());
 
+        // UniswapV3 logic is slightly different, as we use the web3 API wrapper for convenience,
+        // rather than interacting directly over websockets.
+        if exchange == Exchange::UniswapV3 {
+            UniswapV3Handler::start_price_stream(price_report_sender);
+            return Ok(price_report_receiver);
+        }
+
         // Retrieve the websocket URL and connect to it.
         let wss_url = match exchange {
             Exchange::Binance => BinanceHandler::WSS_URL,
             Exchange::Coinbase => CoinbaseHandler::WSS_URL,
             Exchange::Kraken => KrakenHandler::WSS_URL,
             Exchange::Okx => OkxHandler::WSS_URL,
+            _ => unreachable!(),
         };
         let url = Url::parse(&wss_url).unwrap();
         let (mut socket, _response) = connect(url).or(Err(ReporterError::ConnectionFailure))?;
@@ -76,46 +87,36 @@ impl ExchangeConnection {
             Exchange::Coinbase => CoinbaseHandler::websocket_subscribe(&mut socket)?,
             Exchange::Kraken => KrakenHandler::websocket_subscribe(&mut socket)?,
             Exchange::Okx => OkxHandler::websocket_subscribe(&mut socket)?,
+            _ => unreachable!(),
         }
 
         // Get initial ExchangeHandler state and include in a new ExchangeConnection.
         let mut exchange_connection = match exchange {
             Exchange::Binance => ExchangeConnection {
-                quote_token,
-                base_token,
-                exchange,
                 binance_handler: Some(BinanceHandler::new()),
                 coinbase_handler: None,
                 kraken_handler: None,
                 okx_handler: None,
             },
             Exchange::Coinbase => ExchangeConnection {
-                quote_token,
-                base_token,
-                exchange,
                 binance_handler: None,
                 coinbase_handler: Some(CoinbaseHandler::new()),
                 kraken_handler: None,
                 okx_handler: None,
             },
             Exchange::Kraken => ExchangeConnection {
-                quote_token,
-                base_token,
-                exchange,
                 binance_handler: None,
                 coinbase_handler: None,
                 kraken_handler: Some(KrakenHandler::new()),
                 okx_handler: None,
             },
             Exchange::Okx => ExchangeConnection {
-                quote_token,
-                base_token,
-                exchange,
                 binance_handler: None,
                 coinbase_handler: None,
                 kraken_handler: None,
                 okx_handler: Some(OkxHandler::new()),
             },
+            _ => unreachable!(),
         };
 
         // Start listening for inbound messages.
@@ -145,7 +146,7 @@ impl ExchangeConnection {
             } else if let Some(okx_handler) = &mut self.okx_handler {
                 okx_handler.handle_exchange_message(message_json)
             } else {
-                panic!("Unreachable.");
+                unreachable!();
             }
         };
 
@@ -156,7 +157,7 @@ impl ExchangeConnection {
     }
 }
 
-trait ExchangeHandlerApi {
+trait CentralizedExchangeHandler {
     const WSS_URL: &'static str;
     fn new() -> Self;
     /// Send any initial subscription messages to the websocket after it has been created.
@@ -168,7 +169,7 @@ trait ExchangeHandlerApi {
 
 #[derive(Clone, Debug)]
 struct BinanceHandler;
-impl ExchangeHandlerApi for BinanceHandler {
+impl CentralizedExchangeHandler for BinanceHandler {
     const WSS_URL: &'static str = "wss://stream.binance.com:443/ws/ethbusd@bookTicker";
 
     fn new() -> Self {
@@ -209,7 +210,7 @@ struct CoinbaseHandler {
     order_book_bids: HashMap<String, f32>,
     order_book_offers: HashMap<String, f32>,
 }
-impl ExchangeHandlerApi for CoinbaseHandler {
+impl CentralizedExchangeHandler for CoinbaseHandler {
     const WSS_URL: &'static str = "wss://advanced-trade-ws.coinbase.com";
 
     fn new() -> Self {
@@ -320,7 +321,7 @@ impl ExchangeHandlerApi for CoinbaseHandler {
 
 #[derive(Clone, Debug)]
 struct KrakenHandler;
-impl ExchangeHandlerApi for KrakenHandler {
+impl CentralizedExchangeHandler for KrakenHandler {
     const WSS_URL: &'static str = "wss://ws.kraken.com";
 
     fn new() -> Self {
@@ -372,7 +373,7 @@ impl ExchangeHandlerApi for KrakenHandler {
 
 #[derive(Clone, Debug)]
 struct OkxHandler;
-impl ExchangeHandlerApi for OkxHandler {
+impl CentralizedExchangeHandler for OkxHandler {
     const WSS_URL: &'static str = "wss://ws.okx.com:8443/ws/v5/public";
 
     fn new() -> Self {
@@ -417,6 +418,115 @@ impl ExchangeHandlerApi for OkxHandler {
         Some(PriceReport {
             midpoint_price: (best_bid + best_offer) / 2.0,
             reported_timestamp: Some((reported_timestamp_seconds * 1000.0) as u128),
+            local_timestamp: Default::default(),
+        })
+    }
+}
+
+#[derive(Clone, Debug)]
+struct UniswapV3Handler;
+impl UniswapV3Handler {
+    const WSS_URL: &'static str = "wss://mainnet.infura.io/ws/v3/68c04ec6f9ce42c5becbed52a464ef81";
+    const ETH_USDC_ADDR: &'static str = "0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640";
+    const BASE_DECIMALS: u8 = 18; /* i.e. WETH */
+    const QUOTE_DECIMALS: u8 = 6; /* i.e. USDC */
+
+    pub fn start_price_stream(mut sender: RingSender<PriceReport>) {
+        let transport = block_on(web3::transports::WebSocket::new(Self::WSS_URL)).unwrap();
+        let web3_connection = Web3::new(transport);
+        let swap_event_abi = ethabi::Event {
+            name: String::from("Swap"),
+            inputs: vec![
+                ethabi::EventParam {
+                    name: String::from("sender"),
+                    kind: ethabi::param_type::ParamType::Address,
+                    indexed: true,
+                },
+                ethabi::EventParam {
+                    name: String::from("recipient"),
+                    kind: ethabi::param_type::ParamType::Address,
+                    indexed: true,
+                },
+                ethabi::EventParam {
+                    name: String::from("amount0"),
+                    kind: ethabi::param_type::ParamType::Int(256),
+                    indexed: false,
+                },
+                ethabi::EventParam {
+                    name: String::from("amount1"),
+                    kind: ethabi::param_type::ParamType::Int(256),
+                    indexed: false,
+                },
+                ethabi::EventParam {
+                    name: String::from("sqrtPriceX96"),
+                    kind: ethabi::param_type::ParamType::Uint(160),
+                    indexed: false,
+                },
+                ethabi::EventParam {
+                    name: String::from("liquidity"),
+                    kind: ethabi::param_type::ParamType::Uint(128),
+                    indexed: false,
+                },
+                ethabi::EventParam {
+                    name: String::from("tick"),
+                    kind: ethabi::param_type::ParamType::Int(24),
+                    indexed: false,
+                },
+            ],
+            anonymous: false,
+        };
+        let swap_topic_filter = swap_event_abi
+            .filter(ethabi::RawTopicFilter::default())
+            .unwrap();
+        let swap_filter = web3::types::FilterBuilder::default()
+            .address(vec![H160::from_str(Self::ETH_USDC_ADDR).unwrap()])
+            .topic_filter(swap_topic_filter)
+            .build();
+        let swap_filter =
+            block_on(web3_connection.eth_filter().create_logs_filter(swap_filter)).unwrap();
+
+        thread::spawn(move || {
+            let swap_stream = swap_filter.stream(Duration::new(1, 0));
+            futures::pin_mut!(swap_stream);
+            loop {
+                let swap = block_on(swap_stream.next()).unwrap().unwrap();
+                let block_id = BlockId::Number(BlockNumber::Number(swap.block_number.unwrap()));
+                let block_timestamp = block_on(web3_connection.eth().block(block_id))
+                    .unwrap()
+                    .unwrap()
+                    .timestamp;
+                let swap = swap_event_abi
+                    .parse_log(ethabi::RawLog {
+                        topics: swap.topics.clone(),
+                        data: swap.data.clone().0,
+                    })
+                    .unwrap();
+                let mut price_report = Self::handle_event(swap);
+                if let Some(mut price_report) = price_report {
+                    price_report.local_timestamp = get_current_time();
+                    price_report.reported_timestamp = Some(block_timestamp.as_u128());
+                    sender.send(price_report).unwrap();
+                }
+            }
+        });
+    }
+
+    fn handle_event(swap: ethabi::Log) -> Option<PriceReport> {
+        // Extract the `sqrtPriceX96` and convert it to the marginal price of the Uniswapv3 pool,
+        // as per: https://docs.uniswap.org/sdk/v3/guides/fetching-prices#understanding-sqrtprice
+        let sqrt_price_x96 = &swap.params[4].value;
+        let sqrt_price_x96 = match sqrt_price_x96 {
+            ethabi::Token::Uint(sqrt_price_x96) => sqrt_price_x96,
+            _ => unreachable!(),
+        };
+        let price_numerator = U256::from(10).pow(U256::from(Self::BASE_DECIMALS))
+            * U256::from(2).pow(U256::from(192));
+        let price_denominator = U256::from(sqrt_price_x96).pow(U256::from(2));
+        let price = price_numerator / price_denominator;
+        let price = price.as_u32() as f32 / 10_f32.powf(Self::QUOTE_DECIMALS.into());
+        Some(PriceReport {
+            midpoint_price: price,
+            reported_timestamp: None,
             local_timestamp: Default::default(),
         })
     }
