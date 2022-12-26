9b076fe8d0ba2de9c99e30f889d6ae22077a823d
Christopher Bender <chrisbender@berkeley.edu>
2022-12-25 21:48:40 -0600

    external-events: Dynamically determine the reference UniswapV3 pool fee tier (either 1bp, 5bp, 30bp, or 100bp) by querying for the highest TVL.

--- external-events/src/exchanges/handlers_decentralized.rs
+++ external-events/src/exchanges/handlers_decentralized.rs
@@ -25,6 +25,7 @@ impl UniswapV3Handler {
     const FACTORY_ADDRESS: &str = "1f98431c8ad98523631ae4a59f267346ea31f984";
     const POOL_INIT_CODE_HASH: &str =
         "e34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54";
+    const ERC20_ABI: &str = r#"[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}]"#;
 
     pub fn start_price_stream(
         base_token: Token,
@@ -38,7 +39,9 @@ impl UniswapV3Handler {
         let web3_connection = Arc::new(Mutex::new(web3_connection));
 
         // Derive the Uniswap pool address from this Token pair.
-        let (pool_address, is_flipped) = Self::get_pool_address(base_token, quote_token).unwrap();
+        let (pool_address, is_flipped) =
+            Self::get_pool_address(base_token, quote_token, web3_connection.clone()).unwrap();
+        println!("pool_add = {:?}", pool_address);
 
         // Create a filter for Uniswap `Swap` events on this pool.
         let swap_event_abi = ethabi::Event {
@@ -101,6 +104,7 @@ impl UniswapV3Handler {
 
         // Since it may be a while until we receive our first Swap event, we send the most recent
         // historic Swap as the current price.
+        // TODO: Is there a better way to find the most recent Swap?
         let swap_filter_builder = swap_filter_builder
             .from_block(web3::types::BlockNumber::Number(current_block - 10000))
             .to_block(web3::types::BlockNumber::Latest);
@@ -207,7 +211,11 @@ impl UniswapV3Handler {
     /// TVL among all fee tiers (1bp, 5bp, 30bp, 100bp). In addition, we return a boolean
     /// is_flipped that reflects whether the assets are flipped (i.e., quote per base) in the
     /// Uniswap pool.
-    fn get_pool_address(base_token: Token, quote_token: Token) -> Option<(H160, bool)> {
+    fn get_pool_address(
+        base_token: Token,
+        quote_token: Token,
+        web3_connection: Arc<Mutex<Web3<web3::transports::WebSocket>>>,
+    ) -> Option<(H160, bool)> {
         let base_token_addr = H160::from_str(base_token.get_addr()).unwrap();
         let quote_token_addr = H160::from_str(quote_token.get_addr()).unwrap();
         let is_flipped = base_token_addr > quote_token_addr;
@@ -216,31 +224,74 @@ impl UniswapV3Handler {
         } else {
             (base_token_addr, quote_token_addr)
         };
-        let mut fee = [0_u8; 32];
-        // Fee tiers;
+        // Derive all pool addresses from the following fee tiers:
         // HIGH = 10000
         // MEDIUM = 3000
         // LOW = 500
         // LOWEST = 100
-        // TODO: Dynamically choose the fee tier?
-        fee[32 - 4..].clone_from_slice(&3000_u32.to_be_bytes());
+        let pool_addresses = [10_000_u32, 3000_u32, 500_u32, 100_u32].map(|fee_amt| {
+            let mut fee = [0_u8; 32];
+            fee[32 - 4..].clone_from_slice(&fee_amt.to_be_bytes());
+            let pool_address = create2::calc_addr_with_hash(
+                hex::decode(Self::FACTORY_ADDRESS).unwrap()[..20]
+                    .try_into()
+                    .unwrap(),
+                &keccak256(
+                    &[
+                        H256::from(first_token).as_bytes(),
+                        H256::from(second_token).as_bytes(),
+                        &fee,
+                    ]
+                    .concat()[..],
+                ),
+                hex::decode(Self::POOL_INIT_CODE_HASH).unwrap()[..32]
+                    .try_into()
+                    .unwrap(),
+            );
+            H160::from(pool_address)
+        });
 
-        let pool_address = create2::calc_addr_with_hash(
-            hex::decode(Self::FACTORY_ADDRESS).unwrap()[..20]
-                .try_into()
-                .unwrap(),
-            &keccak256(
-                &[
-                    H256::from(first_token).as_bytes(),
-                    H256::from(second_token).as_bytes(),
-                    &fee,
-                ]
-                .concat()[..],
-            ),
-            hex::decode(Self::POOL_INIT_CODE_HASH).unwrap()[..32]
-                .try_into()
-                .unwrap(),
-        );
-        Some((H160::from(pool_address), is_flipped))
+        // Fetch the base balance from each pool address.
+        let erc20_contract = ethabi::Contract::load(Self::ERC20_ABI.as_bytes()).unwrap();
+        let base_balances = pool_addresses.map(|pool_address| {
+            let mut base_balance_call_request = web3::types::CallRequest::default();
+            base_balance_call_request.to = Some(web3::types::Address::from(base_token_addr));
+            base_balance_call_request.data = Some(web3::types::Bytes(
+                erc20_contract
+                    .function("balanceOf")
+                    .unwrap()
+                    .encode_input(&[ethabi::token::Token::Address(pool_address)])
+                    .unwrap(),
+            ));
+            let base_balance = block_on(
+                web3_connection
+                    .lock()
+                    .unwrap()
+                    .eth()
+                    .call(base_balance_call_request, None),
+            )
+            .unwrap()
+            .0;
+            assert!(
+                base_balance.len() == 32,
+                "base_balance.len() = {}, expected 32.",
+                base_balance.len()
+            );
+            U256::from(&base_balance[..32])
+        });
+
+        // Given the derived pool addresses and base balances, pick the pool address with the
+        // highest base balance.
+        println!("pool_addresses = {:?}", pool_addresses);
+        println!("base_balances = {:?}", base_balances);
+        let mut max_base_balance = U256::zero();
+        let mut max_pool_idx: usize = 0;
+        for i in 0..4 {
+            if base_balances[i] > max_base_balance {
+                max_base_balance = base_balances[i];
+                max_pool_idx = i;
+            }
+        }
+        Some((H160::from(pool_addresses[max_pool_idx]), is_flipped))
     }
 }
