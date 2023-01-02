dd270ab6dca732114b68d01cab5f3496cf4e0ad9
Joey Kraut <joey@renegade.fi>
2023-01-02 15:39:59 -0800

    circuits: zk-circuits: valid-wallet-create: Add code skeleton
    This adds no constraints, but adds the statement and witness types,
    as well as the commitment and proving logic.

--- circuits/src/zk_circuits/mod.rs
+++ circuits/src/zk_circuits/mod.rs
@@ -1,3 +1,4 @@
 //! Groups circuitry for full zero knowledge circuits that we are interested
 //! in proving knowledge of witness for throughout the network
 pub mod valid_match_mpc;
+pub mod valid_wallet_create;
