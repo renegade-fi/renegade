630cff73055af95614b148d9a15171343116545f
Joey Kraut <joey@renegade.fi>
2023-01-16 21:39:47 -0800

    circuit-macros: Add tests for associated and non-associated targets
    This tests both the case in which the tracer is enabled and the case
    in which the tracer is disabled.

--- circuit-macros/tests/test_macros.rs
+++ circuit-macros/tests/test_macros.rs
@@ -1,13 +1,20 @@
 use circuit_macros::circuit_trace;
+use curve25519_dalek::scalar::Scalar;
 use lazy_static::lazy_static;
 use merlin::Transcript;
-use mpc_bulletproof::{r1cs::Prover, PedersenGens};
+use mpc_bulletproof::{
+    r1cs::{ConstraintSystem, Prover, Variable},
+    PedersenGens,
+};
+use rand_core::OsRng;
 use std::{
     collections::{hash_map::Entry, HashMap},
     sync::Mutex,
+    thread,
+    time::Duration,
 };
 
-/// A type used for scop
+/// A type used for scoping trace metrics
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct Scope {
     pub path: Vec<String>,
@@ -17,6 +24,10 @@ impl Scope {
     /// Build a new scope
     pub fn new() -> Self {
         Self { path: vec![] }
+    }
+
+    pub fn from_path(path: Vec<String>) -> Self {
+        Self { path }
     }
 
     /// Append a value to the scope
@@ -84,6 +95,11 @@ impl MetricsCapture {
             .unwrap()
             .add_metric(metric_name, value);
     }
+
+    /// Get the metric for the given scope and metric name
+    pub fn get_metric(&mut self, scope: Scope, metric_name: String) -> Option<u64> {
+        self.metrics.get(&scope)?.data.get(&metric_name).cloned()
+    }
 }
 
 lazy_static! {
@@ -91,24 +107,160 @@ lazy_static! {
     static ref CURR_SCOPE: Mutex<Scope> = Mutex::new(Scope::new());
 }
 
-fn helper(x: u64) -> u64 {
-    x + 1
+/// A dummy gadget whose constraint generation is done through an associated function, used to
+/// test the trace macro on an associated function
+pub struct Gadget {}
+impl Gadget {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    #[circuit_trace(n_constraints, n_multipliers, latency)]
+    pub fn apply_constraints(cs: &mut Prover) {
+        // Apply dummy constraints
+        let mut rng = OsRng {};
+        let (_, var) = cs.commit(Scalar::one(), Scalar::random(&mut rng));
+        let (_, _, mul_out) = cs.multiply(var.into(), Variable::Zero().into());
+        cs.constrain(mul_out.into());
+
+        // Add some latency to test the latency metric
+        thread::sleep(Duration::from_millis(100));
+    }
 }
 
-/// A dummy target for the macro
-#[circuit_trace(n_constraints, n_multipliers, latency)]
+/// A dummy macro target that is not an associated function of any abstract gadget, used to
+/// test the non-associated macro arg
+#[circuit_trace(non_associated, n_constraints, n_multipliers, latency)]
 #[allow(unused)]
-fn dummy(x: u64, cs: &mut Prover) -> u64 {
-    let new_x = helper(x);
-    new_x
+fn non_associated_gadget(cs: &mut Prover) {
+    // Apply dummy constraints
+    let mut rng = OsRng {};
+    let (_, var) = cs.commit(Scalar::one(), Scalar::random(&mut rng));
+    let (_, _, mul_out) = cs.multiply(var.into(), Variable::Zero().into());
+    cs.constrain(mul_out.into());
+
+    // Add some latency to test the latency metric
+    thread::sleep(Duration::from_millis(100));
 }
 
+/// Tests the tracer macro on an associated function when the tracer is enabled
+#[cfg(feature = "bench")]
 #[test]
-fn test_macro() {
+fn test_macro_associated() {
+    // Build a dummy constraint system and apply a few constraints
     let mut prover_transcript = Transcript::new("test".as_bytes());
     let pc_gens = PedersenGens::default();
     let mut prover = Prover::new(&pc_gens, &mut prover_transcript);
 
-    dummy(1, &mut prover);
-    println!("SCOPED METRICS: {:?}", SCOPED_METRICS.lock().unwrap());
+    Gadget::apply_constraints(&mut prover);
+
+    // Read the values from the tracer, this test is gated behind the same feature flag
+    // as the tracer, so metrics should have been recorded
+    let gadget_scope = Scope::from_path(vec!["apply_constraints".to_string()]);
+    let mut locked_metrics = SCOPED_METRICS.lock().unwrap();
+
+    let latency_metric = locked_metrics
+        .get_metric(gadget_scope.clone(), "latency".to_string())
+        .unwrap();
+    assert!(latency_metric > 100);
+
+    let n_constraints_metric = locked_metrics
+        .get_metric(gadget_scope.clone(), "n_constraints".to_string())
+        .unwrap();
+    assert_eq!(3, n_constraints_metric);
+
+    let n_multipliers_metric = locked_metrics
+        .get_metric(gadget_scope, "n_multipliers".to_string())
+        .unwrap();
+    assert_eq!(1, n_multipliers_metric);
+}
+
+/// Tests the tracer macro on a non-associated function when the tracer is enabled
+#[cfg(feature = "bench")]
+#[test]
+fn test_macro_non_associated() {
+    // Build a dummy constraint system and apply a few constraints
+    let mut prover_transcript = Transcript::new("test".as_bytes());
+    let pc_gens = PedersenGens::default();
+    let mut prover = Prover::new(&pc_gens, &mut prover_transcript);
+
+    non_associated_gadget(&mut prover);
+
+    // Read the values from the tracer, this test is gated behind the same feature flag
+    // as the tracer, so metrics should have been recorded
+    let gadget_scope = Scope::from_path(vec!["non_associated_gadget".to_string()]);
+    let mut locked_metrics = SCOPED_METRICS.lock().unwrap();
+
+    let latency_metric = locked_metrics
+        .get_metric(gadget_scope.clone(), "latency".to_string())
+        .unwrap();
+    assert!(latency_metric > 100);
+
+    let n_constraints_metric = locked_metrics
+        .get_metric(gadget_scope.clone(), "n_constraints".to_string())
+        .unwrap();
+    assert_eq!(3, n_constraints_metric);
+
+    let n_multipliers_metric = locked_metrics
+        .get_metric(gadget_scope, "n_multipliers".to_string())
+        .unwrap();
+    assert_eq!(1, n_multipliers_metric);
+}
+
+/// Tests the tracer macro on an associated function when the tracer is disabled
+#[cfg(not(feature = "bench"))]
+#[test]
+fn test_macro_associated() {
+    // Build a dummy constraint system and apply a few constraints
+    let mut prover_transcript = Transcript::new("test".as_bytes());
+    let pc_gens = PedersenGens::default();
+    let mut prover = Prover::new(&pc_gens, &mut prover_transcript);
+
+    Gadget::apply_constraints(&mut prover);
+
+    // Read the values from the tracer, this test is gated behind the same feature flag
+    // as the tracer, so metrics should have been recorded
+    let gadget_scope = Scope::from_path(vec!["apply_constraints".to_string()]);
+    let mut locked_metrics = SCOPED_METRICS.lock().unwrap();
+
+    assert!(locked_metrics
+        .get_metric(gadget_scope.clone(), "latency".to_string())
+        .is_none());
+
+    assert!(locked_metrics
+        .get_metric(gadget_scope.clone(), "n_constraints".to_string())
+        .is_none());
+
+    assert!(locked_metrics
+        .get_metric(gadget_scope, "n_multipliers".to_string())
+        .is_none());
+}
+
+/// Tests the tracer macro on a non-associated function when the tracer is disabled
+#[cfg(not(feature = "bench"))]
+#[test]
+fn test_macro_non_associated() {
+    // Build a dummy constraint system and apply a few constraints
+    let mut prover_transcript = Transcript::new("test".as_bytes());
+    let pc_gens = PedersenGens::default();
+    let mut prover = Prover::new(&pc_gens, &mut prover_transcript);
+
+    non_associated_gadget(&mut prover);
+
+    // Read the values from the tracer, this test is gated behind the same feature flag
+    // as the tracer, so metrics should have been recorded
+    let gadget_scope = Scope::from_path(vec!["non_associated_gadget".to_string()]);
+    let mut locked_metrics = SCOPED_METRICS.lock().unwrap();
+
+    assert!(locked_metrics
+        .get_metric(gadget_scope.clone(), "latency".to_string())
+        .is_none());
+
+    assert!(locked_metrics
+        .get_metric(gadget_scope.clone(), "n_constraints".to_string())
+        .is_none());
+
+    assert!(locked_metrics
+        .get_metric(gadget_scope, "n_multipliers".to_string())
+        .is_none());
 }
