e33594cbce453fcaf42eb94c469923ae3002bc29
Joey Kraut <joey@renegade.fi>
2023-02-07 23:23:12 -0800

    core: proof-generation: Add job for `ValidWalletCreate`

--- core/src/proof_generation/proof_manager.rs
+++ core/src/proof_generation/proof_manager.rs
@@ -2,12 +2,28 @@
 //! happen to the state. It provides an abstracted messaging interface for other
 //! workers to submit proof requests to.
 
-use std::{sync::Arc, thread::JoinHandle};
+use std::{convert::TryInto, sync::Arc, thread::JoinHandle};
 
+use circuits::{
+    native_helpers::compute_wallet_commitment,
+    singleprover_prove,
+    types::{balance::Balance, fee::Fee, keychain::KeyChain, order::Order},
+    zk_circuits::valid_wallet_create::{
+        ValidWalletCreate, ValidWalletCreateStatement, ValidWalletCreateWitness,
+    },
+    MAX_BALANCES, MAX_ORDERS,
+};
 use crossbeam::channel::Receiver;
+use crypto::fields::prime_field_to_scalar;
+use curve25519_dalek::scalar::Scalar;
 use rayon::ThreadPool;
 
-use super::{error::ProofManagerError, jobs::ProofManagerJob};
+use crate::{proof_generation::jobs::ProofJob, SizedWallet, MAX_FEES};
+
+use super::{
+    error::ProofManagerError,
+    jobs::{ProofBundle, ProofManagerJob, ValidWalletCreateBundle},
+};
 
 // -------------
 // | Constants |
@@ -46,6 +62,7 @@ impl ProofManager {
             let job = job_queue
                 .recv()
                 .map_err(|err| ProofManagerError::JobQueueClosed(err.to_string()))?;
+
             thread_pool.install(move || {
                 if let Err(e) = Self::handle_proof_job(job) {
                     println!("Error handling proof manager job: {}", e)
@@ -56,7 +73,58 @@ impl ProofManager {
 
     /// The main job handler, run by a thread in the pool
     fn handle_proof_job(job: ProofManagerJob) -> Result<(), ProofManagerError> {
-        println!("Handling job {:?}", job);
+        match job.type_ {
+            ProofJob::ValidWalletCreate {
+                fees,
+                keys,
+                randomness,
+            } => {
+                // Prove `VALID WALLET CREATE`
+                let proof_bundle = Self::prove_valid_wallet_create(fees, keys, randomness)?;
+                job.response_channel
+                    .send(ProofBundle::ValidWalletCreate(proof_bundle))
+                    .map_err(|_| {
+                        ProofManagerError::Response("failed to send proof response".to_string())
+                    })?
+            }
+        };
+
         Ok(())
+    }
+
+    /// Create a proof of `VALID WALLET CREATE`
+    fn prove_valid_wallet_create(
+        fees: Vec<Fee>,
+        keys: KeyChain,
+        randomness: Scalar,
+    ) -> Result<ValidWalletCreateBundle, ProofManagerError> {
+        // Build an empty wallet and compute its commitment
+        let sized_fees: [Fee; MAX_FEES] = fees.try_into().unwrap();
+        let empty_wallet = SizedWallet {
+            balances: vec![Balance::default(); MAX_BALANCES].try_into().unwrap(),
+            orders: vec![Order::default(); MAX_ORDERS].try_into().unwrap(),
+            fees: sized_fees.clone(),
+            keys,
+            randomness,
+        };
+
+        let wallet_commit = compute_wallet_commitment(&empty_wallet);
+
+        // Build the statement and witness for the proof
+        let statement = ValidWalletCreateStatement {
+            wallet_commitment: prime_field_to_scalar(&wallet_commit),
+        };
+        let witness = ValidWalletCreateWitness::<MAX_FEES> {
+            fees: sized_fees,
+            keys,
+            wallet_randomness: randomness,
+        };
+
+        let (commitment, proof) = singleprover_prove::<
+            ValidWalletCreate<MAX_BALANCES, MAX_ORDERS, MAX_FEES>,
+        >(witness, statement.clone())
+        .map_err(|err| ProofManagerError::Prover(err.to_string()))?;
+
+        Ok(ValidWalletCreateBundle(commitment, statement, proof))
     }
 }
