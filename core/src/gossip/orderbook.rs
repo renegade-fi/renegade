192c7d42a99e6f8780f82c648cf48203d6fed034
Joey Kraut <joey@renegade.fi>
2023-02-17 18:05:41 -0800

    core: gossip: orderbook: Correctly track `is_local` for incoming orders

--- core/src/gossip/orderbook.rs
+++ core/src/gossip/orderbook.rs
@@ -88,15 +88,16 @@ impl GossipProtocolExecutor {
             .map_err(|err| GossipError::ValidCommitmentVerification(err.to_string()))?;
         }
 
-        order_info.local = false;
+        order_info.local = order_info.cluster == self.global_state.local_cluster_id;
         self.global_state.add_order(order_info);
         Ok(())
     }
 
     /// Handles a newly discovered order added to the book
     fn handle_new_order(&self, order_id: OrderIdentifier, cluster: ClusterId) {
+        let is_local = cluster == self.global_state.local_cluster_id;
         self.global_state
-            .add_order(NetworkOrder::new(order_id, cluster, false /* local */))
+            .add_order(NetworkOrder::new(order_id, cluster, is_local))
     }
 
     /// Handles a new validity proof attached to an order
@@ -109,14 +110,18 @@ impl GossipProtocolExecutor {
         cluster: ClusterId,
         proof_bundle: ValidCommitmentsBundle,
     ) -> Result<(), GossipError> {
+        let is_local = cluster.eq(&self.global_state.local_cluster_id);
+
         // Verify the proof
-        let bundle_clone = proof_bundle.clone();
-        verify_singleprover_proof::<SizedValidCommitments>(
-            proof_bundle.statement,
-            proof_bundle.commitment,
-            proof_bundle.proof,
-        )
-        .map_err(|err| GossipError::ValidCommitmentVerification(err.to_string()))?;
+        if !is_local {
+            let bundle_clone = proof_bundle.clone();
+            verify_singleprover_proof::<SizedValidCommitments>(
+                bundle_clone.statement,
+                bundle_clone.commitment,
+                bundle_clone.proof,
+            )
+            .map_err(|err| GossipError::ValidCommitmentVerification(err.to_string()))?;
+        }
 
         // Add the order to the book in the `Validated` state
         if !self
@@ -125,12 +130,12 @@ impl GossipProtocolExecutor {
             .contains_order(&order_id)
         {
             self.global_state
-                .add_order(NetworkOrder::new(order_id, cluster, false /* local */));
+                .add_order(NetworkOrder::new(order_id, cluster, is_local));
         }
 
         self.global_state
             .read_order_book()
-            .update_order_validity_proof(&order_id, bundle_clone);
+            .update_order_validity_proof(&order_id, proof_bundle);
 
         Ok(())
     }
