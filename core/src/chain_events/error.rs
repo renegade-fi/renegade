a1376a4158834ff3a69faa6ec7ceaf291856a472
Joey Kraut <joey@renegade.fi>
2023-02-27 16:31:31 -0800

    core: chain-events: Bootstrap `OnChainEventListener` worker
    This worker will monitor for StarkNet events emitted by the contract
    that indicate relevant state updates

---
+++ core/src/chain_events/error.rs
@@ -0,0 +1,5 @@
+//! Defines error types for the on-chain event listener
+
+/// The error type that the event listener emits
+#[derive(Clone, Debug)]
+pub enum OnChainEventListenerError {}
---
+++ core/src/chain_events/mod.rs
@@ -0,0 +1,5 @@
+//! Defines and implements the worker that listens for on-chain events
+
+pub mod error;
+pub mod listener;
+pub mod worker;
