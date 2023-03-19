fb55626122f6d76bf827814ce5e767ed9e7bef05
Joey Kraut <joey@renegade.fi>
2023-03-18 18:10:51 -0700

    core: api-server: http: Add `/network/clusters/:id` route and handler

--- core/src/api_server/http/network.rs
+++ core/src/api_server/http/network.rs
@@ -11,18 +11,27 @@
 use std::collections::HashMap;
 
 use async_trait::async_trait;
+use itertools::Itertools;
 
 use crate::{
     api_server::{
         error::ApiServerError,
         router::{TypedHandler, UrlParams},
     },
-    external_api::{http::network::GetNetworkTopologyResponse, types::Peer, EmptyRequestResponse},
+    external_api::{
+        http::network::{GetClusterInfoResponse, GetNetworkTopologyResponse},
+        types::{Cluster, Peer},
+        EmptyRequestResponse,
+    },
     state::RelayerState,
 };
 
+use super::parse_cluster_id_from_params;
+
 /// Returns the full network topology known to the local node
 pub(super) const GET_NETWORK_TOPOLOGY_ROUTE: &str = "/v0/network";
+/// Returns the cluster information for the specified cluster
+pub(super) const GET_CLUSTER_INFO_ROUTE: &str = "/v0/network/clusters/:cluster_id";
 
 // ------------------
 // | Route Handlers |
@@ -73,6 +82,55 @@ impl TypedHandler for GetNetworkTopologyHandler {
         // Reformat into response
         Ok(GetNetworkTopologyResponse {
             network: peers_by_cluster.into(),
+        })
+    }
+}
+
+/// Handler for the GET "/network/clusters" route
+#[derive(Clone, Debug)]
+pub struct GetClusterInfoHandler {
+    /// A copy of the relayer-global state
+    global_state: RelayerState,
+}
+
+impl GetClusterInfoHandler {
+    /// Constructor
+    pub fn new(global_state: RelayerState) -> Self {
+        Self { global_state }
+    }
+}
+
+#[async_trait]
+impl TypedHandler for GetClusterInfoHandler {
+    type Request = EmptyRequestResponse;
+    type Response = GetClusterInfoResponse;
+
+    async fn handle_typed(
+        &self,
+        _req: Self::Request,
+        params: UrlParams,
+    ) -> Result<Self::Response, ApiServerError> {
+        let cluster_id = parse_cluster_id_from_params(&params)?;
+
+        // For simplicity, fetch all peer info and filter by cluster
+        let peers = self
+            .global_state
+            .read_peer_index()
+            .await
+            .get_info_map()
+            .await;
+
+        let peers: Vec<Peer> = peers
+            .into_iter()
+            .filter(|(_, peer_info)| peer_info.get_cluster_id().eq(&cluster_id))
+            .map(|(_, peer_info)| peer_info.into())
+            .collect_vec();
+
+        Ok(GetClusterInfoResponse {
+            cluster: Cluster {
+                id: cluster_id.to_string(),
+                peers,
+            },
         })
     }
 }
