75e39c34fbc8f222f15e2e14d7d40c48555887be
Joey Kraut <joey@renegade.fi>
2023-03-21 10:32:18 -0700

    core: starknet-client: Add initial client wrapper and definition
    The `starknet-client` module abstracts over various common access
    patterns to the starknet sequencer gateway and provisioned jsonrpc
    nodes.

---
+++ core/src/starknet_client/client.rs
@@ -0,0 +1,118 @@
+//! A wrapper around the starknet client made available by:
+//! https://docs.rs/starknet-core/latest/starknet_core/
+
+use std::{str::FromStr, sync::Arc};
+
+use reqwest::Url;
+use starknet::core::types::FieldElement as StarknetFieldElement;
+use starknet_providers::{
+    jsonrpc::{HttpTransport, JsonRpcClient},
+    SequencerGatewayProvider,
+};
+
+use super::ChainId;
+
+/// The config type for the client, consists of secrets needed to connect to
+/// the gateway and API server, as well as keys for sending transactions
+#[derive(Clone)]
+pub struct StarknetClientConfig {
+    /// The chain this client should submit requests to
+    pub chain: ChainId,
+    /// The address of the Darkpool contract on chain
+    pub contract_addr: String,
+    /// The HTTP addressable JSON-RPC node to connect to for
+    /// requests that cannot go through the gateway
+    pub starknet_json_rpc_addr: Option<String>,
+    /// The API key for the JSON-RPC node
+    ///
+    /// For now, we require only the API key's ID on our RPC node,
+    /// so this parameter is unused
+    pub infura_api_key: Option<String>,
+    /// The starknet signing key, used to submit transactions on-chain
+    pub starknet_pkey: Option<String>,
+}
+
+impl StarknetClientConfig {
+    /// Whether or not the client is enabled given its configuration
+    pub fn enabled(&self) -> bool {
+        self.starknet_json_rpc_addr.is_some()
+    }
+
+    /// Whether or not the client's write ability (submit transaction) is
+    /// enabled given its config
+    pub fn write_enabled(&self) -> bool {
+        self.enabled() && self.starknet_pkey.is_some()
+    }
+
+    /// Build a gateway client from the config values
+    pub fn new_gateway_client(&self) -> SequencerGatewayProvider {
+        match self.chain {
+            ChainId::AlphaGoerli => SequencerGatewayProvider::starknet_alpha_goerli(),
+            ChainId::Mainnet => SequencerGatewayProvider::starknet_alpha_mainnet(),
+        }
+    }
+
+    /// Create a new JSON-RPC client using the API credentials in the config
+    ///
+    /// Returns `None` if the config does not specify the correct credentials
+    pub fn new_jsonrpc_client(&self) -> Option<JsonRpcClient<HttpTransport>> {
+        if !self.enabled() {
+            return None;
+        }
+
+        let transport =
+            HttpTransport::new(Url::parse(&self.starknet_json_rpc_addr.clone().unwrap()).ok()?);
+        Some(JsonRpcClient::new(transport))
+    }
+}
+
+/// A wrapper around the concrete JSON-RPC client that provides helpers for common
+/// Renegade-specific access patterns
+#[derive(Clone)]
+pub struct StarknetClient {
+    /// The config for the client
+    pub config: StarknetClientConfig,
+    /// The address of the contract on-chain
+    pub contract_address: StarknetFieldElement,
+    /// The client used to connect with the sequencer gateway
+    gateway_client: Arc<SequencerGatewayProvider>,
+    /// The client used to send starknet JSON-RPC requests
+    jsonrpc_client: Option<Arc<JsonRpcClient<HttpTransport>>>,
+}
+
+impl StarknetClient {
+    /// Constructor
+    pub fn new(config: StarknetClientConfig) -> Self {
+        let gateway_client = Arc::new(config.new_gateway_client());
+        let jsonrpc_client = config.new_jsonrpc_client().map(|client| Arc::new(client));
+
+        // Parse the contract address
+        let contract_address: StarknetFieldElement =
+            StarknetFieldElement::from_str(&config.contract_addr).expect(&format!(
+                "could not parse contract address {}",
+                config.contract_addr
+            ));
+
+        Self {
+            config,
+            contract_address,
+            gateway_client,
+            jsonrpc_client,
+        }
+    }
+
+    /// Whether or not JSON-RPC is enabled via the given config values
+    pub fn jsonrpc_enabled(&self) -> bool {
+        self.config.enabled()
+    }
+
+    /// Get the underlying gateway client as an immutable reference
+    pub fn get_gateway_client(&self) -> &SequencerGatewayProvider {
+        &self.gateway_client
+    }
+
+    /// Get the underlying RPC client as an immutable reference
+    pub fn get_jsonrpc_client(&self) -> &JsonRpcClient<HttpTransport> {
+        self.jsonrpc_client.as_ref().unwrap()
+    }
+}
