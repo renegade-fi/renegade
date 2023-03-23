da258c47ba98eac71dc02f53dec0000760213443
Joey Kraut <joey@renegade.fi>
2023-03-23 15:03:21 -0700

    core, crypto: Temporarily use `starknet-rs` fork while awaiting bugfix

--- core/src/starknet_client/mod.rs
+++ core/src/starknet_client/mod.rs
@@ -2,10 +2,16 @@
 //! specific information (keys, api credentials, etc) and provides a cleaner
 //! interface for interacting with on-chain state in Renegade specific patterns
 
-use std::str::FromStr;
+use std::{convert::TryInto, str::FromStr};
 
+use circuits::native_helpers::compute_poseidon_hash;
+use crypto::fields::biguint_to_scalar;
+use curve25519_dalek::scalar::Scalar;
+use num_bigint::BigUint;
 use serde::{Deserialize, Serialize};
 use starknet::core::{types::FieldElement as StarknetFieldElement, utils::get_selector_from_name};
+
+use crate::MERKLE_HEIGHT;
 
 pub mod client;
 pub mod error;
@@ -15,12 +21,37 @@ pub mod error;
 // -------------
 
 lazy_static! {
+    /// Contract selector to create a new wallet
+    static ref NEW_WALLET_SELECTOR: StarknetFieldElement = get_selector_from_name("new_wallet")
+        .unwrap();
     /// The event selector for internal node changes
     pub static ref INTERNAL_NODE_CHANGED_EVENT_SELECTOR: StarknetFieldElement =
         get_selector_from_name("Merkle_internal_node_changed").unwrap();
     /// The event selector for Merkle value insertion
     pub static ref VALUE_INSERTED_EVENT_SELECTOR: StarknetFieldElement =
         get_selector_from_name("Merkle_value_inserted").unwrap();
+    /// The value of an empty leaf in the Merkle tree
+    static ref EMPTY_LEAF_VALUE: Scalar = {
+        let val_bigint = BigUint::from_str(
+            "306932273398430716639340090025251549301604242969558673011416862133942957551"
+        ).unwrap();
+        biguint_to_scalar(&val_bigint)
+    };
+    /// The default values of an authentication path; i.e. the values in the path before any
+    /// path elements are changed by insertions
+    ///
+    /// These values are simply recursive hashes of the empty leaf value, as this builds the
+    /// empty tree
+    pub static ref DEFAULT_AUTHENTICATION_PATH: [Scalar; MERKLE_HEIGHT] = {
+        let mut values = Vec::with_capacity(MERKLE_HEIGHT);
+
+        let curr_val = *EMPTY_LEAF_VALUE;
+        for _ in 0..MERKLE_HEIGHT {
+            values.push(compute_poseidon_hash(&[curr_val, curr_val]));
+        }
+
+        values.try_into().unwrap()
+    };
 }
 
 /// Starknet mainnet chain-id
