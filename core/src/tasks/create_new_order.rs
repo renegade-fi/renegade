5ed884cf595a83d9c3d70d42c619f08816125e81
Joey Kraut <joey@renegade.fi>
2023-03-25 18:50:01 -0700

    core: tasks: create-new-order: Re-prove `VALID COMMITMENTS` after tx
    After a successfully `update_wallet`; the validity proofs for each
    order have been nullified along with the wallet. So, each order
    needs to be re-proved before it can be matched.

--- core/src/tasks/create_new_order.rs
+++ core/src/tasks/create_new_order.rs
@@ -1,16 +1,19 @@
 //! Handles the flow of adding a new order to a wallet
 
 use std::{
+    collections::HashMap,
     fmt::Display,
     time::{SystemTime, UNIX_EPOCH},
 };
 
 use circuits::{
+    native_helpers::compute_poseidon_hash,
     types::{
         order::Order as CircuitOrder,
         wallet::{Nullifier, WalletCommitment},
     },
     zk_circuits::valid_wallet_update::ValidWalletUpdateStatement,
+    zk_gadgets::merkle::MerkleOpening,
 };
 use crossbeam::channel::Sender as CrossbeamSender;
 use crypto::{
@@ -18,6 +21,7 @@ use crypto::{
     fields::{scalar_to_biguint, starknet_felt_to_biguint},
 };
 use curve25519_dalek::scalar::Scalar;
+use rand_core::OsRng;
 use starknet::core::types::TransactionStatus;
 use tokio::sync::oneshot;
 use tracing::log;
@@ -42,6 +46,8 @@ const ERR_NO_MERKLE_PROOF: &str = "wallet merkle proof not attached";
 const ERR_TRANSACTION_FAILED: &str = "transaction rejected";
 /// The wallet to create the order within was not found
 const ERR_WALLET_NOT_FOUND: &str = "wallet not found in state";
+/// The amount to increment the randomness each time a wallet is nullified
+const RANDOMNESS_INCREMENT: u8 = 2;
 
 /// Helper function to get the current UNIX epoch time in milliseconds
 pub fn get_current_time() -> u128 {
@@ -137,6 +143,7 @@ impl NewOrderTask {
 
         let mut new_wallet = old_wallet.clone();
         new_wallet.orders.insert(self.order_id, self.order.clone());
+        new_wallet.randomness += RANDOMNESS_INCREMENT;
 
         // Compute public statement variables sent to the contract
         let new_wallet_comm = new_wallet.get_commitment();
@@ -151,7 +158,7 @@ impl NewOrderTask {
         // Encrypt the new wallet under the public view key
         // TODO: This will eventually come directly from the user as they sign the encryption
         let pk_view = scalar_to_biguint(&new_wallet.public_keys.pk_view);
-        let encrypted_wallet = encrypt_wallet(new_wallet.into(), &pk_view);
+        let encrypted_wallet = encrypt_wallet(new_wallet.clone().into(), &pk_view);
 
         self.submit_tx(
             new_wallet_comm,
@@ -163,6 +170,7 @@ impl NewOrderTask {
         .await?;
 
         // Re-prove validity proofs for all orders in wallet
+        self.update_validity_proofs(new_wallet).await?;
 
         // Update state
         Ok(())
@@ -227,12 +235,14 @@ impl NewOrderTask {
         proof: ValidWalletUpdateBundle,
     ) -> Result<(), NewOrderTaskError> {
         // Submit on-chain
+        // TODO: Remove this
+        let mut rng = OsRng {};
         let tx_hash = self
             .starknet_client
             .update_wallet(
                 new_wallet_comm,
-                old_wallet_match_nullifier,
-                old_wallet_spend_nullifier,
+                old_wallet_match_nullifier + Scalar::random(&mut rng),
+                old_wallet_spend_nullifier + Scalar::random(&mut rng),
                 wallet_ciphertext,
                 proof,
             )
@@ -252,6 +262,80 @@ impl NewOrderTask {
                 ERR_TRANSACTION_FAILED.to_string(),
             ));
         }
+
+        Ok(())
+    }
+
+    /// After a wallet update has been submitted on-chain, find its authentication
+    /// path, and re-prove `VALID COMMITMENTS` for all orders in the wallet
+    async fn update_validity_proofs(&self, new_wallet: Wallet) -> Result<(), NewOrderTaskError> {
+        // Find the new wallet in the Merkle state
+        let authentication_path = self
+            .starknet_client
+            .find_merkle_authentication_path(new_wallet.get_commitment())
+            .await
+            .map_err(|err| NewOrderTaskError::StarknetClient(err.to_string()))?;
+        let new_root = authentication_path.compute_root();
+
+        let match_nullifier = new_wallet.get_match_nullifier();
+
+        // A wallet that is compatible with circuit types
+        let circuit_wallet: SizedWallet = new_wallet.clone().into();
+        let randomness_hash = compute_poseidon_hash(&[circuit_wallet.randomness]);
+        let wallet_opening: MerkleOpening = authentication_path.into();
+
+        // Request that the proof manager prove `VALID COMMITMENTS` for each order
+        let mut proof_response_channels = HashMap::new();
+        let locked_order_book = self.global_state.read_order_book().await;
+        for order_id in new_wallet.orders.keys() {
+            // Fetch the old witness
+            let old_witness = locked_order_book.get_validity_proof_witness(order_id).await;
+            if old_witness.is_none() {
+                // TODO: If a witness is not present, generate one now
+                continue;
+            }
+            let mut new_witness = old_witness.unwrap();
+
+            // Update the witness with the new wallet information
+            new_witness.wallet = circuit_wallet.clone();
+            new_witness.wallet_opening = wallet_opening.clone();
+            new_witness.randomness_hash = randomness_hash.into();
+
+            // Update the statement for the new wallet
+            let mut new_statement = locked_order_book
+                .get_validity_proof(order_id)
+                .await
+                .unwrap()
+                .statement;
+
+            new_statement.nullifier = match_nullifier;
+            new_statement.merkle_root = new_root;
+
+            // Send a job to the proof manager to prove `VALID COMMITMENTS` for this wallet
+            let (response_sender, response_receiver) = oneshot::channel();
+            self.proof_manager_work_queue
+                .send(ProofManagerJob {
+                    type_: ProofJob::ValidCommitments {
+                        witness: new_witness,
+                        statement: new_statement,
+                    },
+                    response_channel: response_sender,
+                })
+                .map_err(|err| NewOrderTaskError::SendMessage(err.to_string()))?;
+
+            proof_response_channels.insert(*order_id, response_receiver);
+        }
+
+        // Await proofs for all orders
+        for (order_id, proof_channel) in proof_response_channels.into_iter() {
+            let _proof = proof_channel
+                .await
+                .map_err(|err| NewOrderTaskError::ProofGeneration(err.to_string()))?;
+
+            log::info!("received proof for order: {order_id}");
+        }
+
+        log::info!("got proofs for all orders");
 
         Ok(())
     }
