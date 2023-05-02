65a45a6a305ec603756712c2e4899b5a915b0deb
Joey Kraut <joey@renegade.fi>
2023-05-01 18:10:28 -0700

    circuits: valid-reblind: Add witness and statement type definitions

--- circuits/src/zk_circuits/mod.rs
+++ circuits/src/zk_circuits/mod.rs
@@ -1,6 +1,7 @@
 //! Groups circuitry for full zero knowledge circuits that we are interested
 //! in proving knowledge of witness for throughout the network
 pub mod valid_match_mpc;
+pub mod valid_reblind;
 pub mod valid_wallet_create;
 pub mod valid_wallet_update;
 
