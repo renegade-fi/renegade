db8560c8df990f89c97b02755f623f618e5d3896
Joey Kraut <joey@renegade.fi>
2023-06-02 15:38:49 -0700

    core: external-api, state: Refactor use of orders to fit midpoint interface

--- core/src/state/wallet.rs
+++ core/src/state/wallet.rs
@@ -382,7 +382,8 @@ impl Wallet {
 
     /// Get the balance, fee, and fee_balance for an order by specifying the order directly
     ///
-    /// This is useful for new orders that come in, and are not yet indexed in the global state
+    /// We allow orders to be matched when undercapitalized; i.e. the respective balance does
+    /// not cover the full volume of the order.
     pub fn get_balance_and_fee_for_order(&self, order: &Order) -> Option<(Balance, Fee, Balance)> {
         // The mint the local party will be spending if the order is matched
         let order_mint = match order.side {
@@ -390,21 +391,9 @@ impl Wallet {
             OrderSide::Sell => order.base_mint.clone(),
         };
 
-        // The maximum quantity of the mint that the local party will be spending
-        let order_amount = match order.side {
-            OrderSide::Buy => {
-                let res_amount = (order.amount as f64) * order.price.to_f64();
-                res_amount as u64
-            }
-            OrderSide::Sell => order.amount,
-        };
-
         // Find a balance and fee to associate with this order
         // Choose the first fee for simplicity
         let balance = self.balances.get(&order_mint)?;
-        if balance.amount < order_amount {
-            return None;
-        }
 
         // Choose the first non-default fee
         let fee = self.fees.iter().find(|fee| !fee.is_default())?;
