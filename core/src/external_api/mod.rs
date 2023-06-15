f5efbe764d8ecc510e8036cdc7562e9210d22120
Christopher Bender <chrisbender@berkeley.edu>
2023-06-14 19:02:07 -0700

    core: external-api: Serialize an empty request/response as 'null'

--- core/src/external_api/mod.rs
+++ core/src/external_api/mod.rs
@@ -10,8 +10,45 @@ pub mod types;
 pub mod websocket;
 
 /// An empty request/response type
-#[derive(Clone, Debug, Serialize, Deserialize, Eq, PartialEq)]
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub struct EmptyRequestResponse {}
+
+/// Serialize an empty request/response
+impl Serialize for EmptyRequestResponse {
+    fn serialize<S>(&self, s: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        s.serialize_none()
+    }
+}
+
+/// Deserialize an empty request/response
+impl<'de> Deserialize<'de> for EmptyRequestResponse {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_unit(EmptyRequestResponseVisitor)
+    }
+}
+
+/// Visitor for deserializing an empty request/response
+struct EmptyRequestResponseVisitor;
+impl<'de> serde::de::Visitor<'de> for EmptyRequestResponseVisitor {
+    type Value = EmptyRequestResponse;
+
+    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        formatter.write_str("null")
+    }
+
+    fn visit_unit<E>(self) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(EmptyRequestResponse {})
+    }
+}
 
 /// A helper to serialize a BigUint to a hex string
 pub fn biguint_to_hex_string<S>(val: &BigUint, s: S) -> Result<S::Ok, S::Error>
@@ -40,14 +77,14 @@ mod test {
     use super::EmptyRequestResponse;
 
     /// Tests empty request/response serialization, expected behavior is that it serializes to and from
-    /// an empty json struct
+    /// the string "null"
     #[test]
     fn test_serde_empty() {
         let req = EmptyRequestResponse {};
         let serialized_str = serde_json::to_string(&req).unwrap();
-        assert_eq!(serialized_str, "{}");
+        assert_eq!(serialized_str, "null");
 
         // Test deserialization from empty json struct encoded as a string
-        let _req: EmptyRequestResponse = serde_json::from_str("{}").unwrap();
+        let _req: EmptyRequestResponse = serde_json::from_str("null").unwrap();
     }
 }
