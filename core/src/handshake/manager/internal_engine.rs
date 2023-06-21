edfae57c9c8e2c8b75a252123d21d1a0159f354f
Joey Kraut <joey@renegade.fi>
2023-06-21 13:58:56 -0700

    core: handshake: internal-engine: Add matching engine tests

--- core/src/handshake/manager/internal_engine.rs
+++ core/src/handshake/manager/internal_engine.rs
@@ -59,10 +59,21 @@ impl HandshakeExecutor {
         let mut shuffled_indices = (0..other_orders.len()).collect_vec();
         shuffled_indices.shuffle(&mut rng);
 
-        // Match against each wallet
+        // Match against each other order in the local book
         for order_id in shuffled_indices.into_iter().map(|ind| &other_orders[ind]) {
             // Same order
             if network_order.id == *order_id {
+                continue;
+            }
+
+            // Same wallet
+            let wallet_id = self
+                .global_state
+                .read_wallet_index()
+                .await
+                .get_wallet_for_order(order_id)
+                .ok_or_else(|| HandshakeManagerError::StateNotFound(ERR_NO_WALLET.to_string()))?;
+            if wallet_id == wallet.wallet_id {
                 continue;
             }
 
@@ -74,6 +85,7 @@ impl HandshakeExecutor {
 
             // Settle the match if the two orders cross
             if let Some(handshake_result) = res {
+                // TODO: Implement settlement
                 log::info!("match found: {handshake_result:?}, settling...");
             }
         }
@@ -108,6 +120,7 @@ impl HandshakeExecutor {
 // -----------
 // | Helpers |
 // -----------
+
 /// Match a given order against all other orders in the wallet
 fn match_orders(
     midpoint_price: f64,
@@ -142,4 +155,127 @@ fn match_orders(
         max_minus_min_amount,
         min_amount_order_index: if order1.amount <= order2.amount { 0 } else { 1 },
     }))
+}
+
+#[cfg(test)]
+mod tests {
+    use lazy_static::lazy_static;
+
+    use circuits::types::order::{Order, OrderSide};
+
+    // --------------
+    // | Dummy Data |
+    // --------------
+
+    /// The worst case price for the buy side
+    const BUY_SIDE_WORST_CASE_PRICE: f32 = 10.;
+    /// The worst case price for the sell side
+    const SELL_SIDE_WORST_CASE_PRICE: f32 = 5.;
+
+    lazy_static! {
+        /// The first dummy order used in a valid match
+        static ref ORDER1: Order = Order {
+            base_mint: 1u64.into(),
+            quote_mint: 2u64.into(),
+            side: OrderSide::Buy,
+            amount: 50,
+            worst_case_price: BUY_SIDE_WORST_CASE_PRICE.into(),
+            timestamp: 0,
+        };
+
+        /// The second dummy order used in a valid match
+        static ref ORDER2: Order = Order {
+            base_mint: 1u64.into(),
+            quote_mint: 2u64.into(),
+            side: OrderSide::Sell,
+            amount: 100,
+            worst_case_price: SELL_SIDE_WORST_CASE_PRICE.into(),
+            timestamp: 0,
+        };
+    }
+
+    // ---------
+    // | Tests |
+    // ---------
+
+    /// Test a valid match between two orders
+    #[test]
+    fn test_valid_match() {
+        let order1 = ORDER1.clone();
+        let order2 = ORDER2.clone();
+        let midpoint_price = 7.;
+        let res = super::match_orders(midpoint_price, &order1, &order2).unwrap();
+
+        assert!(res.is_some());
+        let res = res.unwrap();
+
+        assert_eq!(res.base_mint, 1u64.into());
+        assert_eq!(res.quote_mint, 2u64.into());
+        assert_eq!(res.base_amount, 50);
+        assert_eq!(
+            res.quote_amount,
+            350 /* midpoint_price * base_amount */
+        );
+        assert_eq!(res.direction, 0);
+        assert_eq!(res.max_minus_min_amount, 50);
+        assert_eq!(res.min_amount_order_index, 0);
+    }
+
+    /// Test mismatched base mints
+    #[test]
+    fn test_mismatched_base_mints() {
+        let order1 = ORDER1.clone();
+        let mut order2 = ORDER2.clone();
+        order2.base_mint = 3u64.into();
+        let midpoint_price = 7.;
+        let res = super::match_orders(midpoint_price, &order1, &order2).unwrap();
+
+        assert!(res.is_none());
+    }
+
+    /// Test mismatched quote mints
+    #[test]
+    fn test_mismatched_quote_mints() {
+        let order1 = ORDER1.clone();
+        let mut order2 = ORDER2.clone();
+        order2.quote_mint = 3u64.into();
+        let midpoint_price = 7.;
+        let res = super::match_orders(midpoint_price, &order1, &order2).unwrap();
+
+        assert!(res.is_none());
+    }
+
+    /// Test two orders on the same side of the book
+    #[test]
+    fn test_same_side() {
+        let order1 = ORDER1.clone();
+        let mut order2 = ORDER2.clone();
+        order2.side = order1.side;
+        let midpoint_price = 7.;
+        let res = super::match_orders(midpoint_price, &order1, &order2).unwrap();
+
+        assert!(res.is_none());
+    }
+
+    /// Test a midpoint price out of the buy side range
+    #[test]
+    fn test_buy_price_out_of_range() {
+        let order1 = ORDER1.clone();
+        let order2 = ORDER2.clone();
+        let midpoint_price = BUY_SIDE_WORST_CASE_PRICE + 1.;
+        let res = super::match_orders(midpoint_price as f64, &order1, &order2).unwrap();
+
+        assert!(res.is_none());
+    }
+
+    /// Test a midpoint price out of the sell side range
+    #[test]
+    fn test_sell_price_out_of_range() {
+        let order1 = ORDER1.clone();
+        let order2 = ORDER2.clone();
+        let midpoint_price = SELL_SIDE_WORST_CASE_PRICE - 1.;
+        let res = super::match_orders(midpoint_price as f64, &order1, &order2).unwrap();
+
+        assert!(res.is_none());
+    }
 }
