adfd84f4b4d8f147c3971d8e3618bbcb72530d1a
Joey Kraut <joey@renegade.fi>
2023-07-08 12:19:04 -0700

    gossip-api: Refactor into separate crate

---
+++ gossip-api/src/heartbeat.rs
@@ -0,0 +1,29 @@
+//! Groups API definitions for heartbeat requests and responses
+
+use common::types::{
+    gossip::{ClusterId, PeerInfo},
+    wallet::{OrderIdentifier, WalletIdentifier, WalletMetadata},
+};
+use std::collections::HashMap;
+
+use serde::{Deserialize, Serialize};
+
+/// Defines the heartbeat message, both request and response take
+/// on this message format
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct HeartbeatMessage {
+    /// The list of wallets managed by the sending relayer
+    pub managed_wallets: HashMap<WalletIdentifier, WalletMetadata>,
+    /// The set of peers known to the sending relayer
+    /// PeerID is converted to string for serialization
+    pub known_peers: HashMap<String, PeerInfo>,
+    /// The local peer's orderbook
+    pub orders: Vec<(OrderIdentifier, ClusterId)>,
+}
+
+/// Defines a request to bootstrap the cluster state from the recipient
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct BootstrapRequest {
+    /// The requester's peer ID
+    pub peer_info: PeerInfo,
+}
