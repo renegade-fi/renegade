53af992692544708399b08b1b299a8007610ccaf
Joey Kraut <joey@renegade.fi>
2023-07-08 19:05:37 -0700

    workers: price-reporter: Refactor into separate crate

---
+++ workers/price-reporter/src/lib.rs
@@ -0,0 +1,18 @@
+//! The price reporter module manages all external price feeds, including PriceReporter spin-up and
+//! tear-down, websocket connections to all exchanges (both centralized and decentralized), and
+//! aggregation of individual PriceReports into medians.
+
+#![deny(unsafe_code)]
+#![deny(missing_docs)]
+#![deny(clippy::missing_docs_in_private_items)]
+#![allow(incomplete_features)]
+#![feature(let_chains)]
+
+pub mod errors;
+pub mod exchange;
+pub mod manager;
+pub mod reporter;
+pub mod worker;
+
+/// The pubsub topic source name for median price reports
+pub const MEDIAN_SOURCE_NAME: &str = "median";
