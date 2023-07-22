dc7874659eb4e679fa73b54c3512c1c34b975161
Joey Kraut <joey@renegade.fi>
2023-07-22 08:08:04 -0700

    util: Refactor common utilities into separate crate

---
+++ util/src/lib.rs
@@ -0,0 +1,15 @@
+//! Defines one-off utility functions used throughout the node
+#![feature(ip)]
+#![allow(incomplete_features)]
+
+use std::time::{SystemTime, UNIX_EPOCH};
+
+pub mod networking;
+
+/// Returns the current unix timestamp in seconds, represented as u64
+pub fn get_current_time_seconds() -> u64 {
+    SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("negative timestamp")
+        .as_secs()
+}
