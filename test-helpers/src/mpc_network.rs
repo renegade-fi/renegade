759285e1605f810fc5cc4cbb7fd1f688efd15854
Joey Kraut <joey@renegade.fi>
2023-07-24 15:31:47 -0700

    test-helpers: Add helpers for simulating MPC over duplex stream

--- test-helpers/src/mpc_network.rs
+++ test-helpers/src/mpc_network.rs
@@ -5,8 +5,14 @@ pub mod mocks;
 use std::net::SocketAddr;
 
 use dns_lookup::lookup_host;
-use futures::executor::block_on;
-use mpc_stark::{network::QuicTwoPartyNet, MpcFabric};
+use futures::{executor::block_on, Future};
+use mpc_stark::{
+    network::{PartyId, QuicTwoPartyNet},
+    MpcFabric, PARTY0, PARTY1,
+};
+use tokio::io::duplex;
+
+use crate::mpc_network::mocks::MockNetwork;
 
 use self::mocks::PartyIDBeaverSource;
 
@@ -54,4 +60,42 @@ pub fn setup_mpc_fabric(party_id: u64, local_port: u64, peer_port: u64, docker: 
 
     let beaver_source = PartyIDBeaverSource::new(party_id);
     MpcFabric::new_with_size_hint(DEFAULT_MPC_SIZE_HINT, net, beaver_source)
+}
+
+/// Run a mock MPC connected by a duplex stream as the mock network
+///
+/// This will spawn two tasks to execute either side of the MPC
+///
+/// Returns the outputs of both parties
+pub async fn execute_mock_mpc<T, S, F>(mut f: F) -> (T, T)
+where
+    T: Send + 'static,
+    S: Future<Output = T> + Send + 'static,
+    F: FnMut(MpcFabric) -> S,
+{
+    // Build a duplex stream to broker communication between the two parties
+    let (party0_stream, party1_stream) = duplex(1024 /* max_buffer_size */);
+    let party0_fabric = MpcFabric::new(
+        MockNetwork::new(PARTY0, party0_stream),
+        PartyIDBeaverSource::new(PARTY0),
+    );
+    let party1_fabric = MpcFabric::new(
+        MockNetwork::new(PARTY1, party1_stream),
+        PartyIDBeaverSource::new(PARTY1),
+    );
+
+    // Spawn two tasks to execute the MPC
+    let fabric0 = party0_fabric.clone();
+    let fabric1 = party1_fabric.clone();
+    let party0_task = tokio::spawn(f(fabric0));
+    let party1_task = tokio::spawn(f(fabric1));
+
+    let party0_output = party0_task.await.unwrap();
+    let party1_output = party1_task.await.unwrap();
+
+    // Shutdown the fabrics
+    party0_fabric.shutdown();
+    party1_fabric.shutdown();
+
+    (party0_output, party1_output)
 }
