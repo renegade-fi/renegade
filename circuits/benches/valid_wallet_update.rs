9493a7c30803ef7bdbc40579174683e4016ca206
Joey Kraut <joey@renegade.fi>
2023-08-02 18:29:46 -0700

    circuits: zk-circuits: valid-wallet-update: Refactor over Stark curve

--- circuits/benches/valid_wallet_update.rs
+++ circuits/benches/valid_wallet_update.rs
@@ -1,33 +1,54 @@
-//! Tests the process of proving and verifying a `VALID WALLET CREATE` circuit
+//! Benchmarks for the `VALID WALLET UPDATE` circuit
+
 #![allow(incomplete_features)]
 #![feature(generic_const_exprs)]
 
-use circuit_types::traits::{CircuitBaseType, SingleProverCircuit};
-use circuits::zk_circuits::valid_wallet_create::ValidWalletCreate;
+use circuit_types::{
+    order::Order,
+    traits::{CircuitBaseType, SingleProverCircuit},
+    transfers::ExternalTransfer,
+};
+use circuits::zk_circuits::{
+    test_helpers::INITIAL_WALLET,
+    valid_wallet_update::{
+        test_helpers::{construct_witness_statement, SizedStatement, SizedWitness},
+        ValidWalletUpdate,
+    },
+};
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use merlin::HashChainTranscript;
-use mpc_bulletproof::r1cs::Prover;
-use mpc_bulletproof::PedersenGens;
+use mpc_bulletproof::{r1cs::Prover, PedersenGens};
 use rand::thread_rng;
 
-use circuits::zk_circuits::valid_wallet_create::test_helpers::create_default_witness_statement;
+/// Construct a dummy witness and statement for the circuit
+pub fn create_default_witness_statement() -> (SizedWitness, SizedStatement) {
+    // Take a default wallet and cancel an order
+    let original_wallet = INITIAL_WALLET.clone();
+    let mut modified_wallet = INITIAL_WALLET.clone();
+    modified_wallet.orders[0] = Order::default();
 
-/// Tests the time taken to apply the constraints of `VALID WALLET CREATE` circuit
+    construct_witness_statement(
+        original_wallet,
+        modified_wallet,
+        ExternalTransfer::default(),
+    )
+}
+
+/// Benchmark constraint generation for the circuit
 pub fn bench_apply_constraints(c: &mut Criterion) {
-    // Build a witness and statement
-    let (witness, statement) = create_default_witness_statement();
     let mut rng = thread_rng();
     let mut transcript = HashChainTranscript::new(b"test");
     let pc_gens = PedersenGens::default();
     let mut prover = Prover::new(&pc_gens, &mut transcript);
 
+    let (witness, statement) = create_default_witness_statement();
     let (witness_var, _) = witness.commit_witness(&mut rng, &mut prover);
     let statement_var = statement.commit_public(&mut prover);
 
-    let mut group = c.benchmark_group("valid_wallet_create");
+    let mut group = c.benchmark_group("valid_wallet_update");
     group.bench_function(BenchmarkId::new("constraint-generation", ""), |b| {
         b.iter(|| {
-            ValidWalletCreate::apply_constraints(
+            ValidWalletUpdate::apply_constraints(
                 witness_var.clone(),
                 statement_var.clone(),
                 &mut prover,
@@ -37,24 +58,24 @@ pub fn bench_apply_constraints(c: &mut Criterion) {
     });
 }
 
-/// Tests the time taken to prove `VALID WALLET CREATE`
+/// Benchmark proving time for the circuit
 pub fn bench_prover(c: &mut Criterion) {
-    // Build a witness and statement
+    // Build a witness and statement to prove on ahead of time
     let (witness, statement) = create_default_witness_statement();
 
-    let mut group = c.benchmark_group("valid_wallet_create");
+    let mut group = c.benchmark_group("valid_wallet_update");
     group.bench_function(BenchmarkId::new("prover", ""), |b| {
         b.iter(|| {
             let mut transcript = HashChainTranscript::new(b"test");
             let pc_gens = PedersenGens::default();
             let prover = Prover::new(&pc_gens, &mut transcript);
 
-            ValidWalletCreate::prove(witness.clone(), statement.clone(), prover).unwrap();
+            ValidWalletUpdate::prove(witness.clone(), statement.clone(), prover).unwrap();
         });
     });
 }
 
-/// Tests the time taken to verify `VALID WALLET CREATE`
+/// Tests the time taken to verify `VALID WALLET UPDATE`
 pub fn bench_verifier(c: &mut Criterion) {
     // First generate a proof that will be verified multiple times
     let (witness, statement) = create_default_witness_statement();
@@ -63,15 +84,15 @@ pub fn bench_verifier(c: &mut Criterion) {
     let prover = Prover::new(&pc_gens, &mut transcript);
 
     let (commitments, proof) =
-        ValidWalletCreate::prove(witness, statement.clone(), prover).unwrap();
+        ValidWalletUpdate::prove(witness, statement.clone(), prover).unwrap();
 
-    let mut group = c.benchmark_group("valid_wallet_create");
+    let mut group = c.benchmark_group("valid_wallet_update");
     group.bench_function(BenchmarkId::new("verifier", ""), |b| {
         b.iter(|| {
             let mut transcript = HashChainTranscript::new(b"test");
             let verifier = mpc_bulletproof::r1cs::Verifier::new(&pc_gens, &mut transcript);
 
-            ValidWalletCreate::verify(
+            ValidWalletUpdate::verify(
                 commitments.clone(),
                 statement.clone(),
                 proof.clone(),
@@ -82,9 +103,10 @@ pub fn bench_verifier(c: &mut Criterion) {
     });
 }
 
-criterion_group! {
-    name = valid_wallet_create;
+criterion_group!(
+    name = valid_wallet_update;
     config = Criterion::default().sample_size(10);
     targets = bench_apply_constraints, bench_prover, bench_verifier
-}
-criterion_main!(valid_wallet_create);
+);
+
+criterion_main!(valid_wallet_update);
