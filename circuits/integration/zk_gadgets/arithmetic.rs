dde14c7214950f523b544b32e5aa4d46305963dd
Joey Kraut <joey@renegade.fi>
2023-08-03 11:09:55 -0700

    circuits: integration: zk-gadgets: Refactor tests over stark curve

--- circuits/integration/zk_gadgets/arithmetic.rs
+++ circuits/integration/zk_gadgets/arithmetic.rs
@@ -1,46 +1,38 @@
 //! Groups gadgets around arithmetic integration tests
 use circuit_types::traits::MultiproverCircuitBaseType;
 use circuits::zk_gadgets::arithmetic::MultiproverExpGadget;
-use curve25519_dalek::scalar::Scalar;
-use integration_helpers::{
-    mpc_network::field::get_ristretto_group_modulus, types::IntegrationTest,
-};
-use merlin::Transcript;
+use merlin::HashChainTranscript as Transcript;
 use mpc_bulletproof::{
     r1cs::Variable,
     r1cs_mpc::{MpcConstraintSystem, MpcLinearCombination, MpcProver, MpcVariable},
     PedersenGens,
 };
-use rand_core::{OsRng, RngCore};
-use renegade_cryptofields::{bigint_to_scalar, scalar_to_bigint};
+use mpc_stark::{algebra::scalar::Scalar, PARTY0, PARTY1};
+use rand::{thread_rng, RngCore};
+use renegade_crypto::fields::{get_scalar_field_modulus, scalar_to_bigint};
+use test_helpers::{mpc_network::await_result, types::IntegrationTest};
 
 use crate::{IntegrationTestArgs, TestWrapper};
 
 /// Tests that the exponentiation gadget works properly on valid inputs
 fn test_exp_multiprover(test_args: &IntegrationTestArgs) -> Result<(), String> {
     // Party 1 chooses an exponent, party 0 chooses the base
-    let mut rng = OsRng {};
-    let shared_base = test_args
-        .borrow_fabric()
-        .allocate_private_scalar(0 /* owning_party */, Scalar::random(&mut rng))
-        .map_err(|err| format!("Error sharing base: {:?}", err))?;
-    let shared_exp = test_args
-        .borrow_fabric()
-        .allocate_private_u64(1 /* owning_party */, rng.next_u32() as u64)
-        .map_err(|err| format!("Error sharing exponent: {:?}", err))?;
+    let mut rng = thread_rng();
+    let fabric = &test_args.mpc_fabric;
+    let shared_base = fabric.share_scalar(Scalar::random(&mut rng), PARTY0);
+    let shared_exp = fabric.share_scalar(rng.next_u32(), PARTY1);
 
     // Compute the expected result
-    let base_open = scalar_to_bigint(&shared_base.open_and_authenticate().unwrap().to_scalar());
-    let exp_open = scalar_to_bigint(&shared_exp.open_and_authenticate().unwrap().to_scalar());
+    let base_open = await_result(shared_base.open()).to_biguint();
+    let exp_open = await_result(shared_exp.open()).to_biguint();
 
-    let expected_res = base_open.modpow(&exp_open, &get_ristretto_group_modulus().into());
-    let expected_scalar = bigint_to_scalar(&expected_res);
+    let expected_res = base_open.modpow(&exp_open, &get_scalar_field_modulus().into());
+    let expected_scalar = expected_res.into();
 
     // Prove and verify the exp statement
     let pc_gens = PedersenGens::default();
     let mut transcript = Transcript::new(b"test");
-    let mut prover =
-        MpcProver::new_with_fabric(test_args.mpc_fabric.clone().0, &mut transcript, &pc_gens);
+    let mut prover = MpcProver::new_with_fabric(test_args.mpc_fabric.clone(), transcript, &pc_gens);
     let (shared_base_var, _) = shared_base.commit_shared(&mut rng, &mut prover).unwrap();
     let res = MultiproverExpGadget::exp(
         shared_base_var,
@@ -50,7 +42,7 @@ fn test_exp_multiprover(test_args: &IntegrationTestArgs) -> Result<(), String> {
     )
     .map_err(|err| format!("Error computing exp circuit: {:?}", err))?;
     prover.constrain(
-        res - MpcLinearCombination::from_scalar(expected_scalar, test_args.mpc_fabric.clone().0),
+        res - MpcLinearCombination::from_scalar(expected_scalar, test_args.mpc_fabric.clone()),
     );
 
     if prover.constraints_satisfied().unwrap() {
@@ -62,24 +54,18 @@ fn test_exp_multiprover(test_args: &IntegrationTestArgs) -> Result<(), String> {
 
 /// Tests the exp gadget on an invalid witness
 fn test_exp_multiprover_invalid(test_args: &IntegrationTestArgs) -> Result<(), String> {
-    let mut rng = OsRng {};
+    let mut rng = thread_rng();
+    let fabric = &test_args.mpc_fabric;
 
-    let shared_base = test_args
-        .borrow_fabric()
-        .allocate_private_scalar(0 /* owning_party */, Scalar::random(&mut rng))
-        .map_err(|err| format!("Error sharing base: {:?}", err))?;
-    let shared_exp = test_args
-        .borrow_fabric()
-        .allocate_private_u64(1 /* owning_party */, rng.next_u32() as u64)
-        .map_err(|err| format!("Error sharing exponent: {:?}", err))?;
+    let shared_base = fabric.share_scalar(Scalar::random(&mut rng), PARTY0);
+    let shared_exp = fabric.share_scalar(rng.next_u32(), PARTY1);
 
     // Compute the expected result
-    let exp_open = scalar_to_bigint(&shared_exp.open_and_authenticate().unwrap().to_scalar());
+    let exp_open = await_result(shared_exp.open()).to_biguint();
 
     let pc_gens = PedersenGens::default();
     let mut transcript = Transcript::new(b"test");
-    let mut prover =
-        MpcProver::new_with_fabric(test_args.mpc_fabric.clone().0, &mut transcript, &pc_gens);
+    let mut prover = MpcProver::new_with_fabric(test_args.mpc_fabric.clone(), transcript, &pc_gens);
     let (shared_base_var, _) = shared_base.commit_shared(&mut rng, &mut prover).unwrap();
 
     let res = MultiproverExpGadget::exp(
@@ -89,9 +75,8 @@ fn test_exp_multiprover_invalid(test_args: &IntegrationTestArgs) -> Result<(), S
         &mut prover,
     )
     .map_err(|err| format!("Error computing exp circuit: {:?}", err))?;
-    prover.constrain(
-        res - MpcVariable::new_with_type(Variable::One(), test_args.mpc_fabric.clone().0),
-    );
+    prover
+        .constrain(res - MpcVariable::new_with_type(Variable::One(), test_args.mpc_fabric.clone()));
 
     if prover.constraints_satisfied().unwrap() {
         Err("Constraints satisfied".to_string())
