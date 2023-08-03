79dc7c544d3f98194c9c0c3672b83b8913406ae9
Joey Kraut <joey@renegade.fi>
2023-08-03 11:09:55 -0700

    circuits: integration: zk-gadgets: Refactor tests over stark curve

--- circuits/integration/zk_gadgets/bits.rs
+++ circuits/integration/zk_gadgets/bits.rs
@@ -3,56 +3,40 @@
 
 use circuit_types::traits::MultiproverCircuitBaseType;
 use circuits::zk_gadgets::bits::MultiproverToBitsGadget;
-use curve25519_dalek::scalar::Scalar;
-use integration_helpers::{mpc_network::batch_share_plaintext_scalar, types::IntegrationTest};
-use merlin::Transcript;
+use merlin::HashChainTranscript as Transcript;
 use mpc_bulletproof::{
     r1cs_mpc::{MpcConstraintSystem, MpcLinearCombination, MpcProver},
     PedersenGens,
 };
-use rand_core::{OsRng, RngCore};
-use renegade_cryptofields::{bigint_to_scalar_bits, scalar_to_bigint};
+use mpc_stark::{algebra::scalar::Scalar, PARTY0};
+use rand::{thread_rng, RngCore};
+use renegade_crypto::fields::{bigint_to_scalar_bits, scalar_to_bigint};
+use test_helpers::{
+    mpc_network::{await_result, await_result_batch},
+    types::IntegrationTest,
+};
 
 use crate::{IntegrationTestArgs, TestWrapper};
-
-/// A debug macro used for printing wires in an MPC-ZK circuit during execution
-#[allow(unused)]
-macro_rules! print_multiprover_wire {
-    ($x:expr, $cs:ident, $party_id:expr) => {{
-        use renegade_cryptofields::scalar_to_biguint;
-        let x_eval = $cs.eval(&$x.into()).unwrap().open().unwrap().to_scalar();
-        if $party_id == 0 {
-            println!("eval({}): {:?}", stringify!($x), scalar_to_biguint(&x_eval));
-        }
-    }};
-}
 
 /// Tests the to_bits gadget
 fn test_to_bits(test_args: &IntegrationTestArgs) -> Result<(), String> {
     // Generate a random scalar to bitify
-    let mut rng = OsRng {};
+    let mut rng = thread_rng();
+    let fabric = &test_args.mpc_fabric;
     let random_scalar = Scalar::from(rng.next_u64());
 
     // Party 0 shares the scalar as a private input
-    let shared_scalar = test_args
-        .borrow_fabric()
-        .allocate_private_scalar(0 /* owning_party */, random_scalar)
-        .map_err(|err| format!("Error sharing scalar to bitify: {:?}", err))?;
+    let shared_scalar = fabric.share_scalar(random_scalar, PARTY0);
 
     // Bitify the input and share the result to build a circuit statement
     let bits = &bigint_to_scalar_bits::<64 /* bits */>(&scalar_to_bigint(&random_scalar))[..64];
-    let expected_bits = batch_share_plaintext_scalar(
-        bits,
-        0, /* owning_party */
-        test_args.mpc_fabric.0.clone(),
-    );
+    let expected_bits = await_result(fabric.batch_share_plaintext(bits.to_vec(), PARTY0));
 
     let pc_gens = PedersenGens::default();
     let mut transcript = Transcript::new(b"test");
-    let mut prover =
-        MpcProver::new_with_fabric(test_args.mpc_fabric.clone().0, &mut transcript, &pc_gens);
+    let mut prover = MpcProver::new_with_fabric(test_args.mpc_fabric.clone(), transcript, &pc_gens);
     let (shared_scalar_var, _) = shared_scalar.commit_shared(&mut rng, &mut prover).unwrap();
-    let res_bits = MultiproverToBitsGadget::<64 /* bits */, _, _>::to_bits(
+    let res_bits = MultiproverToBitsGadget::<64 /* bits */>::to_bits(
         shared_scalar_var,
         test_args.mpc_fabric.clone(),
         &mut prover,
@@ -61,7 +45,7 @@ fn test_to_bits(test_args: &IntegrationTestArgs) -> Result<(), String> {
 
     for (expected_bit, bit) in expected_bits.iter().zip(res_bits.iter()) {
         let allocated_bit =
-            MpcLinearCombination::from_scalar(*expected_bit, test_args.mpc_fabric.clone().0);
+            MpcLinearCombination::from_scalar(*expected_bit, test_args.mpc_fabric.clone());
 
         prover.constrain(bit.clone() - allocated_bit);
     }
--- circuits/integration/zk_gadgets/poseidon.rs
+++ circuits/integration/zk_gadgets/poseidon.rs
@@ -1,89 +1,64 @@
 //! Groups integration tests for multiprover poseidon gadget
 
-use ark_crypto_primitives::sponge::{poseidon::PoseidonSponge, CryptographicSponge};
+use ark_crypto_primitives::sponge::CryptographicSponge;
 use circuit_types::traits::MultiproverCircuitBaseType;
-use circuits::{
-    mpc_gadgets::poseidon::PoseidonSpongeParameters,
-    zk_gadgets::poseidon::MultiproverPoseidonHashGadget,
-};
+use circuits::zk_gadgets::poseidon::MultiproverPoseidonHashGadget;
 
-use curve25519_dalek::scalar::Scalar;
-use integration_helpers::types::IntegrationTest;
 use itertools::Itertools;
-use merlin::Transcript;
+use merlin::HashChainTranscript as Transcript;
 use mpc_bulletproof::{
     r1cs_mpc::{MpcLinearCombination, MpcProver},
     PedersenGens,
 };
-use mpc_ristretto::authenticated_scalar::AuthenticatedScalar;
-use rand_core::{OsRng, RngCore};
-use renegade_cryptofields::{prime_field_to_scalar, scalar_to_prime_field, DalekRistrettoField};
+use mpc_stark::{
+    algebra::{authenticated_scalar::AuthenticatedScalarResult, scalar::Scalar},
+    PARTY0, PARTY1,
+};
+use rand::{thread_rng, RngCore};
+use renegade_crypto::hash::{compute_poseidon_hash, default_poseidon_params};
+use test_helpers::{mpc_network::await_result_batch, types::IntegrationTest};
 
-use crate::{mpc_gadgets::poseidon::convert_params, IntegrationTestArgs, TestWrapper};
+use crate::{IntegrationTestArgs, TestWrapper};
 
 /// Tests the poseidon hash gadget
 fn test_poseidon_multiprover(test_args: &IntegrationTestArgs) -> Result<(), String> {
     // Each party samples a random set of 5 values
-    let mut rng = OsRng {};
     let n = 1;
+    let mut rng = thread_rng();
+    let fabric = &test_args.mpc_fabric;
     let my_values = (0..n).map(|_| rng.next_u64()).collect_vec();
 
     // Secret share the values
-    let party0_values = test_args
-        .borrow_fabric()
-        .batch_allocate_private_u64s(0 /* owning_party */, &my_values)
-        .map_err(|err| format!("Error sharing party 0 values: {:?}", err))?;
-
-    let party1_values = test_args
-        .borrow_fabric()
-        .batch_allocate_private_u64s(1 /* owning_party */, &my_values)
-        .map_err(|err| format!("Error sharing party 1 values: {:?}", err))?;
+    let party0_values = fabric.batch_share_scalar(my_values.clone(), PARTY0);
+    let party1_values = fabric.batch_share_scalar(my_values, PARTY1);
 
     // Compute the expected result via Arkworks hash
-    let inputs_open: Vec<AuthenticatedScalar<_, _>> =
-        AuthenticatedScalar::batch_open(&party0_values)
-            .map_err(|err| format!("Error opening party 0 inputs: {:?}", err))?
-            .into_iter()
-            .chain(
-                AuthenticatedScalar::batch_open_and_authenticate(&party1_values)
-                    .map_err(|err| format!("Error opening party 1 values: {:?}", err))?
-                    .into_iter(),
-            )
-            .collect_vec();
-
-    let arkworks_input = inputs_open
-        .iter()
-        .map(|hash_input| scalar_to_prime_field(&hash_input.to_scalar()));
-
-    let hasher_params = PoseidonSpongeParameters::default();
-    let arkworks_params = convert_params(&hasher_params);
-    let mut arkworks_hasher = PoseidonSponge::new(&arkworks_params);
-
-    for input in arkworks_input {
-        arkworks_hasher.absorb(&input);
-    }
-    let expected_scalar = prime_field_to_scalar(
-        &arkworks_hasher.squeeze_field_elements::<DalekRistrettoField>(1 /* num_elements */)[0],
-    );
+    let inputs_open = await_result_batch(AuthenticatedScalarResult::open_batch(
+        &party0_values
+            .iter()
+            .cloned()
+            .chain(party1_values.iter().cloned())
+            .collect_vec(),
+    ));
+    let expected_result = compute_poseidon_hash(&inputs_open);
 
     // Prove the statement
     let pc_gens = PedersenGens::default();
     let mut transcript = Transcript::new(b"test");
-    let mut prover =
-        MpcProver::new_with_fabric(test_args.mpc_fabric.clone().0, &mut transcript, &pc_gens);
-    let hash_input: Vec<MpcLinearCombination<_, _>> = party0_values
+    let mut prover = MpcProver::new_with_fabric(test_args.mpc_fabric.clone(), transcript, &pc_gens);
+    let hash_input: Vec<MpcLinearCombination> = party0_values
         .into_iter()
         .chain(party1_values.into_iter())
         .map(|v| v.commit_shared(&mut rng, &mut prover).unwrap())
         .map(|(var, _)| var.into())
         .collect_vec();
 
-    let mut hasher =
-        MultiproverPoseidonHashGadget::new(hasher_params, test_args.mpc_fabric.clone());
+    let params = default_poseidon_params();
+    let mut hasher = MultiproverPoseidonHashGadget::new(params, test_args.mpc_fabric.clone());
     hasher
         .hash(
             &hash_input,
-            &MpcLinearCombination::from_scalar(expected_scalar, test_args.mpc_fabric.clone().0),
+            &MpcLinearCombination::from_scalar(expected_result, test_args.mpc_fabric.clone()),
             &mut prover,
         )
         .map_err(|err| format!("Error computing poseidon hash circuit: {:?}", err))?;
@@ -99,36 +74,31 @@ fn test_poseidon_multiprover(test_args: &IntegrationTestArgs) -> Result<(), Stri
 fn test_poseidon_multiprover_invalid(test_args: &IntegrationTestArgs) -> Result<(), String> {
     // Sample a random input
     // Each party samples a random set of 5 values
-    let mut rng = OsRng {};
     let n = 1;
+    let mut rng = thread_rng();
+    let fabric = &test_args.mpc_fabric;
     let my_values = (0..n).map(|_| rng.next_u64()).collect_vec();
 
     // Secret share the values
-    let party0_values = test_args
-        .borrow_fabric()
-        .batch_allocate_private_u64s(0 /* owning_party */, &my_values)
-        .map_err(|err| format!("Error sharing party 0 values: {:?}", err))?;
-
-    let party1_values = test_args
-        .borrow_fabric()
-        .batch_allocate_private_u64s(1 /* owning_party */, &my_values)
-        .map_err(|err| format!("Error sharing party 1 values: {:?}", err))?;
+    let party0_values = fabric.batch_share_scalar(my_values.clone(), PARTY0);
+    let party1_values = fabric.batch_share_scalar(my_values, PARTY1);
 
     // Prove the statement
-    let hasher_params = PoseidonSpongeParameters::default();
+    let hasher_params = default_poseidon_params();
     let pc_gens = PedersenGens::default();
-    let mut transcript = Transcript::new(b"test");
-    let mut prover =
-        MpcProver::new_with_fabric(test_args.mpc_fabric.clone().0, &mut transcript, &pc_gens);
-    let hash_input: Vec<MpcLinearCombination<_, _>> = party0_values
+    let transcript = Transcript::new(b"test");
+    let mut prover = MpcProver::new_with_fabric(test_args.mpc_fabric.clone(), transcript, &pc_gens);
+
+    let hash_input: Vec<MpcLinearCombination> = party0_values
         .iter()
         .chain(party1_values.iter())
         .map(|v| v.commit_shared(&mut rng, &mut prover).unwrap())
         .map(|(var, _)| var.into())
         .collect_vec();
+
     let expected_out = MpcLinearCombination::from_scalar(
         Scalar::from(rng.next_u64()),
-        test_args.mpc_fabric.clone().0,
+        test_args.mpc_fabric.clone(),
     );
 
     let mut hasher =
