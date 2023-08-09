a52a929e5f2f68d2c6cf439c6a811268ecaba404
Joey Kraut <joey@renegade.fi>
2023-08-09 10:29:09 -0700

    task-driver: Setup `StarknetClient` for integration tests

--- task-driver/integration/main.rs
+++ task-driver/integration/main.rs
@@ -4,8 +4,17 @@
 #![deny(clippy::missing_docs_in_private_items)]
 
 use clap::Parser;
+use common::types::chain_id::ChainId;
+use starknet_client::client::{StarknetClient, StarknetClientConfig};
 use test_helpers::integration_test_main;
 use tracing::log::LevelFilter;
+use util::runtime::await_result;
+
+/// The hostport that the test expects a local devnet node to be running on
+///
+/// This assumes that the integration tests are running in a docker-compose setup
+/// with a DNS alias `sequencer` pointing to a devnet node running in a sister container
+const DEVNET_HOSTPORT: &str = "http://sequencer:5050/rpc";
 
 /// The arguments used to run the integration tests
 #[derive(Debug, Clone, Parser)]
@@ -20,11 +29,26 @@ struct CliArgs {
 }
 
 /// The arguments provided to every integration test
-#[derive(Debug, Clone)]
-struct IntegrationTestArgs;
+#[derive(Clone)]
+struct IntegrationTestArgs {
+    /// The starknet client that resolves to a locally running devnet node
+    starknet_client: StarknetClient,
+}
+
 impl From<CliArgs> for IntegrationTestArgs {
     fn from(_: CliArgs) -> Self {
-        Self
+        let starknet_client = StarknetClient::new(StarknetClientConfig {
+            chain: ChainId::Devnet,
+            contract_addr: "".to_string(),
+            // Assumes we are running in a docker-compose setup with a DNS alias `sequencer`
+            // pointing to a devnet node running in a sister container
+            starknet_json_rpc_addr: Some(DEVNET_HOSTPORT.to_string()),
+            infura_api_key: None,
+            starknet_account_addresses: vec![],
+            starknet_pkeys: vec![],
+        });
+
+        Self { starknet_client }
     }
 }
 
@@ -37,9 +61,16 @@ fn setup_integration_tests(_test_args: &CliArgs) {
 /// Dummy test
 ///
 /// TODO: Remove this test
-fn test_dummy(_test_args: &IntegrationTestArgs) -> Result<(), String> {
-    println!("got here!");
-    Ok(())
+fn test_dummy(test_args: &IntegrationTestArgs) -> Result<(), String> {
+    let client = &test_args.starknet_client;
+    let block_num = await_result(client.get_block_number())
+        .map_err(|e| format!("error fetching block number: {e}"))?;
+
+    if block_num == 0 {
+        Ok(())
+    } else {
+        Err(format!("expected block number to be 0, got {block_num}"))
+    }
 }
 
 inventory::submit!(TestWrapper(IntegrationTest {
