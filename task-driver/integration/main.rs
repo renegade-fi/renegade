a40c08d2211091d1aa86125b0e2f4ee2efe93df8
Joey Kraut <joey@renegade.fi>
2023-08-10 19:20:42 -0700

    task-driver: Setup devnet sequencer for integration tests

--- task-driver/integration/main.rs
+++ task-driver/integration/main.rs
@@ -5,8 +5,12 @@
 
 use clap::Parser;
 use common::types::chain_id::ChainId;
+use mpc_stark::algebra::scalar::Scalar;
+use rand::thread_rng;
 use starknet_client::client::{StarknetClient, StarknetClientConfig};
-use test_helpers::integration_test_main;
+use test_helpers::{
+    contracts::deploy_darkpool, integration_test_main, mpc_network::await_result_with_error,
+};
 use tracing::log::LevelFilter;
 use util::runtime::await_result;
 
@@ -14,12 +18,42 @@ use util::runtime::await_result;
 ///
 /// This assumes that the integration tests are running in a docker-compose setup
 /// with a DNS alias `sequencer` pointing to a devnet node running in a sister container
-const DEVNET_HOSTPORT: &str = "http://sequencer:5050/rpc";
+const DEVNET_HOSTPORT: &str = "http://sequencer:5050";
 
 /// The arguments used to run the integration tests
 #[derive(Debug, Clone, Parser)]
 #[command(author, version, about, long_about=None)]
 struct CliArgs {
+    /// The private key to use for the Starknet account during testing
+    ///
+    /// Defaults to the first pre-deployed account on `starknet-devnet` when
+    /// run with seed 0
+    #[arg(
+        short = 'p',
+        long,
+        default_value = "0xe3e70682c2094cac629f6fbed82c07cd"
+    )]
+    starknet_pkey: String,
+    /// The address of the account contract to use for testing
+    ///
+    /// Defaults to the first pre-deployed account on `starknet-devnet` when
+    /// run with seed 0
+    #[arg(
+        long,
+        default_value = "0x7e00d496e324876bbc8531f2d9a82bf154d1a04a50218ee74cdd372f75a551a"
+    )]
+    starknet_account_addr: String,
+    /// The address of the darkpool deployed on Starknet at the time the test is started
+    ///
+    /// If not provided, the test will deploy a new darkpool contract
+    #[arg(long)]
+    darkpool_addr: Option<String>,
+    /// The location of the contract compilation artifacts as an absolute path
+    #[arg(long, default_value = "/artifacts")]
+    cairo_artifacts_path: String,
+    /// The url the devnet node api server
+    #[arg(long, default_value = DEVNET_HOSTPORT)]
+    devnet_url: String,
     /// The test to run
     #[arg(short, long, value_parser)]
     test: Option<String>,
@@ -36,16 +70,29 @@ struct IntegrationTestArgs {
 }
 
 impl From<CliArgs> for IntegrationTestArgs {
-    fn from(_: CliArgs) -> Self {
+    fn from(test_args: CliArgs) -> Self {
+        // Deploy a version of the darkpool if one is not given
+        let darkpool_addr = if let Some(addr) = test_args.darkpool_addr {
+            addr
+        } else {
+            await_result_with_error(deploy_darkpool(
+                &test_args.starknet_account_addr,
+                &test_args.starknet_pkey,
+                &test_args.cairo_artifacts_path,
+                &test_args.devnet_url,
+            ))
+            .map(|res| res.to_string())
+            .unwrap()
+        };
+
+        // Build a client that references the darkpool
         let starknet_client = StarknetClient::new(StarknetClientConfig {
             chain: ChainId::Devnet,
-            contract_addr: "".to_string(),
-            // Assumes we are running in a docker-compose setup with a DNS alias `sequencer`
-            // pointing to a devnet node running in a sister container
-            starknet_json_rpc_addr: Some(DEVNET_HOSTPORT.to_string()),
+            contract_addr: darkpool_addr,
+            starknet_json_rpc_addr: Some(format!("{}/rpc", test_args.devnet_url)),
             infura_api_key: None,
-            starknet_account_addresses: vec![],
-            starknet_pkeys: vec![],
+            starknet_account_addresses: vec![test_args.starknet_account_addr],
+            starknet_pkeys: vec![test_args.starknet_pkey],
         });
 
         Self { starknet_client }
@@ -63,13 +110,16 @@ fn setup_integration_tests(_test_args: &CliArgs) {
 /// TODO: Remove this test
 fn test_dummy(test_args: &IntegrationTestArgs) -> Result<(), String> {
     let client = &test_args.starknet_client;
-    let block_num = await_result(client.get_block_number())
-        .map_err(|e| format!("error fetching block number: {e}"))?;
+    let mut rng = thread_rng();
+    let random_nullifier = Scalar::random(&mut rng);
 
-    if block_num == 0 {
+    let res = await_result(client.check_nullifier_unused(random_nullifier))
+        .map_err(|err| format!("error checking nullifier: {}", err))?;
+
+    if res {
         Ok(())
     } else {
-        Err(format!("expected block number to be 0, got {block_num}"))
+        Err("nullifier already used".to_string())
     }
 }
 
