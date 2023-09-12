18a7947f75c082aaa1a662f0fdd115fa6e7c18bb
Joey Kraut <joey@renegade.fi>
2023-09-12 14:38:52 -0700

    statev2: storage: cursor: Add `Cursor` implementation

--- statev2/src/lib.rs
+++ statev2/src/lib.rs
@@ -3,4 +3,26 @@
 //! We store our relayer state in an embedded database using `libmdbx` as the underlying storage
 //! engine. The database is then replicated by a raft instance at higher layers in the application
 
+#![deny(missing_docs)]
+#![deny(clippy::missing_docs_in_private_items)]
+#![deny(unsafe_code)]
+
 pub mod storage;
+
+#[cfg(test)]
+pub(crate) mod test_helpers {
+    use tempfile::tempdir;
+
+    use crate::storage::db::{DbConfig, DB};
+
+    /// Create a mock database in a temporary location
+    pub fn mock_db() -> DB {
+        let tempdir = tempdir().unwrap();
+        let path = tempdir.path().to_str().unwrap();
+
+        DB::new(DbConfig {
+            path: path.to_string(),
+        })
+        .unwrap()
+    }
+}
