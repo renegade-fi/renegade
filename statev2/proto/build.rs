b8bbfe929b8d017077e89c570639d36474f2b14f
Joey Kraut <joey@renegade.fi>
2023-09-21 09:24:04 -0700

    statev2: proto: Derive builder pattern for generates proto messages

--- statev2/proto/build.rs
+++ statev2/proto/build.rs
@@ -1,7 +1,27 @@
+//! Builder for the state crate's protos
+
 use std::io::Result;
+
+use prost_build::Config;
 
 /// Build the protos for the state crate
 fn main() -> Result<()> {
-    prost_build::compile_protos(&["proto/state_transitions.proto"], &["proto/"])?;
+    let mut builder_config = Config::default();
+    // Derive a builder pattern for each generated message
+    // The builder options enabled are:
+    // - Use the owned pattern, i.e. each setter consumes the builder and returns a new one
+    // - Allow uninitialized fields by deferring to the struct default implementation
+    // - Strip options; i.e. setters do no take `Option<T>` but `T` for optional fields, which is the
+    //   case for most prost-generated fields
+    let builder_pattern_targets = ".";
+    builder_config.message_attribute(
+        builder_pattern_targets,
+        "#[derive(derive_builder::Builder)]",
+    );
+    builder_config.message_attribute(builder_pattern_targets, "#[builder(pattern = \"owned\")]");
+    builder_config.message_attribute(builder_pattern_targets, "#[builder(default)]");
+    builder_config.message_attribute(builder_pattern_targets, "#[builder(setter(strip_option))]");
+
+    builder_config.compile_protos(&["proto/state_transitions.proto"], &["proto/"])?;
     Ok(())
 }
