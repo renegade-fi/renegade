1ed1d034d82ae79777ff47232ad553284dda09f2
Joey Kraut <joey@renegade.fi>
2023-11-08 16:59:01 -0800

    renegade-crypto, constants: Gate `ark-mpc` dependency behind feature flag
    In WASM contexts (i.e. contracts) we cannot import `ark-mpc`. Aside we
    only superficially use this crate for its types, so it is unimportant
    to certain functionalities, i.e. hashing.

--- renegade-crypto/src/hash.rs
+++ renegade-crypto/src/hash.rs
@@ -5,55 +5,69 @@ mod poseidon2;
 pub use constants::*;
 pub use poseidon2::*;
 
-use ::constants::Scalar;
-use itertools::Itertools;
+#[cfg(feature = "non-wasm")]
+pub use mpc_type_interface::*;
 
-/// A hash chain from a seed used to compute CSPRNG values
-pub struct PoseidonCSPRNG {
-    /// The seed of the CSPRNG, this is chained into a hash function
-    /// to give pseudorandom values
-    state: Scalar,
-}
+#[cfg(feature = "non-wasm")]
+mod mpc_type_interface {
+    //! Defines the interface for the Poseidon 2 sponge
+    //!
+    //! We gate this behind a feature flag to avoid importing `ark-mpc` when not
+    //! necessary. This is of particular use in wasm contexts, where
+    //! `ark-mpc` does not build
 
-impl PoseidonCSPRNG {
-    /// Constructor
-    pub fn new(seed: Scalar) -> Self {
-        Self { state: seed }
+    use ::constants::Scalar;
+    use itertools::Itertools;
+
+    use super::Poseidon2Sponge;
+
+    /// A hash chain from a seed used to compute CSPRNG values
+    pub struct PoseidonCSPRNG {
+        /// The seed of the CSPRNG, this is chained into a hash function
+        /// to give pseudorandom values
+        state: Scalar,
     }
-}
 
-impl Iterator for PoseidonCSPRNG {
-    type Item = Scalar;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let hash_res = compute_poseidon_hash(&[self.state]);
-        self.state = hash_res;
-
-        Some(hash_res)
+    impl PoseidonCSPRNG {
+        /// Constructor
+        pub fn new(seed: Scalar) -> Self {
+            Self { state: seed }
+        }
     }
-}
 
-/// Compute the hash of the randomness of a given wallet
-pub fn compute_poseidon_hash(values: &[Scalar]) -> Scalar {
-    let input_seq = values.iter().map(Scalar::inner).collect_vec();
-    let mut hasher = Poseidon2Sponge::new();
-    let res = hasher.hash(&input_seq);
+    impl Iterator for PoseidonCSPRNG {
+        type Item = Scalar;
 
-    Scalar::new(res)
-}
+        fn next(&mut self) -> Option<Self::Item> {
+            let hash_res = compute_poseidon_hash(&[self.state]);
+            self.state = hash_res;
 
-/// Compute a chained Poseidon hash of the given length from the given seed
-pub fn evaluate_hash_chain(seed: Scalar, length: usize) -> Vec<Scalar> {
-    let mut seed = seed.inner();
-    let mut res = Vec::with_capacity(length);
+            Some(hash_res)
+        }
+    }
 
-    for _ in 0..length {
-        // Create a new hasher to reset the internal state
+    /// Compute the hash of the randomness of a given wallet
+    pub fn compute_poseidon_hash(values: &[Scalar]) -> Scalar {
+        let input_seq = values.iter().map(Scalar::inner).collect_vec();
         let mut hasher = Poseidon2Sponge::new();
-        seed = hasher.hash(&[seed]);
+        let res = hasher.hash(&input_seq);
 
-        res.push(Scalar::new(seed));
+        Scalar::new(res)
     }
 
-    res
+    /// Compute a chained Poseidon hash of the given length from the given seed
+    pub fn evaluate_hash_chain(seed: Scalar, length: usize) -> Vec<Scalar> {
+        let mut seed = seed.inner();
+        let mut res = Vec::with_capacity(length);
+
+        for _ in 0..length {
+            // Create a new hasher to reset the internal state
+            let mut hasher = Poseidon2Sponge::new();
+            seed = hasher.hash(&[seed]);
+
+            res.push(Scalar::new(seed));
+        }
+
+        res
+    }
 }
