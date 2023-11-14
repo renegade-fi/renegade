eead43d3fdaa2c44c032280b045ed964f0e66521
Joey Kraut <joey@renegade.fi>
2023-11-13 17:18:40 -0800

    circuits: zk-gadgets: bits: Refactor implementation and add tests

--- circuits/src/zk_gadgets/bits.rs
+++ circuits/src/zk_gadgets/bits.rs
@@ -1,19 +1,18 @@
 //! Groups gadgets for going from scalar -> bits and from bits -> scalar
+//!
+//! We do not reuse gadgets between single and multi-prover circuits, because
+//! bit operations require particular care in the multiprover context to be done
+//! efficiently
+
 use std::iter;
 
+use ark_ff::One;
 use bitvec::{order::Lsb0, slice::BitSlice};
-use circuit_types::{
-    errors::ProverError,
-    traits::{LinearCombinationLike, MpcLinearCombinationLike},
-};
+use circuit_types::{traits::CircuitVarType, Fabric, MpcPlonkCircuit, PlonkCircuit};
+use constants::{Scalar, ScalarField};
 use itertools::Itertools;
-use mpc_bulletproof::{
-    r1cs::{LinearCombination, RandomizableConstraintSystem, Variable},
-    r1cs_mpc::{MpcLinearCombination, MpcRandomizableConstraintSystem, R1CSError},
-};
-use mpc_stark::{algebra::scalar::Scalar, MpcFabric};
-use num_bigint::BigInt;
-use renegade_crypto::fields::bigint_to_scalar;
+use mpc_plonk::multiprover::proof_system::MpcCircuit;
+use mpc_relation::{errors::CircuitError, BoolVar, ConstraintSystem, Variable};
 
 use crate::mpc_gadgets::bits::to_bits_le;
 
@@ -35,26 +34,8 @@ pub struct ToBitsGadget<const D: usize> {}
 impl<const D: usize> ToBitsGadget<D> {
     /// Converts a value to its bitwise representation in a single-prover
     /// constraint system
-    pub fn to_bits<L, CS>(a: L, cs: &mut CS) -> Result<Vec<Variable>, R1CSError>
-    where
-        CS: RandomizableConstraintSystem,
-        L: LinearCombinationLike,
-    {
-        let a_scalar = cs.eval(&a.clone().into());
-        let bits = &scalar_to_bits_le::<D>(&a_scalar)[..D];
-
-        let mut reconstructed = LinearCombination::default();
-        let mut res_bits = Vec::with_capacity(D);
-        for (index, bit) in bits.iter().enumerate() {
-            let bit_lc = cs.allocate(Some(*bit))?;
-            res_bits.push(bit_lc);
-
-            let shift_bit = bigint_to_scalar(&(BigInt::from(1u64) << index));
-            reconstructed += shift_bit * bit_lc;
-        }
-
-        cs.constrain(reconstructed - a.into());
-        Ok(res_bits)
+    pub fn to_bits(a: Variable, cs: &mut PlonkCircuit) -> Result<Vec<BoolVar>, CircuitError> {
+        cs.unpack(a, D)
     }
 }
 
@@ -64,50 +45,54 @@ impl<const D: usize> ToBitsGadget<D> {
 pub struct MultiproverToBitsGadget<const D: usize>;
 impl<const D: usize> MultiproverToBitsGadget<D> {
     /// Converts a value into its bitwise representation
-    pub fn to_bits<L, CS>(
-        a: L,
-        fabric: &MpcFabric,
-        cs: &mut CS,
-    ) -> Result<Vec<MpcLinearCombination>, ProverError>
-    where
-        CS: MpcRandomizableConstraintSystem,
-        L: MpcLinearCombinationLike,
-    {
+    pub fn to_bits(
+        a: Variable,
+        fabric: &Fabric,
+        cs: &mut MpcPlonkCircuit,
+    ) -> Result<Vec<Variable>, CircuitError> {
         // Evaluate the linear combination so that we can use a raw MPC to get the bits
-        let a_scalar = cs.eval(&a.clone().into());
+        let a_scalar = a.eval_multiprover(cs);
 
         // Convert the scalar to bits in a raw MPC gadget
         let bits = to_bits_le::<D /* bits */>(&a_scalar, fabric);
 
         // Allocate the bits in the constraint system, and constrain their inner product
         // with 1, 2, 4, ..., 2^{D-1} to be equal to the input value
-        let mut reconstructed = MpcLinearCombination::default();
-        let mut res_bits = Vec::with_capacity(D);
-        for (index, bit) in bits.into_iter().enumerate() {
-            let bit_lc = cs.allocate(Some(bit)).map_err(ProverError::R1CS)?;
-            res_bits.push(bit_lc.clone().into());
+        let bit_vars = bits
+            .into_iter()
+            .map(|bit| cs.create_boolean_variable(bit).map(Into::into))
+            .collect::<Result<Vec<_>, _>>()?;
 
-            let shift_bit = bigint_to_scalar(&(BigInt::from(1u64) << index));
-            reconstructed += shift_bit * bit_lc;
-        }
+        let coeffs = (0..D)
+            .scan(ScalarField::one(), |state, _| {
+                let res = *state;
+                *state *= ScalarField::from(2u64);
+                Some(res)
+            })
+            .collect_vec();
 
-        cs.constrain(reconstructed - a.into());
+        let reconstructed = cs.lc_sum(&bit_vars, &coeffs)?;
+        cs.enforce_equal(reconstructed, a)?;
 
-        Ok(res_bits)
+        Ok(bit_vars)
     }
 }
 
 #[cfg(test)]
 mod bits_test {
-    use circuit_types::traits::CircuitBaseType;
-    use merlin::HashChainTranscript as Transcript;
-    use mpc_bulletproof::{
-        r1cs::{ConstraintSystem, Prover},
-        PedersenGens,
+    use ark_mpc::PARTY0;
+    use circuit_types::{
+        traits::{CircuitBaseType, MpcBaseType, MultiproverCircuitBaseType},
+        MpcPlonkCircuit, PlonkCircuit,
     };
-    use mpc_stark::algebra::scalar::Scalar;
+    use constants::Scalar;
+    use mpc_plonk::multiprover::proof_system::MpcCircuit;
+    use mpc_relation::{Circuit, ConstraintSystem};
     use rand::{thread_rng, RngCore};
     use renegade_crypto::fields::{bigint_to_scalar_bits, scalar_to_bigint};
+    use test_helpers::mpc_network::execute_mock_mpc;
+
+    use crate::zk_gadgets::bits::MultiproverToBitsGadget;
 
     use super::ToBitsGadget;
 
@@ -120,19 +105,54 @@ mod bits_test {
 
         // Create the statement by bitifying the input
         let witness = Scalar::from(random_value);
-        let mut bits = bigint_to_scalar_bits::<64 /* bits */>(&scalar_to_bigint(&witness));
+        let bits = bigint_to_scalar_bits::<64 /* bits */>(&scalar_to_bigint(&witness));
 
         // Create a constraint system
-        let pc_gens = PedersenGens::default();
-        let mut transcript = Transcript::new(b"test");
-        let mut prover = Prover::new(&pc_gens, &mut transcript);
+        let mut cs = PlonkCircuit::new_turbo_plonk();
 
         // Bitify the input
-        let input_var = witness.commit_public(&mut prover);
-        let res = ToBitsGadget::<64 /* bits */>::to_bits(input_var, &mut prover).unwrap();
+        let input_var = witness.create_witness(&mut cs);
+        let res = ToBitsGadget::<64 /* bits */>::to_bits(input_var, &mut cs).unwrap();
 
-        for bit in res.into_iter().rev().map(|v| prover.eval(&v.into())) {
-            assert_eq!(bit, bits.pop().unwrap());
+        for (bit, expected) in res.into_iter().zip(bits.into_iter()) {
+            cs.enforce_constant(bit.into(), expected.inner()).unwrap();
         }
+
+        // Check that the constraint system is satisfied
+        assert!(cs.check_circuit_satisfiability(&[]).is_ok());
+    }
+
+    /// Tests the multiprover to_bits gadget
+    #[tokio::test]
+    async fn test_to_bits_multiprover() {
+        // Create a random input to bitify
+        let mut rng = thread_rng();
+        let random_value = rng.next_u64();
+
+        // Create the statement by bitifying the input
+        let witness = Scalar::from(random_value);
+        let bits = bigint_to_scalar_bits::<64 /* bits */>(&scalar_to_bigint(&witness));
+
+        // Create a constraint system
+        let (res, _) = execute_mock_mpc(move |fabric| {
+            let bits = bits.clone();
+            async move {
+                let mut cs = MpcPlonkCircuit::new(fabric.clone());
+                let val = witness.allocate(PARTY0, &fabric);
+                let input_var = val.create_shared_witness(&mut cs).unwrap();
+
+                let res =
+                    MultiproverToBitsGadget::<64 /* bits */>::to_bits(input_var, &fabric, &mut cs)
+                        .unwrap();
+                for (bit, expected) in res.into_iter().zip(bits.into_iter()) {
+                    cs.enforce_constant(bit, expected.inner()).unwrap();
+                }
+
+                cs.check_circuit_satisfiability(&[]).await
+            }
+        })
+        .await;
+
+        assert!(res.is_ok());
     }
 }
