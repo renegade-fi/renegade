469aaf94a74c5f68f9087f2e28ea3c4c21d97c05
Andrew Kirillov <andrew@kirillov.org>
2023-11-16 13:46:53 -0800

    arbitrum-client: create ArbitrumClient struct

--- arbitrum-client/src/lib.rs
+++ arbitrum-client/src/lib.rs
@@ -5,5 +5,9 @@
 //! RPC endpoint, contract address, etc.; executing transactions /
 //! calling functions on the smart contracts; indexing through events; etc.
 
+#![deny(unsafe_code)]
+#![deny(missing_docs)]
+#![deny(clippy::missing_docs_in_private_items)]
+
 mod abi;
 pub mod client;
