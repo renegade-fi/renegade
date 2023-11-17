885384c5dab2b813ebcd9847515f1eb2ef9c8c9b
Joey Kraut <joey@renegade.fi>
2023-11-17 14:01:09 -0800

    circuits: zk-gadgets: wallet-operations: Refactor and add unit tests

--- circuits/src/zk_gadgets/wallet_operations.rs
+++ circuits/src/zk_gadgets/wallet_operations.rs
@@ -1,16 +1,9 @@
 //! Groups logic for computing wallet commitments and nullifiers inside of a
 //! circuit
 
-use circuit_types::{
-    traits::{CircuitVarType, LinearCombinationLike},
-    wallet::WalletShareVar,
-};
-use itertools::Itertools;
-use mpc_bulletproof::{
-    r1cs::{LinearCombination, RandomizableConstraintSystem},
-    r1cs_mpc::R1CSError,
-};
-use renegade_crypto::hash::default_poseidon_params;
+use circuit_types::{traits::CircuitVarType, wallet::WalletShareVar};
+use constants::ScalarField;
+use mpc_relation::{errors::CircuitError, traits::Circuit, Variable};
 
 use super::poseidon::PoseidonHashGadget;
 
@@ -31,64 +24,42 @@ where
     [(); MAX_BALANCES + MAX_ORDERS + MAX_FEES]: Sized,
 {
     /// Compute the commitment to the private wallet shares
-    pub fn compute_private_commitment<
-        L: LinearCombinationLike,
-        CS: RandomizableConstraintSystem,
-    >(
-        private_wallet_share: WalletShareVar<L, MAX_BALANCES, MAX_ORDERS, MAX_FEES>,
-        cs: &mut CS,
-    ) -> Result<LinearCombination, R1CSError> {
-        // Create a new hash gadget
-        let hash_params = default_poseidon_params();
-        let mut hasher = PoseidonHashGadget::new(hash_params);
-
+    pub fn compute_private_commitment<C: Circuit<ScalarField>>(
+        private_wallet_share: WalletShareVar<MAX_BALANCES, MAX_ORDERS, MAX_FEES>,
+        cs: &mut C,
+    ) -> Result<Variable, CircuitError> {
         // Serialize the wallet and hash it into the hasher's state
-        let serialized_wallet: Vec<L> = private_wallet_share.to_vars();
+        let serialized_wallet = private_wallet_share.to_vars();
+
+        let mut hasher = PoseidonHashGadget::new(cs.zero());
         hasher.batch_absorb(&serialized_wallet, cs)?;
 
-        // Squeeze an element out of the state
         hasher.squeeze(cs)
     }
 
     /// Compute the commitment to the full wallet given a commitment to the
     /// private shares
-    pub fn compute_wallet_commitment_from_private<
-        L: LinearCombinationLike,
-        CS: RandomizableConstraintSystem,
-    >(
-        blinded_public_wallet_share: WalletShareVar<L, MAX_BALANCES, MAX_ORDERS, MAX_FEES>,
-        private_commitment: LinearCombination,
-        cs: &mut CS,
-    ) -> Result<LinearCombination, R1CSError> {
-        // Create a new hash gadget
-        let hash_params = default_poseidon_params();
-        let mut hasher = PoseidonHashGadget::new(hash_params);
-
+    pub fn compute_wallet_commitment_from_private<C: Circuit<ScalarField>>(
+        blinded_public_wallet_share: WalletShareVar<MAX_BALANCES, MAX_ORDERS, MAX_FEES>,
+        private_commitment: Variable,
+        cs: &mut C,
+    ) -> Result<Variable, CircuitError> {
         // The public shares are added directly to a sponge H(private_commit || public
-        // shares)
-        let mut hash_input = vec![private_commitment];
-        hash_input.append(
-            &mut blinded_public_wallet_share
-                .to_vars()
-                .into_iter()
-                .map(|var| var.into())
-                .collect_vec(),
-        );
+        // shares), giving the full wallet commitment
+        let mut hasher = PoseidonHashGadget::new(cs.zero());
+        hasher.absorb(private_commitment, cs)?;
+        hasher.batch_absorb(&blinded_public_wallet_share.to_vars(), cs)?;
 
-        hasher.batch_absorb(&hash_input, cs)?;
         hasher.squeeze(cs)
     }
 
     /// Compute the full commitment of a wallet's shares given both the public
     /// and private shares
-    pub fn compute_wallet_share_commitment<
-        L: LinearCombinationLike,
-        CS: RandomizableConstraintSystem,
-    >(
-        public_wallet_share: WalletShareVar<L, MAX_BALANCES, MAX_ORDERS, MAX_FEES>,
-        private_wallet_share: WalletShareVar<L, MAX_BALANCES, MAX_ORDERS, MAX_FEES>,
-        cs: &mut CS,
-    ) -> Result<LinearCombination, R1CSError> {
+    pub fn compute_wallet_share_commitment<C: Circuit<ScalarField>>(
+        public_wallet_share: WalletShareVar<MAX_BALANCES, MAX_ORDERS, MAX_FEES>,
+        private_wallet_share: WalletShareVar<MAX_BALANCES, MAX_ORDERS, MAX_FEES>,
+        cs: &mut C,
+    ) -> Result<Variable, CircuitError> {
         // First compute the private half, then absorb in the public
         let private_comm = Self::compute_private_commitment(private_wallet_share, cs)?;
         Self::compute_wallet_commitment_from_private(public_wallet_share, private_comm, cs)
@@ -100,20 +71,127 @@ where
 pub struct NullifierGadget {}
 impl NullifierGadget {
     /// Compute the nullifier of a set of secret shares given their commitment
-    pub fn wallet_shares_nullifier<L, CS>(
-        share_commitment: L,
-        wallet_blinder: L,
-        cs: &mut CS,
-    ) -> Result<LinearCombination, R1CSError>
-    where
-        L: LinearCombinationLike,
-        CS: RandomizableConstraintSystem,
-    {
+    pub fn wallet_shares_nullifier<C: Circuit<ScalarField>>(
+        share_commitment: Variable,
+        wallet_blinder: Variable,
+        cs: &mut C,
+    ) -> Result<Variable, CircuitError> {
         // The nullifier is computed as H(C(w)||r)
-        let hash_params = default_poseidon_params();
-        let mut hasher = PoseidonHashGadget::new(hash_params);
+        let mut hasher = PoseidonHashGadget::new(cs.zero());
 
         hasher.batch_absorb(&[share_commitment, wallet_blinder], cs)?;
         hasher.squeeze(cs)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::iter;
+
+    use circuit_types::{
+        native_helpers::{
+            compute_wallet_commitment_from_private, compute_wallet_private_share_commitment,
+            compute_wallet_share_commitment, compute_wallet_share_nullifier,
+        },
+        traits::{BaseType, CircuitBaseType},
+        PlonkCircuit, SizedWalletShare,
+    };
+    use constants::Scalar;
+    use mpc_relation::traits::Circuit;
+    use rand::thread_rng;
+
+    use crate::zk_gadgets::wallet_operations::WalletShareCommitGadget;
+
+    use super::NullifierGadget;
+
+    /// Generate random wallet shares
+    fn random_wallet_shares() -> (SizedWalletShare, SizedWalletShare) {
+        let mut rng = thread_rng();
+        let mut share_iter = iter::from_fn(|| Some(Scalar::random(&mut rng)));
+
+        (
+            SizedWalletShare::from_scalars(&mut share_iter),
+            SizedWalletShare::from_scalars(&mut share_iter),
+        )
+    }
+
+    /// Tests the wallet commitment share gadget
+    #[test]
+    fn test_wallet_share_commitments() {
+        let (private_shares, public_shares) = random_wallet_shares();
+
+        let mut cs = PlonkCircuit::new_turbo_plonk();
+        let private_share_var = private_shares.create_witness(&mut cs);
+        let public_share_var = public_shares.create_witness(&mut cs);
+
+        // Private share commitment
+        let expected_private = compute_wallet_private_share_commitment(private_shares.clone());
+        let expected_var = expected_private.create_public_var(&mut cs);
+
+        let priv_comm =
+            WalletShareCommitGadget::compute_private_commitment(private_share_var.clone(), &mut cs)
+                .unwrap();
+
+        cs.enforce_equal(priv_comm, expected_var).unwrap();
+
+        // Public share commitment
+        let expected_pub =
+            compute_wallet_commitment_from_private(public_shares.clone(), expected_private);
+        let expected_var = expected_pub.create_public_var(&mut cs);
+
+        let pub_comm = WalletShareCommitGadget::compute_wallet_commitment_from_private(
+            public_share_var.clone(),
+            priv_comm,
+            &mut cs,
+        )
+        .unwrap();
+
+        cs.enforce_equal(pub_comm, expected_var).unwrap();
+
+        // Full wallet commitment
+        let expected_full = compute_wallet_share_commitment(public_shares, private_shares);
+        let expected_var = expected_full.create_public_var(&mut cs);
+
+        let full_comm = WalletShareCommitGadget::compute_wallet_share_commitment(
+            public_share_var,
+            private_share_var,
+            &mut cs,
+        );
+
+        cs.enforce_equal(full_comm.unwrap(), expected_var).unwrap();
+
+        // Verify that all constraints are satisfied
+        assert!(cs
+            .check_circuit_satisfiability(&[
+                expected_private.inner(),
+                expected_pub.inner(),
+                expected_full.inner()
+            ])
+            .is_ok())
+    }
+
+    /// Tests the nullifier gadget
+    #[test]
+    fn test_nullifier_gadget() {
+        let mut rng = thread_rng();
+        let share_commitment = Scalar::random(&mut rng);
+        let wallet_blinder = Scalar::random(&mut rng);
+
+        let expected = compute_wallet_share_nullifier(share_commitment, wallet_blinder);
+
+        // Check against the gadget
+        let mut cs = PlonkCircuit::new_turbo_plonk();
+        let comm_var = share_commitment.create_witness(&mut cs);
+        let blinder_var = wallet_blinder.create_witness(&mut cs);
+
+        let expected_var = expected.create_public_var(&mut cs);
+
+        let nullifier =
+            NullifierGadget::wallet_shares_nullifier(comm_var, blinder_var, &mut cs).unwrap();
+
+        cs.enforce_equal(nullifier, expected_var).unwrap();
+
+        // Verify that all constraints are satisfied
+        assert!(cs.check_circuit_satisfiability(&[expected.inner()]).is_ok())
     }
 }
