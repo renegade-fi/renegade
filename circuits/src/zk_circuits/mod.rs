b0e730241d00c6a7f8cdbbf25d00c66368d0e305
Joey Kraut <joey@renegade.fi>
2023-11-19 13:19:38 -0800

    circuits: zk-circuits: valid-commitments: Refactor over `mpc-plonk`

--- circuits/src/zk_circuits/mod.rs
+++ circuits/src/zk_circuits/mod.rs
@@ -2,7 +2,7 @@
 //! in proving knowledge of witness for throughout the network
 #![allow(missing_docs, clippy::missing_docs_in_private_items)]
 
-// pub mod valid_commitments;
+pub mod valid_commitments;
 // pub mod valid_match_mpc;
 // pub mod valid_reblind;
 // pub mod valid_settle;
