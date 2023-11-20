bd11be694dae5f7612c5214e36576588f828a1c8
Andrew Kirillov <andrew@kirillov.org>
2023-11-20 14:17:22 -0800

    arbitrum-client: fetch_public_shares_from_tx

--- arbitrum-client/src/helpers.rs
+++ arbitrum-client/src/helpers.rs
@@ -1,5 +1,7 @@
 //! Various helpers for Arbitrum client execution
 
+use alloy_sol_types::SolCall;
+use circuit_types::{traits::BaseType, SizedWalletShare};
 use constants::Scalar;
 use ethers::{
     types::{Bytes, H256},
@@ -7,7 +9,15 @@ use ethers::{
 };
 use serde::{Deserialize, Serialize};
 
-use crate::{errors::ArbitrumClientError, serde_def_types::SerdeScalarField};
+use crate::{
+    abi::{newWalletCall, processMatchSettleCall, updateWalletCall},
+    errors::ArbitrumClientError,
+    serde_def_types::SerdeScalarField,
+    types::{
+        MatchPayload, ValidMatchSettleStatement, ValidWalletCreateStatement,
+        ValidWalletUpdateStatement,
+    },
+};
 
 /// Serializes a calldata element for a contract call
 pub fn serialize_calldata<T: Serialize>(data: &T) -> Result<Bytes, ArbitrumClientError> {
@@ -17,10 +27,10 @@ pub fn serialize_calldata<T: Serialize>(data: &T) -> Result<Bytes, ArbitrumClien
 }
 
 /// Deserializes a return value from a contract call
-pub fn deserialize_retdata<'de, T: Deserialize<'de>>(
-    retdata: &'de Bytes,
+pub fn deserialize_calldata<'de, T: Deserialize<'de>>(
+    calldata: &'de Bytes,
 ) -> Result<T, ArbitrumClientError> {
-    postcard::from_bytes(retdata).map_err(|e| ArbitrumClientError::Serde(e.to_string()))
+    postcard::from_bytes(calldata).map_err(|e| ArbitrumClientError::Serde(e.to_string()))
 }
 
 /// Computes the Keccak-256 hash of the serialization of a scalar,
@@ -28,4 +38,63 @@ pub fn deserialize_retdata<'de, T: Deserialize<'de>>(
 pub fn keccak_hash_scalar(scalar: Scalar) -> Result<H256, ArbitrumClientError> {
     let scalar_bytes = serialize_calldata(&SerdeScalarField(scalar.inner()))?;
     Ok(keccak256(scalar_bytes).into())
+}
+
+pub fn parse_shares_from_new_wallet(
+    calldata: &[u8],
+) -> Result<SizedWalletShare, ArbitrumClientError> {
+    let call = newWalletCall::decode(&calldata, true /* validate */)
+        .map_err(|e| ArbitrumClientError::Serde(e.to_string()))?;
+
+    let mut statement = deserialize_calldata::<ValidWalletCreateStatement>(
+        &call.valid_wallet_create_statement_bytes.into(),
+    )?;
+
+    Ok(SizedWalletShare::from_scalars(
+        &mut statement.public_wallet_shares,
+    ))
+}
+
+pub fn parse_shares_from_update_wallet(
+    calldata: &[u8],
+) -> Result<SizedWalletShare, ArbitrumClientError> {
+    let call = updateWalletCall::decode(&calldata, true /* validate */)
+        .map_err(|e| ArbitrumClientError::Serde(e.to_string()))?;
+
+    let mut statement = deserialize_calldata::<ValidWalletUpdateStatement>(
+        &call.valid_wallet_update_statement_bytes.into(),
+    )?;
+
+    Ok(SizedWalletShare::from_scalars(
+        &mut statement.new_public_shares,
+    ))
+}
+
+pub fn parse_shares_from_process_match_settle(
+    calldata: &[u8],
+) -> Result<SizedWalletShare, ArbitrumClientError> {
+    let call = processMatchSettleCall::decode(&calldata, true /* validate */)
+        .map_err(|e| ArbitrumClientError::Serde(e.to_string()))?;
+
+    let party_0_match_payload =
+        deserialize_calldata::<MatchPayload>(&call.party_0_match_payload.into())?;
+    let party_1_match_payload =
+        deserialize_calldata::<MatchPayload>(&call.party_1_match_payload.into())?;
+
+    let mut valid_match_settle_statement = deserialize_calldata::<ValidMatchSettleStatement>(
+        &call.valid_match_settle_statement_bytes.into(),
+    )?;
+
+    let target_share = public_blinder_share.inner();
+    if party_0_match_payload.wallet_blinder_share == target_share {
+        Ok(SizedWalletShare::from_scalars(
+            &mut valid_match_settle_statement.party0_modified_shares,
+        ))
+    } else if party_1_match_payload.wallet_blinder_share == target_share {
+        Ok(SizedWalletShare::from_scalars(
+            &mut valid_match_settle_statement.party1_modified_shares,
+        ))
+    } else {
+        Err(ArbitrumClientError::BlinderNotFound)
+    }
 }
