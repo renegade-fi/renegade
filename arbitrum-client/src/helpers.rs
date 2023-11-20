1d468cdc0bb54969e7dd25da750c81512836a78d
Andrew Kirillov <andrew@kirillov.org>
2023-11-20 11:49:26 -0800

    arbitrum-client: find_merkle_authentication_path, find_commitment_in_state

--- arbitrum-client/src/helpers.rs
+++ arbitrum-client/src/helpers.rs
@@ -1,9 +1,13 @@
 //! Various helpers for Arbitrum client execution
 
-use ethers::types::Bytes;
+use constants::Scalar;
+use ethers::{
+    types::{Bytes, H256},
+    utils::keccak256,
+};
 use serde::{Deserialize, Serialize};
 
-use crate::errors::ArbitrumClientError;
+use crate::{errors::ArbitrumClientError, serde_def_types::SerdeScalarField};
 
 /// Serializes a calldata element for a contract call
 pub fn serialize_calldata<T: Serialize>(data: &T) -> Result<Bytes, ArbitrumClientError> {
@@ -17,4 +21,11 @@ pub fn deserialize_retdata<'de, T: Deserialize<'de>>(
     retdata: &'de Bytes,
 ) -> Result<T, ArbitrumClientError> {
     postcard::from_bytes(retdata).map_err(|e| ArbitrumClientError::Serde(e.to_string()))
+}
+
+/// Computes the Keccak-256 hash of the serialization of a scalar,
+/// used for filtering events that have indexed scalar topics
+pub fn keccak_hash_scalar(scalar: Scalar) -> Result<H256, ArbitrumClientError> {
+    let scalar_bytes = serialize_calldata(&SerdeScalarField(scalar.inner()))?;
+    Ok(keccak256(scalar_bytes).into())
 }
