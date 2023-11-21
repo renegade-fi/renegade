09c40a97728ffe7c6bf656e3eace540dca349ece
Andrew Kirillov <andrew@kirillov.org>
2023-11-21 09:39:33 -0800

    meta: comment out broken modules

--- common/src/types.rs
+++ common/src/types.rs
@@ -1,13 +1,13 @@
 //! Defines common types that many crates can depend on
 pub mod exchange;
 pub mod gossip;
-pub mod handshake;
+// pub mod handshake;
 pub mod merkle;
-pub mod network_order;
-pub mod proof_bundles;
+// pub mod network_order;
+// pub mod proof_bundles;
 pub mod tasks;
 pub mod token;
-pub mod wallet;
+// pub mod wallet;
 
 // Re-export the mock types
 #[cfg(feature = "mocks")]
