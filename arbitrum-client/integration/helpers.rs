45496303278eaabea9baf97e3b72126490315d2b
Andrew Kirillov <andrew@kirillov.org>
2023-11-25 07:32:38 -0800

    arbitrum-client: add integration test setup

---
+++ arbitrum-client/integration/helpers.rs
@@ -0,0 +1,18 @@
+//! Helper functions for Arbitrum client integration tests
+
+use std::{fs::File, io::Read};
+use eyre::{Result, eyre};
+
+use crate::constants::DEPLOYMENTS_KEY;
+
+/// Parse a `deployments.json` file to get the address of a deployed contract
+pub fn parse_addr_from_deployments_file(file_path: &str, contract_key: &str) -> Result<String> {
+    let mut file_contents = String::new();
+    File::open(file_path)?.read_to_string(&mut file_contents)?;
+
+    let parsed_json = json::parse(&file_contents)?;
+    parsed_json[DEPLOYMENTS_KEY][contract_key]
+        .as_str()
+        .map(|s| s.to_string())
+        .ok_or_else(|| eyre!("Could not parse darkpool address from deployments file"))
+}
