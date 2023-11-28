74a9a06f415f76c3adfcb5db0c9355c6f0993905
Joey Kraut <joey@renegade.fi>
2023-11-28 11:26:16 -0800

    circuits: benches: Update match and settlement benchmarks

--- circuits/benches/match.rs
+++ circuits/benches/match.rs
@@ -1,36 +1,33 @@
-//! Groups integration tests for matching an order and proving `VALID MATCH MPC`
-//! collaboratively
-//!
-//! TODO: Benchmark with various simulated latencies
+//! Groups integration tests for matching an order and proving `VALID MATCH
+//! SETTLE` collaboratively
 
 #![feature(generic_const_exprs)]
 #![allow(incomplete_features)]
 
 use std::time::{Duration, Instant};
 
+use ark_mpc::{PARTY0, PARTY1};
 use circuit_types::{
     balance::Balance,
     fixed_point::FixedPoint,
     order::Order,
-    r#match::LinkableMatchResult,
-    traits::{
-        LinkableBaseType, MpcBaseType, MpcType, MultiProverCircuit, MultiproverCircuitBaseType,
-    },
+    r#match::MatchResult,
+    traits::{MpcBaseType, MpcType, MultiProverCircuit, MultiproverCircuitBaseType},
+    MpcPlonkCircuit,
 };
 use circuits::{
     mpc_circuits::r#match::compute_match,
-    multiprover_prove, singleprover_prove, verify_singleprover_proof,
-    zk_circuits::valid_match_mpc::{
-        test_helpers::create_dummy_witness, ValidMatchMpcCircuit, ValidMatchMpcSingleProver,
-        ValidMatchMpcWitness,
+    multiprover_prove, singleprover_prove,
+    test_helpers::{dummy_wallet_share, random_indices},
+    verify_singleprover_proof,
+    zk_circuits::valid_match_settle::{
+        SizedValidMatchSettle, ValidMatchSettle, ValidMatchSettleStatement, ValidMatchSettleWitness,
     },
 };
+use constants::{Scalar, MAX_BALANCES, MAX_FEES, MAX_ORDERS};
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 
-use merlin::HashChainTranscript;
-use mpc_bulletproof::{r1cs_mpc::MpcProver, PedersenGens};
-use mpc_stark::{algebra::scalar::Scalar, PARTY0, PARTY1};
-use rand::thread_rng;
+use mpc_relation::traits::Circuit;
 use test_helpers::mpc_network::execute_mock_mpc_with_delay;
 use tokio::runtime::Builder as RuntimeBuilder;
 
@@ -46,29 +43,32 @@ const LARGE_DELAY_MS: u64 = 100;
 // -----------
 // | Helpers |
 // -----------
-
-/// Get a dummy, single-prover witness for `VALID MATCH MPC`
-pub fn get_dummy_singleprover_witness() -> ValidMatchMpcWitness {
-    // Generate a proof that will be used by the benchmarks to verify
-    ValidMatchMpcWitness {
-        order1: Order::default().to_linkable(),
-        order2: Order::default().to_linkable(),
-        balance1: Balance::default().to_linkable(),
-        balance2: Balance::default().to_linkable(),
-        amount1: Scalar::one(),
-        amount2: Scalar::one(),
-        price1: FixedPoint::from_integer(1),
-        price2: FixedPoint::from_integer(1),
-        match_res: LinkableMatchResult {
-            quote_mint: Scalar::one().to_linkable(),
-            base_mint: Scalar::one().to_linkable(),
-            quote_amount: Scalar::one().to_linkable(),
-            base_amount: Scalar::one().to_linkable(),
-            direction: Scalar::one().to_linkable(),
-            max_minus_min_amount: Scalar::one().to_linkable(),
-            min_amount_order_index: Scalar::one().to_linkable(),
+/// Get a dummy witness and statement for `VALID MATCH SETTLE`
+fn dummy_witness_statement() -> (
+    ValidMatchSettleWitness<MAX_BALANCES, MAX_ORDERS, MAX_FEES>,
+    ValidMatchSettleStatement<MAX_BALANCES, MAX_ORDERS, MAX_FEES>,
+) {
+    (
+        ValidMatchSettleWitness {
+            order1: Order::default(),
+            balance1: Balance::default(),
+            amount1: Scalar::zero(),
+            price1: FixedPoint { repr: Scalar::zero() },
+            order2: Order::default(),
+            balance2: Balance::default(),
+            amount2: Scalar::zero(),
+            price2: FixedPoint { repr: Scalar::zero() },
+            match_res: MatchResult::default(),
+            party0_public_shares: dummy_wallet_share(),
+            party1_public_shares: dummy_wallet_share(),
         },
-    }
+        ValidMatchSettleStatement {
+            party0_modified_shares: dummy_wallet_share(),
+            party1_modified_shares: dummy_wallet_share(),
+            party0_indices: random_indices(),
+            party1_indices: random_indices(),
+        },
+    )
 }
 
 /// Benchmark the time taken to run the raw `match` MPC circuits with a given
@@ -87,15 +87,13 @@ pub fn bench_match_mpc_with_delay(c: &mut Criterion, delay: Duration) {
                     |fabric| async move {
                         // Allocate the inputs in the fabric
                         let start = Instant::now();
-                        let o1 = Order::default().to_linkable().allocate(PARTY0, &fabric);
-                        let o2 = Order::default().to_linkable().allocate(PARTY1, &fabric);
+                        let o1 = Order::default().allocate(PARTY0, &fabric);
                         let amount1 = Scalar::one().allocate(PARTY0, &fabric);
                         let amount2 = Scalar::one().allocate(PARTY1, &fabric);
                         let price = FixedPoint::from_integer(1).allocate(PARTY0, &fabric);
 
                         // Run the MPC
-                        let match_res =
-                            compute_match(&o1, &o2, &amount1, &amount2, &price, &fabric);
+                        let match_res = compute_match(&o1, &amount1, &amount2, &price, &fabric);
 
                         // Open the result
                         let _open = match_res.open_and_authenticate().await;
@@ -128,26 +126,26 @@ pub fn bench_apply_constraints_with_delay(c: &mut Criterion, delay: Duration) {
                 // Execute an MPC to generate the constraints
                 let (party0_time, party1_time) = execute_mock_mpc_with_delay(
                     |fabric| async move {
-                        // Create a witness to the proof
-                        let witness = create_dummy_witness(&fabric);
+                        // Build a witness and statement
+                        let (witness, statement) = dummy_witness_statement();
+                        let witness = witness.allocate(PARTY0, &fabric);
+                        let statement = statement.allocate(PARTY0, &fabric);
 
                         // Create a constraint system to allocate the constraints within
-                        let pc_gens = PedersenGens::default();
-                        let transcript = HashChainTranscript::new(b"test");
-                        let mut prover =
-                            MpcProver::new_with_fabric(fabric.clone(), transcript, pc_gens);
+                        let mut cs = MpcPlonkCircuit::new(fabric.clone());
 
                         // Start the measurement after the setup code
                         let start = Instant::now();
 
                         // Allocate the inputs in the constraint system
-                        let (witness_var, _) =
-                            witness.commit_shared(&mut thread_rng(), &mut prover).unwrap();
-                        ValidMatchMpcCircuit::apply_constraints_multiprover(
+                        let witness_var = witness.create_shared_witness(&mut cs);
+                        let statement_var = statement.create_shared_public_var(&mut cs);
+
+                        ValidMatchSettle::apply_constraints_multiprover(
                             witness_var,
-                            (),
-                            fabric,
-                            &mut prover,
+                            statement_var,
+                            &fabric,
+                            &mut cs,
                         )
                         .unwrap();
 
@@ -158,7 +156,8 @@ pub fn bench_apply_constraints_with_delay(c: &mut Criterion, delay: Duration) {
                         // measure time taken to generate constraints and prove, so they more
                         // directly estimate constraint generation
                         // latency, but as part of a larger circuit
-                        let _satisfied = prover.constraints_satisfied().await;
+                        let statement_vals = statement.to_authenticated_scalars();
+                        let _satisfied = cs.check_circuit_satisfiability(&statement_vals);
                         start.elapsed()
                     },
                     delay,
@@ -188,21 +187,23 @@ pub fn bench_prover_latency_with_delay(c: &mut Criterion, delay: Duration) {
                 // Execute an MPC to generate the constraints
                 let (party0_time, party1_time) = execute_mock_mpc_with_delay(
                     |fabric| async move {
-                        // Create a witness to the proof
-                        let witness = create_dummy_witness(&fabric);
+                        // Build a witness and statement
+                        let (witness, statement) = dummy_witness_statement();
+                        let witness = witness.allocate(PARTY0, &fabric);
+                        let statement = statement.allocate(PARTY0, &fabric);
 
                         // Start the measurement after the setup code
                         let start = Instant::now();
 
                         // Allocate the inputs in the constraint system
-                        let (_comm, proof) = multiprover_prove::<ValidMatchMpcCircuit>(
-                            witness.clone(),
-                            (), // statement
-                            fabric,
+                        let proof = multiprover_prove::<
+                            ValidMatchSettle<MAX_BALANCES, MAX_ORDERS, MAX_FEES>,
+                        >(
+                            witness.clone(), statement.clone(), fabric
                         )
                         .unwrap();
 
-                        let _opened_proof = black_box(proof.open().await);
+                        let _opened_proof = black_box(proof.open_authenticated().await);
                         start.elapsed()
                     },
                     delay,
@@ -287,18 +288,15 @@ pub fn bench_prover_latency__large_delay(c: &mut Criterion) {
 /// Benchmarks the verification latency of a `VALID MATCH MPC` statement
 pub fn bench_verifier_latency(c: &mut Criterion) {
     // Create a dummy proof to verify in the benchmark loop
-    let dummy_witness = get_dummy_singleprover_witness();
-    let (witness_comm, proof) =
-        singleprover_prove::<ValidMatchMpcSingleProver>(dummy_witness, ()).unwrap();
+    let (witness, statement) = dummy_witness_statement();
+    let proof = singleprover_prove::<SizedValidMatchSettle>(witness, statement.clone()).unwrap();
 
     let mut group = c.benchmark_group("match-mpc");
     group.bench_function(BenchmarkId::new("verifier", ""), |b| {
         b.iter(|| {
-            assert!(verify_singleprover_proof::<ValidMatchMpcSingleProver>(
-                (), // statement
-                witness_comm.clone(),
-                proof.clone()
-            )
+            assert!(verify_singleprover_proof::<
+                ValidMatchSettle<MAX_BALANCES, MAX_ORDERS, MAX_FEES>,
+            >(statement.clone(), &proof)
             .is_err());
         })
     });
