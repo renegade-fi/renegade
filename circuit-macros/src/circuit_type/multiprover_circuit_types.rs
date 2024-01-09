16a1e95c05825db1cc57ab9f6a2c9aecc4d463fd
Joey Kraut <joey@renegade.fi>
2024-01-09 09:25:17 -0800

    circuit-macros: proof-linking: Add multiprover linking witness alloc

--- circuit-macros/src/circuit_type/multiprover_circuit_types.rs
+++ circuit-macros/src/circuit_type/multiprover_circuit_types.rs
@@ -7,7 +7,10 @@ use syn::{parse_quote, ItemImpl, ItemStruct};
 use crate::circuit_type::{ident_with_suffix, new_ident};
 
 use super::{
-    ident_strip_prefix, ident_with_generics, mpc_types::MPC_TYPE_PREFIX, path_from_ident,
+    ident_strip_prefix, ident_with_generics,
+    mpc_types::MPC_TYPE_PREFIX,
+    path_from_ident,
+    proof_linking::{build_create_shared_witness_method, requires_proof_linking},
     singleprover_circuit_types::VAR_TYPE_ASSOCIATED_NAME,
 };
 
@@ -24,14 +27,17 @@ pub(crate) const VAR_SUFFIX: &str = "Var";
 // --------------------------
 
 /// Build the multiprover circuit types from a base type
-pub(crate) fn build_multiprover_circuit_types(mpc_type: &ItemStruct) -> TokenStream2 {
+pub(crate) fn build_multiprover_circuit_types(
+    mpc_type: &ItemStruct,
+    base_type: &ItemStruct,
+) -> TokenStream2 {
     // Build the variable and commitment types and the trait implementations
     // for committing to the base type in an MPC circuit
-    build_base_type_impl(mpc_type)
+    build_base_type_impl(mpc_type, base_type)
 }
 
 /// Build an `impl MultiproverCircuitBaseType` block
-fn build_base_type_impl(mpc_type: &ItemStruct) -> TokenStream2 {
+fn build_base_type_impl(mpc_type: &ItemStruct, base_type: &ItemStruct) -> TokenStream2 {
     let base_generics = mpc_type.generics.clone();
     let where_clause = mpc_type.generics.where_clause.clone();
 
@@ -53,12 +59,20 @@ fn build_base_type_impl(mpc_type: &ItemStruct) -> TokenStream2 {
     );
     let derived_var_type_ident = ident_with_generics(&var_type_ident, base_generics.clone());
 
+    // Maybe build a `create_shared_witness method`
+    let mut create_shared_witness_method = TokenStream2::new();
+    if requires_proof_linking(base_type) {
+        create_shared_witness_method = build_create_shared_witness_method(mpc_type, base_type);
+    }
+
     let impl_block: ItemImpl = parse_quote! {
         impl #base_generics #trait_name for #mpc_type_name
             #where_clause
         {
             type #base_type_associated_name = #derived_base_type_ident;
             type #var_type_associated_name = #derived_var_type_ident;
+
+            #create_shared_witness_method
         }
     };
     impl_block.to_token_stream()
