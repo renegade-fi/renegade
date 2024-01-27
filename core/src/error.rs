db78c036819d55084fc688f0c1192a0927dc3aa3
Joey Kraut <joey@renegade.fi>
2024-01-27 11:37:52 -0800

    statev2, core: Refactor core crate over the `statev2` interface

--- core/src/error.rs
+++ core/src/error.rs
@@ -3,6 +3,8 @@
 use std::error::Error;
 use std::fmt::Display;
 
+use statev2::error::StateError;
+
 /// An error type at the coordinator level
 #[derive(Clone, Debug)]
 pub enum CoordinatorError {
@@ -12,11 +14,19 @@ pub enum CoordinatorError {
     Recovery(String),
     /// Failure to send a cancel signal to a worker
     CancelSend(String),
+    /// An error setting up global state
+    State(String),
 }
 
 impl Error for CoordinatorError {}
 impl Display for CoordinatorError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{:?}", self)
+    }
+}
+
+impl From<StateError> for CoordinatorError {
+    fn from(value: StateError) -> Self {
+        CoordinatorError::State(value.to_string())
     }
 }
