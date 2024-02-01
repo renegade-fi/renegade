4f53c3dbac593324f1ea9ecc0a47729c0ea553c8
Joey Kraut <joey@renegade.fi>
2024-02-01 15:48:15 -0800

    state: replication: Remove raft nodes when their analogous peer expires

--- state/src/replication/network/traits.rs
+++ state/src/replication/network/traits.rs
@@ -40,15 +40,31 @@ pub trait RaftNetwork {
 #[cfg(any(test, feature = "mocks"))]
 pub mod test_helpers {
     //! Test helpers for the network abstraction
+    use common::{new_shared, Shared};
     use crossbeam::channel::{
         unbounded, Receiver as CrossbeamReceiver, Sender as CrossbeamSender, TryRecvError,
     };
     use raft::prelude::Message as RaftMessage;
     use std::io::{Error as IOError, ErrorKind};
 
-    use crate::replication::error::ReplicationError;
+    use crate::replication::{error::ReplicationError, RaftPeerId};
 
     use super::RaftNetwork;
+
+    /// A mock controller for the network, allowing us to simulate network
+    /// interference
+    pub struct MockNetworkController {
+        /// A list of disconnected one way connections
+        disconnects: Shared<Vec<(RaftPeerId, RaftPeerId)>>,
+    }
+
+    impl MockNetworkController {
+        /// Sever the connection between two peers unidirectionally
+        pub fn disconnect(&self, from: RaftPeerId, to: RaftPeerId) {
+            let key = (from, to);
+            self.disconnects.write().unwrap().push(key);
+        }
+    }
 
     /// A mock network that is brokered by `N` channels
     ///
@@ -59,11 +75,17 @@ pub mod test_helpers {
         senders: Vec<CrossbeamSender<RaftMessage>>,
         /// The owner's receiver channel                                    
         receiver: CrossbeamReceiver<RaftMessage>,
+        /// A shared list of severed connections for testing failures
+        ///
+        /// This is a list of tuples, where each tuple represents a
+        /// unidirectionally severed point-to-point connection
+        disconnects: Shared<Vec<(RaftPeerId, RaftPeerId)>>,
     }
 
     impl MockNetwork {
         /// Constructor, returns a handle to the network mesh for each node
-        pub fn new_n_way_mesh(n_nodes: usize) -> Vec<Self> {
+        pub fn new_n_way_mesh(n_nodes: usize) -> (MockNetworkController, Vec<Self>) {
+            let disconnects = new_shared(Vec::new());
             let mut senders = Vec::with_capacity(n_nodes);
             let mut receivers = Vec::with_capacity(n_nodes);
 
@@ -73,14 +95,23 @@ pub mod test_helpers {
                 receivers.push(r);
             }
 
-            receivers.into_iter().map(|r| Self { senders: senders.clone(), receiver: r }).collect()
+            let endpoints = receivers
+                .into_iter()
+                .map(|r| Self {
+                    senders: senders.clone(),
+                    receiver: r,
+                    disconnects: disconnects.clone(),
+                })
+                .collect();
+
+            (MockNetworkController { disconnects }, endpoints)
         }
 
         /// Create a double sided mock connection
         #[allow(dead_code)]
-        pub fn new_duplex_conn() -> (Self, Self) {
-            let mut res = Self::new_n_way_mesh(2 /* n_nodes */);
-            (res.remove(0), res.remove(0))
+        pub fn new_duplex_conn() -> (MockNetworkController, Self, Self) {
+            let (controller, mut res) = Self::new_n_way_mesh(2 /* n_nodes */);
+            (controller, res.remove(0), res.remove(0))
         }
     }
 
@@ -88,6 +119,22 @@ pub mod test_helpers {
         type Error = ReplicationError;
 
         fn send(&mut self, message: RaftMessage) -> Result<(), Self::Error> {
+            // Check for a disconnect
+            let (to, from) = (message.to, message.from);
+            let disconnects = self.disconnects.read().unwrap();
+            if disconnects.contains(&(from, to)) {
+                return Err(ReplicationError::SendMessage(IOError::new(
+                    ErrorKind::ConnectionRefused,
+                    "Connection severed",
+                )));
+            }
+
+            // If the reverse path is disconnected, don't fail just do nothing
+            if disconnects.contains(&(to, from)) {
+                return Ok(());
+            }
+            drop(disconnects); // Unlock
+
             let recipient = (message.to - 1) as usize;
             self.senders[recipient]
                 .send(message)
