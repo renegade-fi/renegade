7fc4957e5cdb6dca71366eb34d92f5e6cd15c45c
Andrew Kirillov <andrew@kirillov.org>
2024-02-05 12:05:43 -0800

    bin: release: accept environment argument

--- bin/release.zsh
+++ bin/release.zsh
@@ -1,9 +1,15 @@
 set -e
 
+# Check if the first argument is either "staging" or "production"
+if [[ $1 != "staging" && $1 != "production" ]]; then
+    echo "Error: First argument must be either 'staging' or 'production'."
+    exit 1
+fi
+
 # Set variables
 aws_account_id="377928551571"
 aws_region="ca-central-1"
-ecr_repository_name="renegade"
+ecr_repository_name="renegade-$1"
 image_name="renegade-relayer"
 image_tag="latest"
 
@@ -27,4 +33,4 @@ docker tag $image_name:$image_tag $aws_account_id.dkr.ecr.$aws_region.amazonaws.
 # Push the Docker image to ECR
 docker push $aws_account_id.dkr.ecr.$aws_region.amazonaws.com/$ecr_repository_name:$image_tag
 
-echo "Successfully released production build"
+echo "Successfully released $1 build"
