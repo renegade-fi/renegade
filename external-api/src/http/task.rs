7f986d7f17ca3063737c952af3285267773f185f
Joey Kraut <joey@renegade.fi>
2024-02-12 10:43:47 -0800

    workers: api-server: Add `/v0/task_queue/:wallet_id` route + handler

--- external-api/src/http/task.rs
+++ external-api/src/http/task.rs
@@ -1,5 +1,6 @@
 //! Defines API types for task status introspection
 
+use common::types::tasks::{QueuedTask, QueuedTaskState, TaskIdentifier};
 use serde::Serialize;
 
 /// The response type for a request to fetch task status
@@ -7,4 +8,33 @@ use serde::Serialize;
 pub struct GetTaskStatusResponse {
     /// The status of the requested task
     pub status: String,
+}
+
+/// A type encapsulating status information for a task
+#[derive(Clone, Debug, Serialize)]
+pub struct TaskStatus {
+    /// The ID of the task
+    pub id: TaskIdentifier,
+    /// The status of the task
+    pub status: String,
+    /// Whether or not the task has already committed
+    pub committed: bool,
+}
+
+impl From<QueuedTask> for TaskStatus {
+    fn from(task: QueuedTask) -> Self {
+        let (status, committed) = match task.state {
+            QueuedTaskState::Queued => ("queued".to_string(), false),
+            QueuedTaskState::Running { state, committed } => (state, committed),
+        };
+
+        TaskStatus { id: task.id, status, committed }
+    }
+}
+
+/// The response type for a request to fetch all tasks on a wallet
+#[derive(Clone, Debug, Serialize)]
+pub struct TaskQueueListResponse {
+    /// The list of tasks on a wallet
+    pub tasks: Vec<TaskStatus>,
 }
