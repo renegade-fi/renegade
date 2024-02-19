23944643316cf20033a43fc5b2905e6d0a4d0cf9
Andrew Kirillov <andrew@kirillov.org>
2024-02-19 12:22:39 -0800

    util, core: vendor in formatter, include source loc

--- util/src/logging/mod.rs
+++ util/src/logging/mod.rs
@@ -15,6 +15,9 @@ use opentelemetry_semantic_conventions::{
 pub use tracing_subscriber::filter::LevelFilter;
 use tracing_subscriber::fmt::format::Format;
 
+mod formatter;
+pub use formatter::DatadogFormatter;
+
 /// Initialize a logger at the given log level
 pub fn setup_system_logger(level: LevelFilter) {
     tracing_subscriber::fmt().event_format(Format::default().pretty()).with_max_level(level).init();
