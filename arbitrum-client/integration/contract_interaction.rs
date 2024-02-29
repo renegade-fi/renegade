4e4284de063ce755e58e28f341e8de680aebfaa3
Andrew Kirillov <andrew@kirillov.org>
2024-02-29 14:03:11 -0800

    arbitrum-client: integration: add integration test for settle_online_relayer_fee

--- arbitrum-client/integration/contract_interaction.rs
+++ arbitrum-client/integration/contract_interaction.rs
@@ -3,8 +3,8 @@
 use circuit_types::transfers::ExternalTransfer;
 use common::types::proof_bundles::{
     mocks::{
-        dummy_link_proof, dummy_valid_match_settle_bundle, dummy_valid_wallet_update_bundle,
-        dummy_validity_proof_bundle,
+        dummy_link_proof, dummy_relayer_fee_settlement_bundle, dummy_valid_match_settle_bundle,
+        dummy_valid_wallet_update_bundle, dummy_validity_proof_bundle,
     },
     MatchBundle,
 };
@@ -104,3 +104,41 @@ async fn test_process_match_settle(test_args: IntegrationTestArgs) -> Result<()>
     assert_eq_result!(party_1_new_shares, recovered_party_1_shares)
 }
 integration_test_async!(test_process_match_settle);
+
+/// Test settling the relayer fee and then recovering the shares of the sender
+/// and recipient wallets
+async fn test_settle_online_relayer_fee(test_args: IntegrationTestArgs) -> Result<()> {
+    let client = &test_args.client;
+
+    // Generate a dummy proof bundle for the relayer fee settlement
+    let mut valid_relayer_fee_settlement_bundle = dummy_relayer_fee_settlement_bundle();
+    let new_sender_shares = random_wallet_shares();
+    let new_recipient_shares = random_wallet_shares();
+    valid_relayer_fee_settlement_bundle.statement.sender_updated_public_shares =
+        new_sender_shares.clone();
+    valid_relayer_fee_settlement_bundle.statement.recipient_updated_public_shares =
+        new_recipient_shares.clone();
+
+    // Settle the relayer fee
+    client
+        .settle_online_relayer_fee(
+            &valid_relayer_fee_settlement_bundle,
+            vec![], // relayer_wallet_commitment_signature
+        )
+        .await?;
+
+    let recovered_sender_shares =
+        client.fetch_public_shares_for_blinder(new_sender_shares.blinder).await?;
+
+    // Check that the recovered sender public shares are the same as the original
+    // ones
+    assert_eq_result!(new_sender_shares, recovered_sender_shares)?;
+
+    let recovered_recipient_shares =
+        client.fetch_public_shares_for_blinder(new_recipient_shares.blinder).await?;
+
+    // Check that the recovered recipient public shares are the same as the original
+    // ones
+    assert_eq_result!(new_recipient_shares, recovered_recipient_shares)
+}
+integration_test_async!(test_settle_online_relayer_fee);
