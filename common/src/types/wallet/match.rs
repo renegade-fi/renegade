8131e0644e40b63b3e9dbb0733510429cb966816
Joey Kraut <joey@renegade.fi>
2024-03-07 15:32:23 -0800

    task-driver: integration: settle-match: Add fees to settlement test

--- common/src/types/wallet/match.rs
+++ common/src/types/wallet/match.rs
@@ -1,6 +1,7 @@
 //! Match settlement helpers
 
 use circuit_types::{native_helpers::create_wallet_shares_from_private, r#match::MatchResult};
+use util::matching_engine::compute_fee_obligation;
 
 use super::{OrderIdentifier, Wallet};
 
@@ -12,6 +13,7 @@ impl Wallet {
         order_id: &OrderIdentifier,
     ) -> Result<(), String> {
         // Subtract the matched volume from the order
+        let match_fee = self.match_fee;
         let order = self.get_order_mut(order_id).unwrap();
         order.amount =
             order.amount.checked_sub(match_res.base_amount).expect("order volume underflow");
@@ -19,13 +21,23 @@ impl Wallet {
         // Select the correct mints and amounts based on the order side
         let (send_mint, send_amt) = match_res.send_mint_amount(order.side);
         let (recv_mint, recv_amt) = match_res.receive_mint_amount(order.side);
+        let fees = compute_fee_obligation(match_fee, order.side, match_res);
+        let net_recv = recv_amt - fees.total();
 
         // Update the balances
         let send_balance = self.get_balance_mut(&send_mint).unwrap();
         send_balance.amount = send_balance.amount.checked_sub(send_amt).expect("balance underflow");
 
         let recv_balance = self.get_balance_mut_or_default(&recv_mint);
-        recv_balance.amount = recv_balance.amount.checked_add(recv_amt).expect("balance overflow");
+        recv_balance.amount = recv_balance.amount.checked_add(net_recv).expect("balance overflow");
+        recv_balance.relayer_fee_balance = recv_balance
+            .relayer_fee_balance
+            .checked_add(fees.relayer_fee)
+            .expect("balance overflow");
+        recv_balance.protocol_fee_balance = recv_balance
+            .protocol_fee_balance
+            .checked_add(fees.protocol_fee)
+            .expect("balance overflow");
 
         // Update the public shares of the wallet, reblinding the wallet should be done
         // separately
