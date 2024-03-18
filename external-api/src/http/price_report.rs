6c19e8ce67a99e25eae19eda7587725f1b3a23c6
Andrew Kirillov <andrew@kirillov.org>
2024-03-18 14:46:06 -0700

    workers: price-reporter: replace median price reports with Binance

--- external-api/src/http/price_report.rs
+++ external-api/src/http/price_report.rs
@@ -1,40 +1,20 @@
 //! Groups price reporting API types
 
-use std::collections::HashMap;
-
-use common::types::{
-    exchange::{Exchange, ExchangeConnectionState, PriceReporterState},
-    token::Token,
-};
+use common::types::{exchange::PriceReporterState, token::Token};
 use serde::{Deserialize, Serialize};
 
-// ---------------
-// | HTTP Routes |
-// ---------------
-
-/// Exchange health check route
-pub const EXCHANGE_HEALTH_ROUTE: &str = "/v0/exchange/health_check";
-
-// -------------
-// | API Types |
-// -------------
-
-/// A request to get the health of each exchange for a given token pair
+/// A request to get the relayer's price report for a pair
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct GetExchangeHealthStatesRequest {
+pub struct GetPriceReportRequest {
     /// The base token
     pub base_token: Token,
     /// The quote token
     pub quote_token: Token,
 }
 
-/// A response containing the health of each exchange
+/// A response containing the relayer's price report for a pair
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct GetExchangeHealthStatesResponse {
-    /// The PriceReporterState corresponding to the instantaneous median
-    /// PriceReport
-    pub median: PriceReporterState,
-    /// The map of all ExchangeConnectionState corresponding to each individual
-    /// exchange
-    pub all_exchanges: HashMap<Exchange, ExchangeConnectionState>,
+pub struct GetPriceReportResponse {
+    /// The PriceReporterState corresponding to the pair
+    pub price_report: PriceReporterState,
 }
