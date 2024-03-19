fe3b4e1e7d63c4a67fc540f6616f74bcd70565cb
Joey Kraut <joey@renegade.fi>
2024-03-19 11:33:08 -0700

    common: wallet: keychain: Use contract-style recoverable signatures

--- common/src/types/wallet/keychain.rs
+++ common/src/types/wallet/keychain.rs
@@ -1,8 +1,12 @@
 //! Keychain helpers for the wallet
 
 use constants::Scalar;
-use ethers::{signers::Wallet as EthersWallet, types::Signature, utils::keccak256};
-use k256::ecdsa::SigningKey;
+use ethers::{
+    core::k256::ecdsa::SigningKey as EthersSigningKey,
+    types::{Signature, U256},
+    utils::keccak256,
+};
+use util::raw_err_str;
 
 use super::Wallet;
 
@@ -11,17 +15,26 @@ const ERR_NO_SK_ROOT: &str = "wallet does not have an `sk_root` value";
 
 impl Wallet {
     /// Sign a wallet transition commitment with the wallet's keychain
+    ///
+    /// The contracts expect a recoverable signature with the recover ID set to
+    /// 0/1; we use ethers `sign_prehash_recoverable` to generate this signature
     pub fn sign_commitment(&self, commitment: Scalar) -> Result<Signature, String> {
         // Fetch the `sk_root` key
         let root_key = self.key_chain.secret_keys.sk_root.as_ref().ok_or(ERR_NO_SK_ROOT)?;
-        let key = SigningKey::try_from(root_key)?;
-        let wallet = EthersWallet::from(key);
+        let key = EthersSigningKey::try_from(root_key)?;
 
+        // Hash the message and sign it
         let comm_bytes = commitment.to_biguint().to_bytes_be();
         let digest = keccak256(comm_bytes);
+        let (sig, recovery_id) = key
+            .sign_prehash_recoverable(&digest)
+            .map_err(raw_err_str!("failed to sign commitment: {}"))?;
 
-        // Sign the commitment
-        wallet.sign_hash(digest.into()).map_err(|e| e.to_string())
+        Ok(Signature {
+            r: U256::from_big_endian(&sig.r().to_bytes()),
+            s: U256::from_big_endian(&sig.s().to_bytes()),
+            v: recovery_id.to_byte() as u64,
+        })
     }
 }
 
