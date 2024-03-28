2f9ef03ec68b60206f2631664018e8eeffcf1724
Andrew Kirillov <andrew@kirillov.org>
2024-03-28 14:35:28 -0700

    common: types: token: set up stablecoin set & exchange mapping

--- common/src/types/token.rs
+++ common/src/types/token.rs
@@ -28,6 +28,64 @@ use crate::biguint_to_str_addr;
 
 use super::exchange::{Exchange, ALL_EXCHANGES};
 
+// ----------------
+// | Quote Tokens |
+// ----------------
+
+/// USDC ticker
+pub const USDC_TICKER: &str = "USDC";
+/// USDT ticker
+pub const USDT_TICKER: &str = "USDT";
+/// USD ticker
+///
+/// We don't actually allow USD as a quote asset since it's not an ERC20,
+/// but it is used as a quote in some exchanges, so we must be able to
+/// stream prices for it.
+pub const USD_TICKER: &str = "USD";
+
+// ---------------
+// | Base Tokens |
+// ---------------
+
+/// WBTC ticker
+pub const WBTC_TICKER: &str = "WBTC";
+/// WETH ticker
+pub const WETH_TICKER: &str = "WETH";
+/// BNB ticker
+pub const BNB_TICKER: &str = "BNB";
+/// MATIC ticker
+pub const MATIC_TICKER: &str = "MATIC";
+/// LDO ticker
+pub const LDO_TICKER: &str = "LDO";
+/// LINK ticker
+pub const LINK_TICKER: &str = "LINK";
+/// UNI ticker
+pub const UNI_TICKER: &str = "UNI";
+/// CRV ticker
+pub const CRV_TICKER: &str = "CRV";
+/// DYDX ticker
+pub const DYDX_TICKER: &str = "DYDX";
+/// AAVE ticker
+pub const AAVE_TICKER: &str = "AAVE";
+/// SUSHI ticker
+pub const SUSHI_TICKER: &str = "SUSHI";
+/// 1INCH ticker
+pub const _1INCH_TICKER: &str = "1INCH";
+/// COMP ticker
+pub const COMP_TICKER: &str = "COMP";
+/// MKR ticker
+pub const MKR_TICKER: &str = "MKR";
+/// TORN ticker
+pub const TORN_TICKER: &str = "TORN";
+/// REN ticker
+pub const REN_TICKER: &str = "REN";
+/// SHIB ticker
+pub const SHIB_TICKER: &str = "SHIB";
+/// ENS ticker
+pub const ENS_TICKER: &str = "ENS";
+/// MANA ticker
+pub const MANA_TICKER: &str = "MANA";
+
 /// A helper enum to describe the state of each ticker on each Exchange. Same
 /// means that the ERC-20 and Exchange tickers are the same, Renamed means that
 /// the Exchange ticker is different from the underlying ERC-20, and Unsupported
@@ -42,13 +100,6 @@ pub enum ExchangeTicker {
     Unsupported,
 }
 
-// We populate three global heap-allocated structs for convenience and metadata
-// lookup. The first is a bidirectional map between the ERC-20 contract address
-// and the ERC-20 ticker. The second is a HashMap between the ERC-20 contract
-// address and the number of decimals (fixed-point offset). The third is a
-// HashMap between the ERC-20 ticker and each Exchange's expected name for each
-// ticker.
-
 /// The remapping of tickers between exchanges. The layout of `TICKER_NAMES` is
 /// (Renegade Ticker, Binance Ticker, Coinbase Ticker, Kraken Ticker, Okx
 /// Ticker), where "Renegade Ticker" denotes the ticker expected in the Renegade
@@ -62,84 +113,95 @@ pub static TICKER_NAMES: &[(
 )] = &[
     // L1
     (
-        "WBTC",
+        WBTC_TICKER,
         ExchangeTicker::Renamed("BTC"),
         ExchangeTicker::Renamed("BTC"),
         ExchangeTicker::Renamed("BTC"),
         ExchangeTicker::Renamed("BTC"),
     ),
     (
-        "WETH",
+        WETH_TICKER,
         ExchangeTicker::Renamed("ETH"),
         ExchangeTicker::Renamed("ETH"),
         ExchangeTicker::Renamed("ETH"),
         ExchangeTicker::Renamed("ETH"),
     ),
     (
-        "BNB",
+        BNB_TICKER,
         ExchangeTicker::Same,
         ExchangeTicker::Unsupported,
         ExchangeTicker::Unsupported,
         ExchangeTicker::Same,
     ),
     (
-        "MATIC",
+        MATIC_TICKER,
         ExchangeTicker::Same,
         ExchangeTicker::Same,
         ExchangeTicker::Same,
         ExchangeTicker::Same,
     ),
     // LSDs
-    ("LDO", ExchangeTicker::Same, ExchangeTicker::Same, ExchangeTicker::Same, ExchangeTicker::Same),
+    (
+        LDO_TICKER,
+        ExchangeTicker::Same,
+        ExchangeTicker::Same,
+        ExchangeTicker::Same,
+        ExchangeTicker::Same,
+    ),
     // Stables
     (
-        "USDC",
-        ExchangeTicker::Same,
-        ExchangeTicker::Renamed("USD"), // USDC has 1:1 peg with USD on Coinbase
-        ExchangeTicker::Same,
-        ExchangeTicker::Same,
-    ),
-    (
-        "USDT",
+        USDC_TICKER,
         ExchangeTicker::Same,
         ExchangeTicker::Same,
         ExchangeTicker::Same,
         ExchangeTicker::Same,
     ),
     (
-        "USD",
-        ExchangeTicker::Unsupported,
+        USDT_TICKER,
         ExchangeTicker::Same,
         ExchangeTicker::Same,
-        ExchangeTicker::Unsupported,
+        ExchangeTicker::Same,
+        ExchangeTicker::Same,
     ),
     // Oracles
     (
-        "LINK",
+        LINK_TICKER,
         ExchangeTicker::Same,
         ExchangeTicker::Same,
         ExchangeTicker::Same,
         ExchangeTicker::Same,
     ),
     // DeFi Trading
-    ("UNI", ExchangeTicker::Same, ExchangeTicker::Same, ExchangeTicker::Same, ExchangeTicker::Same),
-    ("CRV", ExchangeTicker::Same, ExchangeTicker::Same, ExchangeTicker::Same, ExchangeTicker::Same),
     (
-        "DYDX",
+        UNI_TICKER,
+        ExchangeTicker::Same,
+        ExchangeTicker::Same,
+        ExchangeTicker::Same,
+        ExchangeTicker::Same,
+    ),
+    (
+        CRV_TICKER,
+        ExchangeTicker::Same,
+        ExchangeTicker::Same,
+        ExchangeTicker::Same,
+        ExchangeTicker::Same,
+    ),
+    (
+        DYDX_TICKER,
         ExchangeTicker::Same,
         ExchangeTicker::Unsupported,
         ExchangeTicker::Same,
         ExchangeTicker::Same,
     ),
     (
-        "SUSHI",
+        SUSHI_TICKER,
         ExchangeTicker::Same,
         ExchangeTicker::Same,
         ExchangeTicker::Same,
         ExchangeTicker::Same,
     ),
     (
-        "1INCH",
+        _1INCH_TICKER,
         ExchangeTicker::Same,
         ExchangeTicker::Same,
         ExchangeTicker::Same,
@@ -147,41 +209,59 @@ pub static TICKER_NAMES: &[(
     ),
     // DeFi Lending
     (
-        "AAVE",
+        AAVE_TICKER,
         ExchangeTicker::Same,
         ExchangeTicker::Same,
         ExchangeTicker::Same,
         ExchangeTicker::Same,
     ),
     (
-        "COMP",
+        COMP_TICKER,
         ExchangeTicker::Same,
         ExchangeTicker::Same,
         ExchangeTicker::Same,
         ExchangeTicker::Same,
     ),
-    ("MKR", ExchangeTicker::Same, ExchangeTicker::Same, ExchangeTicker::Same, ExchangeTicker::Same),
+    (
+        MKR_TICKER,
+        ExchangeTicker::Same,
+        ExchangeTicker::Same,
+        ExchangeTicker::Same,
+        ExchangeTicker::Same,
+    ),
     // DeFi Other
     (
-        "TORN",
+        TORN_TICKER,
         ExchangeTicker::Same,
         ExchangeTicker::Unsupported,
         ExchangeTicker::Unsupported,
         ExchangeTicker::Unsupported,
     ),
     // Bridges
-    ("REN", ExchangeTicker::Same, ExchangeTicker::Same, ExchangeTicker::Same, ExchangeTicker::Same),
-    // Misc
     (
-        "SHIB",
+        REN_TICKER,
         ExchangeTicker::Same,
         ExchangeTicker::Same,
         ExchangeTicker::Same,
         ExchangeTicker::Same,
     ),
-    ("ENS", ExchangeTicker::Same, ExchangeTicker::Same, ExchangeTicker::Same, ExchangeTicker::Same),
+    // Misc
     (
-        "MANA",
+        SHIB_TICKER,
+        ExchangeTicker::Same,
+        ExchangeTicker::Same,
+        ExchangeTicker::Same,
+        ExchangeTicker::Same,
+    ),
+    (
+        ENS_TICKER,
+        ExchangeTicker::Same,
+        ExchangeTicker::Same,
+        ExchangeTicker::Same,
+        ExchangeTicker::Same,
+    ),
+    (
+        MANA_TICKER,
         ExchangeTicker::Same,
         ExchangeTicker::Same,
         ExchangeTicker::Same,
@@ -194,10 +274,11 @@ pub static TICKER_NAMES: &[(
 pub static TOKEN_REMAPS: OnceLock<BiMap<String, String>> = OnceLock::new();
 
 /// The decimal mapping for the given environment, maps from the token address
-/// to the number of decimals the token uses
+/// to the number of decimals the token uses (fixed-point offset)
 pub static ADDR_DECIMALS_MAP: OnceLock<HashMap<String, u8>> = OnceLock::new();
 
 lazy_static! {
+    /// The mapping of ERC-20 ticker to the expected ticker names on each Exchange.
     static ref EXCHANGE_TICKERS: HashMap<Exchange, HashMap<String, String>> = {
         let mut exchange_tickers = HashMap::<Exchange, HashMap<String, String>>::new();
         for exchange in [Exchange::Binance, Exchange::Coinbase, Exchange::Kraken, Exchange::Okx] {
@@ -239,6 +320,29 @@ lazy_static! {
             }
         }
         exchange_tickers
+    };
+    /// The set of stablecoin quote assets
+    static ref STABLECOIN_QUOTES: HashSet<Token> = {
+        let stablecoin_tickers = [USDC_TICKER, USDT_TICKER];
+        stablecoin_tickers.iter().map(|ticker| Token::from_ticker(ticker)).collect()
+    };
+    /// The mapping between exchanges and the stablecoin quote asset on which they are generally
+    /// the most liquid
+    static ref MOST_LIQ_STABLE: HashMap<Exchange, Token> = {
+        let mut most_liquid_stable = HashMap::<Exchange, Token>::new();
+
+        // Most liquid stablecoin quote on Binance is generally USDT
+        most_liquid_stable.insert(Exchange::Binance, Token::from_ticker(USDT_TICKER));
+        // Most liquid stablecoin quote on Coinbase is generally USDC
+        most_liquid_stable.insert(Exchange::Coinbase, Token::from_ticker(USDC_TICKER));
+        // Most liquid "stablecoin" quote on Kraken is generally USD
+        most_liquid_stable.insert(Exchange::Kraken, Token::from_ticker(USD_TICKER));
+        // Most liquid stablecoin quote on Okx is generally USDT
+        most_liquid_stable.insert(Exchange::Okx, Token::from_ticker(USDT_TICKER));
+
+        // We currently omit UniswapV3 here, as it requires more involved routing logic
+
+        most_liquid_stable
     };
 }
 
