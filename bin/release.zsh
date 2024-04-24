e121269f80c44bf6d4f186f41a95164fbef78349
Andrew Kirillov <andrew@kirillov.org>
2024-04-23 22:34:32 -0700

    github, docker: sequencer: support dev env & deploy erc20s first

--- bin/release.zsh
+++ bin/release.zsh
@@ -1,16 +1,16 @@
 set -e
 
-# Check if the first argument is either "staging", "testnet", or "mainnet"
-if [[ $1 != "staging" && $1 != "testnet" && $1 != "mainnet" ]]; then
-    echo "Error: First argument must be either 'staging', 'testnet', or 'mainnet'."
+# Check if the first argument is either "staging", "dev", "testnet", or "mainnet"
+if [[ $1 != "staging" && $1 != "testnet" && $1 != "mainnet" && $1 != "dev" ]]; then
+    echo "Error: First argument must be either 'staging', 'testnet', 'mainnet', or 'dev'."
     exit 1
 fi
 
 # Set variables
 aws_account_id="377928551571"
-aws_region="ca-central-1"
-ecr_repository_name="renegade-$1"
-image_name="renegade-relayer"
+aws_region="us-east-2"
+ecr_repository_name="relayer-$1"
+image_name="relayer"
 image_tag="latest"
 
 # Log in to Amazon ECR
