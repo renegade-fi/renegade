1c4d323df650e7aaaa7e31370bb1d7e8c0615bf9
Joey Kraut <joey@renegade.fi>
2024-05-11 18:49:35 -0700

    state: interface: Connect state interface to raft client

--- state/src/notifications.rs
+++ state/src/notifications.rs
@@ -4,10 +4,18 @@
 //! The underlying raft node will dequeue a proposal and apply it to the state
 //! machine. Only then will the notification be sent to the worker.
 use common::{new_async_shared, AsyncShared};
-use std::collections::HashMap;
+use futures::{ready, Future};
+use std::{
+    collections::HashMap,
+    pin::Pin,
+    task::{Context, Poll},
+};
 use uuid::Uuid;
 
 use crate::{applicator::return_type::ApplicatorReturnType, error::StateError};
+
+/// Error message emitted when a proposal channel closes unexpectedly
+const ERR_PROPOSAL_CLOSED: &str = "Proposal channel closed unexpectedly";
 
 /// The id of a proposal
 pub type ProposalId = Uuid;
@@ -53,5 +61,29 @@ impl OpenNotifications {
         let (sender, receiver) = new_proposal_result_channel();
         self.map.write().await.insert(id, sender);
         receiver
+    }
+}
+
+/// A wrapper around a proposal receiver that gives a more convenient async
+/// interface without channel like semantics
+pub struct ProposalWaiter {
+    /// The inner channel
+    inner: ProposalResultReceiver,
+}
+
+impl ProposalWaiter {
+    /// Create a new waiter from the given channel
+    pub fn new(inner: ProposalResultReceiver) -> Self {
+        Self { inner }
+    }
+}
+
+impl Future for ProposalWaiter {
+    type Output = ProposalReturnType;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let res = ready!(Pin::new(&mut self.get_mut().inner).poll(cx))
+            .map_err(|_| StateError::Proposal(ERR_PROPOSAL_CLOSED.to_string()))?;
+        Poll::Ready(res)
     }
 }
