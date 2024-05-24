44e6ae5b298a1e18710a863d13494dada4f567ae
Andrew Kirillov <andrew@kirillov.org>
2024-05-24 13:27:39 -0700

    arbitrum-client: track txs sent/submitted

--- arbitrum-client/src/helpers.rs
+++ arbitrum-client/src/helpers.rs
@@ -24,6 +24,7 @@ use crate::{
         settleOnlineRelayerFeeCall, updateWalletCall,
     },
     client::MiddlewareStack,
+    constants::{NUM_TXS_SENT_METRIC, NUM_TXS_SUBMITTED_METRIC},
     errors::ArbitrumClientError,
 };
 
@@ -45,12 +46,17 @@ pub fn deserialize_calldata<'de, T: Deserialize<'de>>(
 pub async fn send_tx(
     tx: ContractCall<MiddlewareStack, impl Detokenize>,
 ) -> Result<TransactionReceipt, ArbitrumClientError> {
-    tx.send()
+    incr_txs_sent();
+    let res = tx
+        .send()
         .await
         .map_err(|e| ArbitrumClientError::ContractInteraction(e.to_string()))?
         .await
         .map_err(|e| ArbitrumClientError::ContractInteraction(e.to_string()))?
-        .ok_or(ArbitrumClientError::TxDropped)
+        .ok_or(ArbitrumClientError::TxDropped);
+
+    incr_txs_submitted();
+    res
 }
 
 /// Parses wallet shares from the calldata of a `newWallet` call
@@ -182,4 +188,18 @@ pub fn parse_shares_from_redeem_fee(
     let mut shares = statement.new_wallet_public_shares.into_iter().map(Scalar::new);
 
     Ok(SizedWalletShare::from_scalars(&mut shares))
+}
+
+/// Increment the number of transactions sent
+#[inline]
+pub fn incr_txs_sent() {
+    #[cfg(feature = "tx-metrics")]
+    metrics::counter!(NUM_TXS_SENT_METRIC).increment(1);
+}
+
+/// Increment the number of transactions submitted
+#[inline]
+pub fn incr_txs_submitted() {
+    #[cfg(feature = "tx-metrics")]
+    metrics::counter!(NUM_TXS_SUBMITTED_METRIC).increment(1);
 }
