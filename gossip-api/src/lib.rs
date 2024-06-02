521a4b7ac4b356ec56163835d416fa5a399da8f0
Joey Kraut <joey@renegade.fi>
2024-06-02 14:56:11 -0700

    network-manager: Spawn request authentication on blocking thread

--- gossip-api/src/lib.rs
+++ gossip-api/src/lib.rs
@@ -8,6 +8,8 @@
 
 use ed25519_dalek::{Digest, Keypair, PublicKey, Sha512, Signature, SignatureError};
 use serde::Serialize;
+use tracing::instrument;
+use util::telemetry::helpers::backfill_trace_field;
 
 pub mod pubsub;
 pub mod request_response;
@@ -17,22 +19,30 @@ pub mod request_response;
 // -----------
 
 /// Sign a request body with the given key
+#[instrument(name = "sign_message", skip_all, fields(req_size))]
 pub fn sign_message<M: Serialize>(req: &M, key: &Keypair) -> Result<Vec<u8>, SignatureError> {
+    let buf = serde_json::to_vec(req).unwrap();
+    backfill_trace_field("req_size", buf.len());
+
     let mut hash_digest = Sha512::new();
-    hash_digest.update(&serde_json::to_vec(req).unwrap());
+    hash_digest.update(&buf);
     let sig_bytes = key.sign_prehashed(hash_digest, None /* context */)?.to_bytes();
 
     Ok(sig_bytes.to_vec())
 }
 
 /// Check a signature on a request body with the given key
+#[instrument(name = "check_signature", skip_all)]
 pub fn check_signature<M: Serialize>(
     req: &M,
     sig: &[u8],
     key: &PublicKey,
 ) -> Result<(), SignatureError> {
+    let buf = serde_json::to_vec(req).unwrap();
+    backfill_trace_field("req_size", buf.len());
+
     let mut hash_digest = Sha512::new();
-    hash_digest.update(&serde_json::to_vec(req).unwrap());
+    hash_digest.update(&buf);
     let sig = Signature::from_bytes(sig)?;
 
     key.verify_prehashed(hash_digest, None /* context */, &sig)
