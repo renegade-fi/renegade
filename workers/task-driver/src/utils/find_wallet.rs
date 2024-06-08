1bd121bc2cb6d2bb61fe96feda461c7953631ef6
Joey Kraut <joey@renegade.fi>
2024-06-08 13:34:14 -0700

    task-driver: utils: find-wallet: Optimize wallet lookup implementation

--- workers/task-driver/src/utils/find_wallet.rs
+++ workers/task-driver/src/utils/find_wallet.rs
@@ -10,6 +10,12 @@ use renegade_crypto::hash::PoseidonCSPRNG;
 
 use super::ERR_WALLET_NOT_FOUND;
 
+/// The wallet index at which to being the exponential phase of the search
+const EXPONENTIAL_SEARCH_START_IDX: usize = 100;
+/// The amount to exponentially increase the share index by in the first phase
+/// of wallet search
+const EXPONENTIAL_SEARCH_MULTIPLIER: usize = 2;
+
 /// Get the i'th set of private shares for a wallet from the given share seed
 ///
 /// The `idx` here is an index (in number of wallets) into the share CSPRNG
@@ -24,7 +30,7 @@ pub(crate) fn gen_private_shares(
     // share stream
     let shares_per_wallet = SizedWallet::NUM_SCALARS - 1;
     let mut share_csprng = PoseidonCSPRNG::new(share_seed);
-    share_csprng.advance_by((idx - 1) * shares_per_wallet).unwrap();
+    share_csprng.advance_by(idx * shares_per_wallet).unwrap();
 
     // Sample private secret shares for the wallet
     let mut new_private_shares =
@@ -40,33 +46,113 @@ pub(crate) async fn find_latest_wallet_tx(
     blinder_seed: Scalar,
     arbitrum_client: &ArbitrumClient,
 ) -> Result<(usize, Scalar, Scalar), String> {
-    // Find the latest transaction updating the wallet, as indexed by the public
-    // share of the blinders
-    let mut blinder_csprng = PoseidonCSPRNG::new(blinder_seed);
+    let (min_idx, max_idx) =
+        find_wallet_share_index_phase_one(blinder_seed, arbitrum_client).await?;
+    let (idx, blinder, blinder_private) =
+        find_wallet_share_index_phase_two(min_idx, max_idx, blinder_seed, arbitrum_client).await?;
 
-    let mut blinder_index = 0;
-    let mut curr_blinder = Scalar::zero();
-    let mut curr_blinder_private_share = Scalar::zero();
+    // Check that the implied public blinder exists
+    let public_blinder = blinder - blinder_private;
+    match arbitrum_client.get_public_blinder_tx(public_blinder).await.map_err(|e| e.to_string())? {
+        None => Err(ERR_WALLET_NOT_FOUND.to_string()),
+        Some(_) => Ok((idx, blinder, blinder_private)),
+    }
+}
 
-    let mut updating_tx = None;
+/// The first phase of the find wallet process, exponentially increase the share
+/// index until one is not seen on-chain
+///
+/// The first value not seen on-chain becomes the initial `max` for the binary
+/// search phase
+///
+/// Returns the bounds for the binary search phase
+async fn find_wallet_share_index_phase_one(
+    blinder_seed: Scalar,
+    arbitrum_client: &ArbitrumClient,
+) -> Result<(usize, usize), String> {
+    let mut curr_min = 0;
+    let mut curr_max = EXPONENTIAL_SEARCH_START_IDX;
 
-    while let (blinder, private_share) = blinder_csprng.next_tuple().unwrap()
-        && let Some(tx) = arbitrum_client
-            .get_public_blinder_tx(blinder - private_share)
+    loop {
+        let public_blinder = get_public_blinder_at_idx(curr_max, blinder_seed);
+
+        // Check if the blinder has been seen on-chain
+        match arbitrum_client
+            .get_public_blinder_tx(public_blinder)
             .await
             .map_err(|e| e.to_string())?
-    {
-        updating_tx = Some(tx);
-
-        curr_blinder = blinder;
-        curr_blinder_private_share = private_share;
-        blinder_index += 1;
+        {
+            None => break,
+            Some(_) => {
+                curr_min = curr_max;
+                curr_max *= EXPONENTIAL_SEARCH_MULTIPLIER;
+            },
+        }
     }
 
-    // Error if not found
-    if updating_tx.is_none() {
-        return Err(ERR_WALLET_NOT_FOUND.to_string());
-    }
+    Ok((curr_min, curr_max))
+}
 
-    Ok((blinder_index, curr_blinder, curr_blinder_private_share))
+/// The second phase of the find wallet process, binary search the range of
+/// indices [min_idx, max_idx) to find the latest update of the wallet
+async fn find_wallet_share_index_phase_two(
+    min_idx: usize,
+    max_idx: usize,
+    blinder_seed: Scalar,
+    arbitrum_client: &ArbitrumClient,
+) -> Result<(usize, Scalar, Scalar), String> {
+    let mut curr_min = min_idx;
+    let mut curr_max = max_idx;
+    let (mut curr_min_blinder, mut curr_min_blinder_private) =
+        get_blinder_and_private_share_at_idx(curr_min, blinder_seed);
+
+    loop {
+        // Return condition
+        if curr_max - curr_min <= 1 {
+            return Ok((curr_min, curr_min_blinder, curr_min_blinder_private));
+        }
+
+        let curr_idx = (curr_min + curr_max) / 2;
+        let idx_from_min = curr_idx - curr_min - 1;
+
+        // Build the blinder relative to the min idx to avoid recomputing the stream up
+        // to the min
+        let (curr_blinder, curr_blinder_private) =
+            get_blinder_and_private_share_at_idx(idx_from_min, curr_min_blinder_private);
+        let curr_blinder_public = curr_blinder - curr_blinder_private;
+
+        // Check if the blinder has been seen on-chain
+        match arbitrum_client
+            .get_public_blinder_tx(curr_blinder_public)
+            .await
+            .map_err(|e| e.to_string())?
+        {
+            None => {
+                curr_max = curr_idx;
+            },
+            Some(_) => {
+                curr_min = curr_idx;
+                curr_min_blinder = curr_blinder;
+                curr_min_blinder_private = curr_blinder_private;
+            },
+        }
+    }
+}
+
+/// Get the public blinder at index `idx` from the blinder stream at the given
+/// seed
+fn get_public_blinder_at_idx(idx: usize, blinder_seed: Scalar) -> Scalar {
+    let (blinder, private_share) = get_blinder_and_private_share_at_idx(idx, blinder_seed);
+    blinder - private_share
+}
+
+/// Get the blinder and private blinder share at index `idx` from the blinder
+/// stream at the given seed
+fn get_blinder_and_private_share_at_idx(idx: usize, blinder_seed: Scalar) -> (Scalar, Scalar) {
+    let mut blinder_csprng = PoseidonCSPRNG::new(blinder_seed);
+
+    // Advance the stream by 2 * idx; we draw two samples per blinder, the blinder
+    // and the private share
+    blinder_csprng.advance_by(idx * 2).unwrap();
+    blinder_csprng.next_tuple().unwrap()
 }
