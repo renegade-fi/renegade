aae2cb0ec5cf3f6f3f266ad6a6aea28fd5d60ad9
Andrew Kirillov <andrew@kirillov.org>
2024-06-08 18:44:03 -0700

    state: replication: deduplicate membership changes

--- bin/build_and_push.sh
+++ bin/build_and_push.sh
@@ -4,10 +4,14 @@ ENVIRONMENT=${1:-dev}
 ECR_URL=377928551571.dkr.ecr.us-east-2.amazonaws.com/relayer-$ENVIRONMENT
 
 GIT_HASH=$(git rev-parse HEAD)
+
+TAG_1=$ECR_URL\:$GIT_HASH
+TAG_2=$ECR_URL\:latest
+
 docker build -t relayer:latest  -f ./docker/release/Dockerfile .
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ECR_URL
 
-docker tag relayer:latest $ECR_URL:$GIT_HASH
-docker tag relayer:latest $ECR_URL:latest
-docker push $ECR_URL:$GIT_HASH
-docker push $ECR_URL:latest
+docker tag relayer:latest $TAG_1
+docker tag relayer:latest $TAG_2
+docker push $TAG_1
+docker push $TAG_2
