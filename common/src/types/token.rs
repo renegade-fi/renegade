e39351a12774947a35d9c316785d6092f2adccbc
Andrew Kirillov <andrew@kirillov.org>
2024-06-17 14:53:10 -0700

    common: token: s/ren/arb

--- common/src/types/token.rs
+++ common/src/types/token.rs
@@ -81,8 +81,8 @@ pub const COMP_TICKER: &str = "COMP";
 pub const MKR_TICKER: &str = "MKR";
 /// TORN ticker
 pub const TORN_TICKER: &str = "TORN";
-/// REN ticker
-pub const REN_TICKER: &str = "REN";
+/// ARB ticker
+pub const ARB_TICKER: &str = "ARB";
 /// SHIB ticker
 pub const SHIB_TICKER: &str = "SHIB";
 /// ENS ticker
@@ -250,7 +250,7 @@ pub static TICKER_NAMES: &[(
     ),
     // Bridges
     (
-        REN_TICKER,
+        ARB_TICKER,
         ExchangeTicker::Same,
         ExchangeTicker::Same,
         ExchangeTicker::Same,
