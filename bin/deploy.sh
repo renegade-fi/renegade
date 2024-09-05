8707d3b7eb07d9aea8b57d5a5866d6a4066a4fb5
Joey Kraut <joey@renegade.fi>
2024-09-05 14:59:23 -0700

    bin: Update `build_and_push` and `deploy` to use cli args

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,16 +1,32 @@
 #!/bin/sh
 REGION=us-east-2
-ENVIRONMENT=${1:-dev}
-CLUSTER_IDX=${2:-0}
-CLUSTER_NAME=$ENVIRONMENT-cluster$CLUSTER_IDX
-SERVICE_NAME=$ENVIRONMENT-cluster$CLUSTER_IDX-service
+DEFAULT_ENVIRONMENT=dev
+
+# Get the current git commit hash (long form)
+DEFAULT_IMAGE_TAG=$(git rev-parse HEAD)
+
+# Parse command line arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --image-tag) IMAGE_TAG="$2"; shift ;;
+        --env) ENVIRONMENT="$2"; shift ;;
+        *) echo "Unknown parameter: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+# Use defaults if not provided
+ENVIRONMENT=${ENVIRONMENT:-$DEFAULT_ENVIRONMENT}
+IMAGE_TAG=${IMAGE_TAG:-$DEFAULT_IMAGE_TAG}
+
+CLUSTER_NAME=$ENVIRONMENT-cluster0
+SERVICE_NAME=$ENVIRONMENT-cluster0-service
 TASK_FAMILY=$ENVIRONMENT-cluster0-task-def
 ECR_URL=377928551571.dkr.ecr.us-east-2.amazonaws.com/relayer-$ENVIRONMENT
 
-# Fetch the latest image URI from ECR
-IMAGE_URI=$(aws ecr describe-images --repository-name relayer-$ENVIRONMENT --region us-east-2 --image-ids imageTag=latest | jq -r '.imageDetails[0].imageTags[0]')
-FULL_IMAGE_URI="$ECR_URL:$IMAGE_URI"
+FULL_IMAGE_URI="$ECR_URL:$IMAGE_TAG"
 echo "Using image URI: $FULL_IMAGE_URI"
+echo "Deploying relayer to: $ENVIRONMENT"
 
 # Fetch the existing definition of the task and create a new revision with the updated URI
 TASK_DEFINITION=$(aws ecs describe-task-definition --task-definition $TASK_FAMILY --region $REGION --query 'taskDefinition')
@@ -31,5 +47,5 @@ NEW_REVISION=$(echo $NEW_TASK_INFO | jq -r '.taskDefinition.revision')
 echo "Created new task revision: $NEW_REVISION"
 
 # Update the ECS cluster to the new revision
-aws ecs update-service --cluster $CLUSTER_NAME --service $SERVICE_NAME --task-definition $TASK_FAMILY:$NEW_REVISION --region $REGION >/dev/null 2>&1
+aws ecs update-service --cluster $CLUSTER_NAME --service $SERVICE_NAME --task-definition $TASK_FAMILY:$NEW_REVISION --region $REGION --force-new-deployment >/dev/null 2>&1
 echo "ECS cluster updated to new revision"
