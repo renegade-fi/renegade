efbd69e8ede612e51a8a10f106612b9907cc458b
Andrew Kirillov <andrew@kirillov.org>
2024-10-16 11:06:09 -0700

    external-api: http: admin, workers: api-server: http: admin: add augmented order endpoint

--- external-api/src/http/admin.rs
+++ external-api/src/http/admin.rs
@@ -6,7 +6,7 @@
 
 use circuit_types::Amount;
 use common::types::{
-    wallet::{order_metadata::OrderMetadata, WalletIdentifier},
+    wallet::{order_metadata::OrderMetadata, OrderIdentifier, WalletIdentifier},
     MatchingPoolName, Price,
 };
 use serde::{Deserialize, Serialize};
@@ -46,23 +46,22 @@ pub struct IsLeaderResponse {
 /// The response to an "open orders" request
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct OpenOrdersResponse {
-    /// The open orders
-    pub orders: Vec<OpenOrder>,
+    /// The open order IDs
+    pub orders: Vec<OrderIdentifier>,
 }
 
-/// An open order, containing the order's metadata
-/// and potentially the fillable amount of the order
-/// given the underlying wallet's balances and
-/// the current price of the base asset
+/// An order's metadata, augmented with the containing
+/// wallet's ID, and optionally the fillable amount
+/// of the order and the price used to calculate it
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct OpenOrder {
+pub struct AdminOrderMetadata {
     /// The order metadata
     pub order: OrderMetadata,
     /// The ID of the wallet containing the order
     pub wallet_id: WalletIdentifier,
     /// The fillable amount of the order, if calculated
     pub fillable: Option<Amount>,
-    /// The price used to calculate the fillable amount, if calculated
+    /// The price used to calculate the fillable amount
     pub price: Option<Price>,
 }
 
@@ -79,11 +78,11 @@ pub struct CreateOrderInMatchingPoolRequest {
     pub matching_pool: MatchingPoolName,
 }
 
-/// The response to an "order metadata" request
+/// The response to an admin "order metadata" request
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct AdminOrderMetadataResponse {
     /// The order metadata
-    pub order: OrderMetadata,
+    pub order: AdminOrderMetadata,
 }
 
 /// The response to a "get order matching pool" request
