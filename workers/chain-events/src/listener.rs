51fcfe39922738679db455fc8993b84a94b8c4ec
Joey Kraut <joey@renegade.fi>
2024-10-17 13:53:55 -0700

    chain-events: listener: Refresh wallet if nullifier spent

--- workers/chain-events/src/listener.rs
+++ workers/chain-events/src/listener.rs
@@ -1,8 +1,10 @@
 //! Defines the core implementation of the on-chain event listener
 
-use std::thread::JoinHandle;
+use rand::{rngs::OsRng, Rng};
+use std::{thread::JoinHandle, time::Duration};
 
 use arbitrum_client::{abi::NullifierSpentFilter, client::ArbitrumClient};
+use circuit_types::wallet::Nullifier;
 use common::types::CancelChannel;
 use constants::in_bootstrap_mode;
 use ethers::prelude::StreamExt;
@@ -17,6 +19,11 @@ use tracing::{error, info};
 use util::runtime::sleep_forever_async;
 
 use super::error::OnChainEventListenerError;
+
+/// The minimum delay in seconds for wallet refresh
+const MIN_NULLIFIER_REFRESH_DELAY_S: u64 = 20; // 20 seconds
+/// The maximum delay in seconds for wallet refresh
+const MAX_NULLIFIER_REFRESH_DELAY_S: u64 = 40; // 40 seconds
 
 // ----------
 // | Worker |
@@ -74,6 +81,11 @@ impl OnChainEventListenerExecutor {
         &self.config.arbitrum_client
     }
 
+    /// Shorthand for fetching a reference to the global state
+    fn state(&self) -> &State {
+        &self.config.global_state
+    }
+
     /// The main execution loop for the executor
     pub async fn execute(self) -> Result<(), OnChainEventListenerError> {
         // If the node is running in bootstrap mode, sleep forever
@@ -119,7 +131,62 @@ impl OnChainEventListenerExecutor {
             .map_err(|err| OnChainEventListenerError::SendMessage(err.to_string()))?;
 
         // Nullify any orders that used this nullifier in their validity proof
-        self.config.global_state.nullify_orders(nullifier).await?;
+        self.state().nullify_orders(nullifier).await?;
+
+        // Check if the wallet that this nullifier belongs to needs a refresh
+        self.check_wallet_refresh(nullifier).await?;
+        Ok(())
+    }
+
+    /// Check if the wallet that this nullifier belongs to needs a refresh
+    async fn check_wallet_refresh(
+        &self,
+        nullifier: Nullifier,
+    ) -> Result<(), OnChainEventListenerError> {
+        // Get the wallet ID that this nullifier belongs to
+        let maybe_wallet = self.state().get_wallet_for_nullifier(&nullifier).await?;
+        if maybe_wallet.is_none() {
+            return Ok(());
+        }
+
+        let state_clone = self.state().clone();
+        tokio::spawn(async move {
+            if let Err(e) = Self::enqueue_wallet_refresh_if_needed(nullifier, state_clone).await {
+                error!("error checking for wallet nullifier refresh: {e}");
+            }
+        });
+
+        Ok(())
+    }
+
+    /// Wait for a randomized delay then enqueue a wallet refresh if the wallet
+    /// is still indexed by an old nullifier
+    ///
+    /// We do not immediately refresh the wallet, but instead wait for a
+    /// randomized timeout to pass. This allows time for other components of the
+    /// relayer to process the nullifier spend and update the wallet state
+    /// accordingly. Randomizing the timeout prevents a thundering herd
+    ///
+    /// As such, after the timeout, we check whether the wallet is still indexed
+    /// by the nullifier, and if so we refresh it
+    async fn enqueue_wallet_refresh_if_needed(
+        nullifier: Nullifier,
+        state: State,
+    ) -> Result<(), OnChainEventListenerError> {
+        // Generate a random delay and sleep for that duration
+        let mut rng = OsRng;
+        let delay_seconds =
+            rng.gen_range(MIN_NULLIFIER_REFRESH_DELAY_S..=MAX_NULLIFIER_REFRESH_DELAY_S);
+        let delay = Duration::from_secs(delay_seconds);
+        tokio::time::sleep(delay).await;
+
+        // Check if a wallet is still indexed by the nullifier
+        let maybe_wallet = state.get_wallet_for_nullifier(&nullifier).await?;
+        if let Some(id) = maybe_wallet {
+            info!("refreshing wallet {id} after nullifier spend");
+            state.append_wallet_refresh_task(id).await?;
+        }
+
         Ok(())
     }
 }
