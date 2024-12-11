9cdcc7ffd8f11277eb8342a813555530edf3c322
Andrew Kirillov <andrew@kirillov.org>
2024-12-10 16:31:18 -0800

    workers: event-manager: manager: create event manager executor

--- workers/event-manager/src/error.rs
+++ workers/event-manager/src/error.rs
@@ -2,4 +2,17 @@
 
 /// An error that occurred in the event manager
 #[derive(Clone, Debug)]
-pub enum EventManagerError {}
+pub enum EventManagerError {
+    /// The event manager was cancelled
+    Cancelled(String),
+    /// The event export address is invalid
+    InvalidEventExportAddr(String),
+    /// An error occurred while connecting to the event export socket
+    SocketConnection(String),
+    /// An error occurred while serializing an event
+    Serialize(String),
+    /// An error occurred while writing to the event export socket
+    SocketWrite(String),
+    /// An error occurred while setting up the event manager
+    SetupError(String),
+}
