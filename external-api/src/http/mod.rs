191f20c0f506beedd75c924178c65ec0d24cbafa
Joey Kraut <joey@renegade.fi>
2025-01-20 17:16:37 -0800

    external-api: Gate API subsets behind feature flags

--- external-api/src/http/mod.rs
+++ external-api/src/http/mod.rs
@@ -2,13 +2,21 @@
 
 use serde::{Deserialize, Serialize};
 
+#[cfg(feature = "admin-api")]
 pub mod admin;
+#[cfg(feature = "external-match-api")]
 pub mod external_match;
+#[cfg(feature = "network-api")]
 pub mod network;
+#[cfg(feature = "order-book-api")]
 pub mod order_book;
+#[cfg(feature = "full-api")]
 pub mod price_report;
+#[cfg(feature = "task-api")]
 pub mod task;
+#[cfg(feature = "task-api")]
 pub mod task_history;
+#[cfg(feature = "wallet-api")]
 pub mod wallet;
 
 /// A ping response
--- external-api/src/types/mod.rs
+++ external-api/src/types/mod.rs
@@ -1,13 +1,22 @@
 //! API types for the relayer's websocket and HTTP APIs
-
+#[cfg(feature = "admin-api")]
 mod admin;
+#[cfg(feature = "network-api")]
 mod api_network_info;
+#[cfg(feature = "order-book-api")]
 mod api_order_book;
+#[cfg(feature = "task-api")]
 mod api_task_history;
+#[cfg(feature = "wallet-api")]
 mod api_wallet;
 
+#[cfg(feature = "admin-api")]
 pub use admin::*;
+#[cfg(feature = "network-api")]
 pub use api_network_info::*;
+#[cfg(feature = "order-book-api")]
 pub use api_order_book::*;
+#[cfg(feature = "task-api")]
 pub use api_task_history::*;
+#[cfg(feature = "wallet-api")]
 pub use api_wallet::*;
