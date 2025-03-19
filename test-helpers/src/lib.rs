439ae4c912f02f4b7108c7e23b16dc3f9650c887
Joey Kraut <joey@renegade.fi>
2025-03-19 15:23:31 -0700

    test-helpers: Cargo.toml: Gate modules behind features

--- test-helpers/src/lib.rs
+++ test-helpers/src/lib.rs
@@ -3,10 +3,16 @@
 #![deny(clippy::missing_docs_in_private_items)]
 #![deny(unsafe_code)]
 
+#[cfg(feature = "arbitrum")]
 pub mod arbitrum;
 pub mod assertions;
+#[cfg(feature = "arbitrum")]
 pub mod contract_interaction;
+#[cfg(feature = "test-harness")]
 pub mod macros;
+#[cfg(feature = "mocks")]
 pub mod mocks;
+#[cfg(feature = "mpc-network")]
 pub mod mpc_network;
+#[cfg(feature = "test-harness")]
 pub mod types;
