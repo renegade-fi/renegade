5b808a46ff3feb01c308e8a30289e033ea447965
Joey Kraut <joey@renegade.fi>
2025-06-18 13:05:09 -0700

    api-server: integration: external_match: Add min fill tests

--- workers/api-server/integration/external_match/min_fill_size.rs
+++ workers/api-server/integration/external_match/min_fill_size.rs
@@ -8,9 +8,9 @@ use test_helpers::{assert_eq_result, integration_test_async};
 
 use crate::ctx::IntegrationTestCtx;
 
-/// Test a base denominated min fill size
+/// Test a base denominated min fill size, with no quote
 #[allow(non_snake_case)]
-async fn test_min_fill_size__no_quote(mut ctx: IntegrationTestCtx) -> Result<()> {
+async fn test_min_fill_size__base_denominated__no_quote(mut ctx: IntegrationTestCtx) -> Result<()> {
     // Clear the state
     ctx.clear_state().await?;
 
@@ -33,4 +33,34 @@ async fn test_min_fill_size__no_quote(mut ctx: IntegrationTestCtx) -> Result<()>
     let resp = ctx.send_external_quote_req(&external_order).await?;
     assert_eq_result!(resp.status(), StatusCode::NO_CONTENT)
 }
-integration_test_async!(test_min_fill_size__no_quote);
+integration_test_async!(test_min_fill_size__base_denominated__no_quote);
+
+/// Test a quote denominated min fill size, with no quote
+#[allow(non_snake_case)]
+async fn test_min_fill_size__quote_denominated__no_quote(
+    mut ctx: IntegrationTestCtx,
+) -> Result<()> {
+    // Clear the state
+    ctx.clear_state().await?;
+
+    // Create an external order with a min fill size
+    let quote_amount = ctx.quote_token().convert_from_decimal(100.); // 100 USDC
+    let base_amount = ctx.expected_base_amount(quote_amount);
+    let external_order = ExternalOrder {
+        base_mint: ctx.base_mint(),
+        quote_mint: ctx.quote_mint(),
+        side: OrderSide::Sell,
+        quote_amount,
+        min_fill_size: quote_amount,
+        ..Default::default()
+    };
+
+    // Setup an order which is too small to match
+    let order = ctx.build_matching_order_with_amount(&external_order, base_amount - 1)?;
+    ctx.setup_wallet_with_order(order).await?;
+
+    // Fetch a quote, it should return no content
+    let resp = ctx.send_external_quote_req(&external_order).await?;
+    assert_eq_result!(resp.status(), StatusCode::NO_CONTENT)
+}
+integration_test_async!(test_min_fill_size__quote_denominated__no_quote);
