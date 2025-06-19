0a1f527077cc6ab8219dddc5eebdebfe0eb1f9a7
Joey Kraut <joey@renegade.fi>
2025-06-18 18:21:41 -0700

    api-server: integration: exact-amount: Add exact amount tests

---
+++ workers/api-server/integration/external_match/exact_amount.rs
@@ -0,0 +1,119 @@
+//! Integration tests for specifying exact output amounts
+
+use circuit_types::order::OrderSide;
+use external_api::http::external_match::ExternalOrder;
+use eyre::Result;
+use test_helpers::{assert_eq_result, integration_test_async};
+
+use crate::ctx::IntegrationTestCtx;
+
+/// Test specifying an exact quote amount on the buy side
+#[allow(non_snake_case)]
+async fn test_exact_quote_amount__buy_side(mut ctx: IntegrationTestCtx) -> Result<()> {
+    // Clear the state
+    ctx.clear_state().await?;
+
+    // Create an external order with an exact quote amount
+    let exact_quote_output = ctx.quote_token().convert_from_decimal(1000.);
+    let external_order = ExternalOrder {
+        base_mint: ctx.base_mint(),
+        quote_mint: ctx.quote_mint(),
+        side: OrderSide::Buy,
+        exact_quote_output,
+        ..Default::default()
+    };
+
+    // Setup a matching order, then request a quote
+    ctx.setup_crossing_wallet(&external_order).await?;
+
+    // Fetch a quote, then assemble the quote into a match bundle
+    let resp = ctx.request_external_quote(&external_order).await?;
+    let quote = resp.signed_quote.quote;
+    let send_amount = quote.send.amount;
+    assert_eq_result!(send_amount, exact_quote_output)
+}
+integration_test_async!(test_exact_quote_amount__buy_side);
+
+/// Test specifying an exact quote amount on the sell side
+#[allow(non_snake_case)]
+async fn test_exact_quote_amount__sell_side(mut ctx: IntegrationTestCtx) -> Result<()> {
+    // Clear the state
+    ctx.clear_state().await?;
+
+    // Create an external order with an exact quote amount
+    let exact_quote_output = ctx.quote_token().convert_from_decimal(1000.);
+    let external_order = ExternalOrder {
+        base_mint: ctx.base_mint(),
+        quote_mint: ctx.quote_mint(),
+        side: OrderSide::Sell,
+        exact_quote_output,
+        ..Default::default()
+    };
+
+    // Setup a matching order, then request a quote
+    ctx.setup_crossing_wallet(&external_order).await?;
+
+    // Fetch a quote, then assemble the quote into a match bundle
+    let resp = ctx.request_external_quote(&external_order).await?;
+    let quote = resp.signed_quote.quote;
+
+    // The API should account for fees in the match amount, so we should get exactly
+    // the specified amount back
+    let recv_amount = quote.receive.amount;
+    assert_eq_result!(recv_amount, exact_quote_output)
+}
+integration_test_async!(test_exact_quote_amount__sell_side);
+
+/// Test specifying an exact base amount on the buy side
+#[allow(non_snake_case)]
+async fn test_exact_base_amount__buy_side(mut ctx: IntegrationTestCtx) -> Result<()> {
+    // Clear the state
+    ctx.clear_state().await?;
+
+    // Create an external order with an exact base amount
+    let exact_base_output = ctx.base_token().convert_from_decimal(1.);
+    let external_order = ExternalOrder {
+        base_mint: ctx.base_mint(),
+        quote_mint: ctx.quote_mint(),
+        side: OrderSide::Buy,
+        exact_base_output,
+        ..Default::default()
+    };
+
+    // Setup a matching order, then request a quote
+    ctx.setup_crossing_wallet(&external_order).await?;
+
+    // Fetch a quote, then assemble the quote into a match bundle
+    let resp = ctx.request_external_quote(&external_order).await?;
+    let quote = resp.signed_quote.quote;
+    let recv_amount = quote.receive.amount;
+    assert_eq_result!(recv_amount, exact_base_output)
+}
+integration_test_async!(test_exact_base_amount__buy_side);
+
+/// Test specifying an exact base amount on the sell side
+#[allow(non_snake_case)]
+async fn test_exact_base_amount__sell_side(mut ctx: IntegrationTestCtx) -> Result<()> {
+    // Clear the state
+    ctx.clear_state().await?;
+
+    // Create an external order with an exact base amount
+    let exact_base_output = ctx.base_token().convert_from_decimal(1.);
+    let external_order = ExternalOrder {
+        base_mint: ctx.base_mint(),
+        quote_mint: ctx.quote_mint(),
+        side: OrderSide::Sell,
+        exact_base_output,
+        ..Default::default()
+    };
+
+    // Setup a matching order, then request a quote
+    ctx.setup_crossing_wallet(&external_order).await?;
+
+    // Fetch a quote, then assemble the quote into a match bundle
+    let resp = ctx.request_external_quote(&external_order).await?;
+    let quote = resp.signed_quote.quote;
+    let send_amount = quote.send.amount;
+    assert_eq_result!(send_amount, exact_base_output)
+}
+integration_test_async!(test_exact_base_amount__sell_side);
--- workers/api-server/integration/external_match/mod.rs
+++ workers/api-server/integration/external_match/mod.rs
@@ -1,4 +1,5 @@
 //! Integration tests for the external match API
 
 mod basic_match;
+mod exact_amount;
 mod min_fill_size;
