79987047500b795063cd8a3af0a56455031211e8
Joey Kraut <108701651+joeykraut@users.noreply.github.com>
2025-11-05 08:19:24 -0800

    circuits: zk-circuits: valid-note-redemption: Add circuit + tests (#1167)

--- circuits/src/zk_circuits/v2/fees/mod.rs
+++ circuits/src/zk_circuits/v2/fees/mod.rs
@@ -1,5 +1,6 @@
 //! Fee-related circuit definitions
 
+pub mod valid_note_redemption;
 pub mod valid_private_protocol_fee_payment;
 pub mod valid_private_relayer_fee_payment;
 pub mod valid_public_protocol_fee_payment;
